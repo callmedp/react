ccefb50374be2b2aabc2e9b111b770577b6dbb51
ft0593 <akamarnath2@gmail.com>
2017-06-14 11:47:57 +0530

    removed ipdb

--- careerplus/apps/payment/ccavenue.py
+++ careerplus/apps/payment/ccavenue.py
@@ -85,7 +85,6 @@ class Ccavenue(View, PaymentMixin, OrderMixin):
     def decrypt(self, cipherText, workingKey):
 
         if py_major == '3' and py_minor == '4':
-            import ipdb; ipdb.set_trace()
             # python 3.4
             iv = '\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f'
             decDigest = md5()
