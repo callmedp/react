026e3ace6dedb425883dd74f501c1e46723bbe0f
Indra <iitd.indra@gmail.com>
2017-07-20 14:39:26 +0530

    fixed admin erro

--- careerplus/apps/partner/admin.py
+++ careerplus/apps/partner/admin.py
@@ -3,18 +3,17 @@ from . import models
 
 
 class VendorHierarchyInline(admin.TabularInline):
-	model = models.VendorHierarchy
-	extra = 1
+    model = models.VendorHierarchy
+    extra = 1
 
 
 class VendorAdmin(admin.ModelAdmin):
-	list_display = ('id', 'name', 'slug', 'email', 'mobile', 'pan', 'website')
+    list_display = ('id', 'name', 'slug', 'email', 'mobile', 'pan', 'website')
 
-	inlines = [VendorHierarchyInline]
+    inlines = [VendorHierarchyInline]
     model = models.VendorHierarchy
     fk_name = 'vendee'
     readonly_fields = ('modified',)
-    raw_id_fields = ['vendee', 'employee']
     
     extra = 0
 
