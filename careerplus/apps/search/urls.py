33159003a6e2b6dca15ddf0228075653b1bb20c7
Indra <iitd.indra@gmail.com>
2017-07-31 18:39:16 +0530

    search listinh

--- careerplus/apps/search/urls.py
+++ careerplus/apps/search/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
-from .views import SearchListView, SearchQueryView
+from .views import SearchListView, SearchQueryView, TestListingView
 
 urlpatterns = [
     url(r'^results/$', SearchListView.as_view(), name='result_old'),
     url(r'^results/(?P<keyword_in_url>[-_./\w()]+)/$', SearchListView.as_view(), name='result'),
     url(r'^$', SearchQueryView.as_view()),
+ 	url(r'^listing/$', TestListingView.as_view()),
+       
 ]
