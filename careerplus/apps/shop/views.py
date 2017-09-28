556945d7fb913fa7e0a7638d124846a3235de1b5
ft0593 <akamarnath2@gmail.com>
2017-09-28 14:36:46 +0530

    ccavenue

--- careerplus/apps/shop/views.py
+++ careerplus/apps/shop/views.py
@@ -336,10 +336,10 @@ class ProductDetailView(TemplateView, ProductInformationMixin, CartMixin):
 
         ctx.update(json.loads(self.sqs.pFBT))
         get_fakeprice = self.get_solar_fakeprice(self.sqs.pPinb, self.sqs.pPfinb)
-        # ctx.update(self.getSelectedProduct(product))
-        # ctx.update(self.getSelectedProductPrice(product))
-        ctx.update({'sqs':self.sqs})
-        ctx.update({'get_fakeprice':get_fakeprice})
+        # ctx.update(self.getSelectedProduct_solr(self.sqs))
+        # ctx.update(self.getSelectedProductPrice_solr(self.sqs))
+        ctx.update({'sqs': self.sqs})
+        ctx.update({'get_fakeprice': get_fakeprice})
         return ctx
 
     def redirect_if_necessary(self, current_path, product):
