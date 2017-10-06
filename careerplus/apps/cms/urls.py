7157c08276d5791151cf1dcd1ec07ef5aaf59730
ft0593 <akamarnath2@gmail.com>
2017-10-06 13:45:36 +0530

    changes cms url

--- careerplus/apps/cms/urls.py
+++ careerplus/apps/cms/urls.py
@@ -4,8 +4,7 @@ from .views import CMSPageView, LeadManagementView,\
    DownloadPdfView
 
 urlpatterns = [
-    url(r'^page/(?P<slug>[-\w]+)/(?P<pk>\d+)/$', CMSPageView.as_view(), name='page'),
-    
+    url(r'^(?P<slug>[-\w]+)/(?P<pk>\d+)/$', CMSPageView.as_view(), name='page'),
     url(r'^lead-management/$', LeadManagementView.as_view(), name='lead-management'),
 
     url(r'^download-pdf/(?P<pk>\d+)/$', DownloadPdfView.as_view(), name='download-pdf'),
