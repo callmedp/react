cbab7484100e621107e878124a601a5bb80c5bbe
Snigdha <snig_b@yahoo.com>
2017-10-18 14:05:34 +0530

    g plus

--- careerplus/config/settings_live.py
+++ careerplus/config/settings_live.py
@@ -9,17 +9,18 @@ SITE_DOMAIN = 'learning1.shine.com'
 SITE_PROTOCOL = 'http'
 SHINE_SITE = 'https://www.shine.com'
 SHINE_API_URL = 'https://shine.com/api/v2'
-
+COMPRESS_ENABLED = True
+COMPRESS_OFFLINE = True
 
 HAYSTACK_CONNECTIONS = {
     'default': {
         'ENGINE': 'core.library.haystack.custom_solr_backend.CustomSolrEngine',
-        'URL': 'http://172.22.65.33:8983/solr/prdt',
+        'URL': 'http://172.22.65.33:8983/solr/live_prod',
         'INCLUDE_SPELLING': False,
     },
 }
 
 ####### CCAVENUE SETTINGS ###########################
-CCAVENUE_ACCESS_CODE = 'AVEX73EI34CC49XECC'
+CCAVENUE_ACCESS_CODE = 'AVEX73EI34CC48XECC'
 CCAVENUE_WORKING_KEY = 'BB84397177B2D640744BA272627C2A61'
 CCAVENUE_URL = 'https://secure.ccavenue.com/transaction/transaction.do?command=initiateTransaction'
