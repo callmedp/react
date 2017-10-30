16ae06a7ed9172625b7b88f6a755800574be808e
superindian <iitd.indra@gmail.com>
2017-10-30 14:34:11 +0530

    advanced search

--- careerplus/apps/search/urls.py
+++ careerplus/apps/search/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url
 from .views import SearchListView, RecommendedSearchView, RedirectToRecommendationsView
 
 urlpatterns = [
-    url(r'^recommended/(?P<area_slug>[-./\w+\(\)]+)-(?P<area>[\d]+)/(?P<skills_slug>[-./\w+]+)_(?P<skills>[-\d]+)/$',
-        RecommendedSearchView.as_view(), name='recommended_listing'),
+    # url(r'^recommended/(?P<area_slug>[-./\w+\(\)]+)-(?P<area>[\d]+)/(?P<skills_slug>[-./\w+]+)_(?P<skills>[-\d]+)/$',
+    #     RecommendedSearchView.as_view(), name='recommended_listing'),
     url(r'^results/$', SearchListView.as_view(), name='search_listing'),
-    url(r'^redirect-to-recommendations/$', RedirectToRecommendationsView.as_view(), name='recommend')
+    # url(r'^redirect-to-recommendations/$', RedirectToRecommendationsView.as_view(), name='recommend')
 ]
