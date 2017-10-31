9ae4023d00e9a256b62ae8b862327073561493ed
Snigdha <snig_b@yahoo.com>
2017-10-31 19:17:21 +0530

    Celery setting

--- careerplus/config/celery.py
+++ careerplus/config/celery.py
@@ -6,7 +6,7 @@ from celery import Celery
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'careerplus.config.settings')
 
-app = Celery('careerplus', backend='redis', broker='redis://localhost:6379/0')
+app = Celery('careerplus', backend=None, broker='redis://localhost:6379/0')
 
 # Using a string here means the worker don't have to serialize
 # the configuration object to child processes.
