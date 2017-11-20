83e81b1cb88ad64abdf57cd6155624514f1da769
Akbar HT Media <akbar.uddin@hindustantimes.com>
2017-11-20 17:41:54 +0530

    stuff fixes

---
+++ careerplus/static_core/shinelearn/js/common/plugin.activeOnScroll.js
@@ -0,0 +1,38 @@
+;(function($, activeOnScroll) {
+	
+	var Scroll = {
+		class: 'active',
+		_load: function (container) {
+			var child = $(container).children();
+			child.removeClass(this.class);
+			child.first().addClass(this.class);
+		},
+		_scroll: function (container) {
+			var child = $(container).children();
+			var height = $(container).height();
+
+			$(document).on("scroll", function(event) {
+				var scrollPos = $(document).scrollTop();
+			    child.each(function () {
+			        var currLink = $(this);
+			        var refElement = $(currLink.attr("href"));
+			        if (refElement.offset().top <= scrollPos + height && refElement.offset().top + refElement.outerHeight() > scrollPos + height) {
+			            child.removeClass("active");
+			            currLink.addClass("active");
+			        }
+			    });
+			});
+
+		}
+	}
+
+	function init(argument) {
+		this.className = argument.className || '.scroll-tab';
+
+		Scroll._load(this.className);
+		Scroll._scroll(this.className);
+
+	}
+
+	activeOnScroll.init = init;
+}($, (window.activeOnScroll = window.activeOnScroll || {} )))
