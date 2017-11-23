f41a2843141cd0ec3267305aaba99f53bed6535a
Akbar HT Media <akbar.uddin@hindustantimes.com>
2017-11-23 11:15:05 +0530

    talenteconomy category page

--- careerplus/apps/talenteconomy/urls.py
+++ careerplus/apps/talenteconomy/urls.py
@@ -10,6 +10,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
      url(r'^$', TemplateView.as_view(template_name="talenteconomy/landing.html"), name='talent-landing'),
+     url(r'^category/', TemplateView.as_view(template_name="talenteconomy/category.html"), name='talent-landing'),
 
     
 
