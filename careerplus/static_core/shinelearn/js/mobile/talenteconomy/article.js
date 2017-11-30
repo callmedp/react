6c3959189726d2de1660f06f0691ae1f92c7e781
Akbar HT Media <akbar.uddin@hindustantimes.com>
2017-11-30 16:16:30 +0530

    ga add

--- careerplus/static_core/shinelearn/js/mobile/talenteconomy/article.js
+++ careerplus/static_core/shinelearn/js/mobile/talenteconomy/article.js
@@ -12,6 +12,17 @@
 			infinite: true
 		});
 
+		$(".article-slider").slick({
+			autoplay:false,
+			autoplaySpeed:3000,
+			dots: false,
+			arrows: false,
+			variableWidth: true,
+			slidesToShow: 2,
+			slidesToScroll: 1,
+			infinite: true
+		});
+
 	}
 
 	Article.init = init;
