adb7f845251ae52b77dcdb5f02cda221f24589ab
upendersingh <upenders379@gmail.com>
2017-12-01 12:13:00 +0530

    change in cart lead

--- careerplus/apps/crmapi/functions.py
+++ careerplus/apps/crmapi/functions.py
@@ -11,15 +11,19 @@ def lead_create_on_crm(cart_obj, data_dict=None):
     post_url = settings.SHINECPCRM_DICT.get('base_url') + \
         settings.SHINECPCRM_DICT.get('create_lead_url')
     try:
-        rsp = requests.post(
-            post_url, data=json.dumps(data_dict),
-            headers=headers,
-            timeout=settings.SHINECPCRM_DICT.get('timeout'))
-        if rsp.status_code == 201 and rsp.get('status') == 1:
-            cart_obj.lead_creted = True
-            cart_obj.save()
-            logging.info(rsp.json())
-        elif rsp.status_code == 400 and rsp.get('status') == 0:
-            logging.getLogger('error_log').error(rsp.json())
+        mobileNo = data_dict.get('mobile')
+        if mobileNo:
+            rsp = requests.post(
+                post_url, data=json.dumps(data_dict),
+                headers=headers,
+                timeout=settings.SHINECPCRM_DICT.get('timeout'))
+            if rsp.status_code == 201 and rsp.get('status') == 1:
+                cart_obj.lead_creted = True
+                cart_obj.save()
+                logging.info(rsp.json())
+            elif rsp.status_code == 400 and rsp.get('status') == 0:
+                logging.getLogger('error_log').error(rsp.json())
+        else:
+            logging.getLogger("error_log").error("cart have no mobile number")
     except Exception as e:
         logging.getLogger('error_log').error("%s" % str(e))
