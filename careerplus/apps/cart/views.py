25f12ef1a743d905e6dbbb53404801e4865b695c
Snigdha <snig_b@yahoo.com>
2017-12-02 19:14:10 +0530

    Undo changes temp

--- careerplus/apps/cart/views.py
+++ careerplus/apps/cart/views.py
@@ -78,7 +78,7 @@ class AddToCartView(View, CartMixin):
                 req_options = request.POST.getlist('req_options[]')
                 cv_id = request.POST.get('cv_id')
                 data['status'] = self.updateCart(product, addons, cv_id, cart_type, req_options)
-                cart_drop_out_mail.apply_async((), countdown=10)
+                cart_drop_out_mail.apply_async((), countdown=45*60)
 
                 try:
                     cart_obj = Cart.objects.get(pk=cart_pk)
@@ -89,7 +89,7 @@ class AddToCartView(View, CartMixin):
                     cart_drop_out_mail.apply_async((cart_pk,), countdown=10)
                     source_type = "cart_drop_out"
                     create_lead_on_crm.apply_async(
-                        (cart_obj.pk, source_type,), countdown=10)
+                        (cart_obj.pk, source_type,), countdown=12*60*60)
             except Exception as e:
                 data['error_message'] = str(e)
                 logging.getLogger('error_log').error("%s " % str(e))
@@ -366,7 +366,7 @@ class PaymentShippingView(UpdateView, CartMixin):
                 valid_form = self.form_valid(form)
                 source_type = "shipping_drop_out"
                 create_lead_on_crm.apply_async(
-                    (obj.pk, source_type,), countdown=10)
+                    (obj.pk, source_type,), countdown=60*20)
                 return valid_form
             except Exception as e:
                 non_field_error = 'Personal detail not updated due to %s' % (str(e))
