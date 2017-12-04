69121160568b26288bb8a0f77c50e449181827ff
superindian <iitd.indra@gmail.com>
2017-12-04 21:07:51 +0530

    article js changes

--- careerplus/static_core/shinelearn/js/talenteconomy/article.js
+++ careerplus/static_core/shinelearn/js/talenteconomy/article.js
@@ -5,7 +5,25 @@
 	        interval: false,
 	    });
 
-	    $('.carousel[data-type="multi"] .item').each(function(){
+	    
+     $('.carousel[data-type="multi"] .item').each(function(){
+        var next = $(this).next();
+        if (!next.length) {
+          next = $(this).siblings(':first');
+        }
+        next.children(':first-child').clone().appendTo($(this));
+
+        for (var i=0;i<3;i++) {
+          next=next.next();
+          if (!next.length) {
+            next = $(this).siblings(':first');
+          }
+          
+          next.children(':first-child').clone().appendTo($(this));
+        }
+      });
+
+	    /*$('.carousel[data-type="multi"] .item').each(function(){
 	        var next = $(this).next();
 	    
 	        if (!next.length) {
@@ -24,7 +42,7 @@
 	          
 	         // next.children(':first-child').clone().appendTo($(this));
 	        }
-	    });
+	    });*/
 
 	  	$(document).on('click', '#article_share', function(event) {
 	        $.ajax({
