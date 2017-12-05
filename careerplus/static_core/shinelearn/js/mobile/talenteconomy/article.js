a8075ff6ddba565e2344154f8f27c5681b25ea21
aditigupta1527 <aditi.gupta@hindustantimes.com>
2017-12-05 12:37:51 +0530

    mobile comments

--- careerplus/static_core/shinelearn/js/mobile/talenteconomy/article.js
+++ careerplus/static_core/shinelearn/js/mobile/talenteconomy/article.js
@@ -36,6 +36,10 @@
         });
     });
 
+	$(document).on('click', '.js_redirect', function(event) {
+        window.location.href = window.MOBILE_LOGIN_URL + "?next="+window.location.pathname;
+    });
+
 	}
 
 	Article.init = init;
