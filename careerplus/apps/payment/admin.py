84b102b2462a5487324f2a7a51c646dc696ba2a3
ft0593 <akamarnath2@gmail.com>
2017-12-15 10:54:12 +0530

    admin changes

--- careerplus/apps/payment/admin.py
+++ careerplus/apps/payment/admin.py
@@ -7,7 +7,8 @@ class PaymentTxnAdmin(admin.ModelAdmin):
     list_display = ['id', 'order', 'txn', 'status', 'payment_mode',
         'payment_date', 'currency', 'instrument_number', 'instrument_issuer',
         'instrument_issue_date', 'created']
-    list_filter = ('status', 'payment_mode')
+    list_filter = ('status', 'payment_mode', 'currency')
     search_fields = ('id', 'txn', 'instrument_number')
+    raw_id_fields = ('order', 'cart')
 
 admin.site.register(PaymentTxn, PaymentTxnAdmin)
