e73da846142d947fff3fc0d720162b99ccd1c2be
mohitarora11 <mohit.11.arora@gmail.com>
2018-01-11 12:36:59 +0530

    search bar in navigation

--- careerplus/static_core/shinelearn/js/common/common.js
+++ careerplus/static_core/shinelearn/js/common/common.js
@@ -1,3 +1,34 @@
 $(document).on("click","a[href^='tel']",function(){
     MyGA.SendEvent('CallbackRequested', 'Call Interactions', 'General Enquiry', 'success');
 });
+
+
+$('.cls_search').each(function(index,item){
+        $(item).find('input[name=q]').typeahead({
+            local: qSearch
+        });
+
+        $(item).validate({
+            rules : {
+                'q':'required'
+            },
+            messages:{
+                'q':'Please enter a query'
+            },
+            submitHandler:function(form){
+                var qVal = encodeURI($(form).find('input[name=q]').val());
+                location.href = '/search/results/?q='+qVal;
+            },
+            errorPlacement:function(error,element){
+                
+                $(element).closest('div').find('.error-txt').html(error);  
+            },
+            highlight:function(el){
+                $(el).closest('div').addClass('error-search');
+            },
+            unhighlight:function(el){
+                $(el).closest('div').removeClass('error-search');
+            }
+        })
+
+    });
