4ffb8899a43b4c3a4e9ad84a2dae9b3a17c7516b
aditigupta1527 <aditi.gupta@hindustantimes.com>
2018-02-09 15:53:07 +0530

    views 2

--- careerplus/apps/hrinsider/urls.py
+++ careerplus/apps/hrinsider/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
 from django.views.generic import TemplateView
 
-from .views import HRLandingView
+from .views import HRLandingView, HRBlogDetailView
 
 urlpatterns = [
      url(r'^$', HRLandingView.as_view(), name='hr-landing'),
+     url(r'^(?P<cat_slug>[-\w]+)/(?P<slug>[-\w]+)/$', 
+     	HRBlogDetailView.as_view(), name='hr-articles-detail'),
 
 ]
 
--- careerplus/apps/hrinsider/views.py
+++ careerplus/apps/hrinsider/views.py
@@ -7,9 +7,13 @@ from django.views.generic import (
     DetailView,
     View)
 
+from django.http import HttpResponseForbidden, Http404,\
+    HttpResponsePermanentRedirect, HttpResponse
 from django.conf import settings
 from meta.views import Meta
 from django.db.models import Count
+from django.urls import reverse
+from django.template.loader import render_to_string
 from blog.mixins import BlogMixin, PaginationMixin
 from blog.models import Category, Blog, Tag, Author
 
@@ -28,23 +32,18 @@ class HRLandingView(TemplateView, BlogMixin):
 
     def get_context_data(self, **kwargs):
         context = super(self.__class__, self).get_context_data(**kwargs)
-        categories = Category.objects.filter(is_active=True, visibility=2).order_by('-name')
+        categories = Category.objects.filter(is_active=True, visibility=3).order_by('-name')
 
-        article_list = Blog.objects.filter(status=1, visibility=2).select_related('p_cat','author').order_by('-publish_date')[:10]
-        top_article_list = Blog.objects.filter(status=1, visibility=2).select_related('p_cat','author')[:9]
+        article_list = Blog.objects.filter(status=1, visibility=3).select_related('p_cat','author').order_by('-publish_date')[:10]
+        top_article_list = Blog.objects.filter(status=1, visibility=3).select_related('p_cat','author')[:9]
 
-        authors = Author.objects.filter(visibility=2,blog__visibility=2,blog__status=1).annotate(no_of_blog=Count('blog')).order_by('-no_of_blog')
+        authors = Author.objects.filter(visibility=3,blog__visibility=3,blog__status=1).annotate(no_of_blog=Count('blog')).order_by('-no_of_blog')
         author_list = zip_longest(*[iter(authors)]*5, fillvalue=None)
-        
-        top_3_cats = [article.p_cat.slug for article in top_article_list][:3]
-        top_cats = '+'.join(top_3_cats)
-        popular_courses = self.get_product(top_cats)
         
         context.update({
         'top_article_list':[top_article_list[:3], top_article_list[3:6], top_article_list[6:9]],
         'categories': categories,
         'article_list': article_list,
-        'popular_courses':popular_courses,
         'authors':authors,
         'authors_list': list(author_list)
         })
@@ -56,13 +55,96 @@ class HRLandingView(TemplateView, BlogMixin):
     def get_breadcrumb_data(self):
         breadcrumbs = []
         breadcrumbs.append({"url": '/', "name": "Home"})
-        breadcrumbs.append({"url": None, "name": "Talent Economy"})
+        breadcrumbs.append({"url": None, "name": "HR Insider"})
         data = {"breadcrumbs": breadcrumbs}
         return data
 
     def get_meta_details(self):
         meta = Meta(
-            title="Talent Economy: Career Skilling for a future ready India",
-            description="Talent Economy - The best way to choose better career options. Get experts' advice & ideas for planning your future growth @ Shine Learning",
+            title="HR insider: Career Skilling for a future ready India",
+            description="HR insider - The best way to choose better career options. Get experts' advice & ideas for planning your future growth @ Shine Learning",
+        )
+        return {"meta": meta}
+
+
+
+class HRBlogDetailView(DetailView, BlogMixin):
+    template_name = "talenteconomy/article-detail.html"
+    model = Blog
+
+    def __init__(self):
+        self.article = None
+        self.paginated_by = 1
+        self.page = 1
+
+    def get_queryset(self):
+        qs = Blog.objects.filter(status=1,visibility=3)
+        return qs
+
+    def get_object(self, queryset=None):
+        cat_slug = self.kwargs.get('cat_slug')
+        slug = self.kwargs.get('slug')
+        if queryset is None:
+            queryset = self.get_queryset()
+
+        if slug is not None:
+            queryset = queryset.filter(slug=slug, status=1, visibility=3)
+        try:
+            obj = queryset.get()
+        except:
+            raise Http404
+        return obj
+
+    def get(self, request, *args, **kwargs):
+        self.slug = kwargs.get('slug', None)
+        self.page = request.GET.get('page', 1)
+        self.object = self.get_object()
+        self.object.no_views += 1
+        self.object.update_score()
+        self.object.save()
+
+        context = super(self.__class__, self).get(request, args, **kwargs)
+        return context
+
+    def get_context_data(self, **kwargs):
+        context = super(self.__class__, self).get_context_data(**kwargs)
+        blog = self.object
+        p_cat = blog.p_cat
+        articles = p_cat.primary_category.filter(status=1, visibility=3).exclude(pk=blog.pk)
+        articles = articles.order_by('-publish_date')
+
+        context['meta'] = blog.as_meta(self.request)
+
+        context.update(self.get_breadcrumb_data())
+        context['SITEDOMAIN'] = settings.SITE_DOMAIN
+
+        main_obj = Blog.objects.filter(slug=blog.slug, status=1, visibility=3).prefetch_related('tags')
+
+        context.update({
+            "main_article": main_obj[0],
+        })
+
+        article_list = Blog.objects.filter(p_cat=p_cat, status=1, visibility=3).order_by('-publish_date') | Blog.objects.filter(sec_cat__in=[p_cat], status=1, visibility=3).order_by('-publish_date')
+        article_list = article_list.exclude(slug=blog.slug)
+        article_list = article_list.distinct().select_related('created_by').prefetch_related('tags')
+
+        context.update(self.get_meta_details())
+        return context
+
+    def get_breadcrumb_data(self):
+        breadcrumbs = []
+        breadcrumbs.append({"url": '/', "name": "Home"})
+        breadcrumbs.append({"url": reverse('talent:talent-landing'), "name": "Talent Economy"})
+        breadcrumbs.append({"url": reverse('talent:te-articles-by-category', kwargs={'slug': self.object.p_cat.slug}), "name": self.object.p_cat.name})
+        breadcrumbs.append({"url": None, "name": self.object.display_name})
+        data = {"breadcrumbs": breadcrumbs}
+        return data
+
+    def get_meta_details(self):
+        heading = self.object.heading
+        des = self.object.get_description()
+        meta = Meta(
+            title= heading + "- HR Insider",
+            description = des,
         )
         return {"meta": meta}
