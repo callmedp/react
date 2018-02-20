4913bf2530e03ff3bbfeb6af893e8a119d915861
ft0593 <akamarnath2@gmail.com>
2018-02-20 17:38:27 +0530

    call task

--- careerplus/apps/console/schedule_tasks/views.py
+++ careerplus/apps/console/schedule_tasks/views.py
@@ -90,7 +90,7 @@ class GenerateAutoLoginTask(FormView):
                 else:
                     exp_days = None
 
-                gen_auto_login_token_task(
+                gen_auto_login_token_task.delay(
                     task=Task.pk, user=request.user.pk,
                     next_url=next_url, exp_days=exp_days)
                 messages.add_message(
