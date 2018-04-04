a012b73196075d7dbf2278caa0c39f70daaf23f5
upendersingh <upenders379@gmail.com>
2018-04-04 16:23:18 +0530

    Bug #31639:resolved

---
+++ careerplus/static_core/shinelearn/js/talenteconomy/scrollpage.js
@@ -0,0 +1,49 @@
+var showTalentpageOnScroll = (function(){
+    var ajaxCalled = false,
+    prev_page = 0,
+    ajaxOffSetDistance  = 600,
+    urlUpdateOffset = 100,
+    defaultUrl = top.window.location.pathname;
+    defaultTitle = document.title;
+
+    function onScroll() {
+        if($('#id_ajax_talentpage').length < 1){
+            return;
+        };
+
+        $(window).scroll(function() {
+            if ($(window).scrollTop() > $('#id_ajax_talentpage').offset().top - ajaxOffSetDistance) {
+               makeAjax();
+            };
+         });
+    };
+
+    function makeAjax() {
+        page = $("#talentpg_id").val();
+        slug = $("#talentpg_slug").val();
+        if(!ajaxCalled){
+            if (page != undefined & page != prev_page){
+                prev_page = page;
+                data = "?page="+ page+ "&slug=" + slug;
+                ajaxCalled = true;
+                $.ajax({
+                    url: "/talenteconomy/ajax/talent-detail-loading/" + data,
+                    type: "GET",
+                    dataType: "json",
+                    success: function(data) {
+                        $("#talent_load_more").remove();
+                        // var dynamicDiv = $('<div/>',{'class' : 'cls_ajax_article','html' : data.article_detail,'data-url':data.url,'data-title':data.title});
+                        $('#related-talent-container').append(data.article_detail);
+                        ajaxCalled = false;
+                    },
+                    failure: function(response){}
+                });
+            }
+        }
+    };
+
+    return {
+        scroll : onScroll
+    }
+
+})();
