a3d744719704c7401303dd593c944534690e3f84
ft0593 <akamarnath2@gmail.com>
2018-04-18 15:44:56 +0530

    popular updated

--- careerplus/apps/hrinsider/views.py
+++ careerplus/apps/hrinsider/views.py
@@ -56,14 +56,14 @@ class HRLandingView(TemplateView, BlogMixin):
                     article_list = Blog.objects.filter(
                         status=1,
                         visibility=3).select_related(
-                        'p_cat', 'author').order_by('-publish_date')[:15]
+                        'p_cat', 'author').order_by('-score')[:15]
                     context.update({
                         "no_results": True})
             else:
                 article_list = Blog.objects.filter(
                     status=1,
                     visibility=3).select_related(
-                    'p_cat', 'author').order_by('-score')[:15]
+                    'p_cat', 'author').order_by('-publish_date')[:15]
 
         top_article_list = Blog.objects.filter(
             status=1, visibility=3).select_related(
