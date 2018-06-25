23b76d3cc763e06970067bc1041ca03fc788e212
ft0593 <akamarnath2@gmail.com>
2018-06-25 18:16:13 +0530

    flat coupon

--- careerplus/apps/coupon/mixins.py
+++ careerplus/apps/coupon/mixins.py
@@ -7,9 +7,12 @@ from django.conf import settings
 
 class CouponMixin(object):
     def create_feature_coupon(self, users=[]):
+        on_products = settings.FEATURE_PROFILE_PRODUCTS
+        fprd = on_products[0]
+
         coupon_obj = Coupon.objects.create_coupon(
-            coupon_type='percent',
-            value=100,
+            coupon_type='flat',
+            value=settings.FP_COUPON_FLAT_AMOUNT,
             users=users,
             valid_until=None,
             prefix="fp",
@@ -18,7 +21,6 @@ class CouponMixin(object):
         )
         # coupon_obj.min_purchase = coupon_amount
         # coupon_obj.max_deduction = coupon_amount
-        on_products = settings.FEATURE_PROFILE_PRODUCTS
         today = timezone.now()
         expiry = today + datetime.timedelta(days=90)
         coupon_obj.valid_from = today
