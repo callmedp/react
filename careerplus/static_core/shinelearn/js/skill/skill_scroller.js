1261a8791f82c0fa1663eea496936f83e13dcc41
ft0593 <akamarnath2@gmail.com>
2018-06-25 12:54:53 +0530

    js changes

--- careerplus/static_core/shinelearn/js/skill/skill_scroller.js
+++ careerplus/static_core/shinelearn/js/skill/skill_scroller.js
@@ -21,7 +21,7 @@ var Scroller = function(itm,className) {
             that.item.find('.cls_scroller_hiddenDiv').removeClass('hidden');
         }else {
             that.item.css({
-                'top': 0
+                'top': 20
             })
             that.item.find('.cls_scroller_hiddenDiv').addClass('hidden');
         }
