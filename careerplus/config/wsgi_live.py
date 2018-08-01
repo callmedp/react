33d5ad4e20da775f5531015ee5fd0ed70dfed3af
Animesh <animax@Animeshs-MacBook-Pro.local>
2018-08-01 16:57:01 +0530

    New relic setup

--- careerplus/config/wsgi_live.py
+++ careerplus/config/wsgi_live.py
@@ -8,10 +8,11 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
-#import newrelic.agent
+import newrelic.agent
 from django.core.wsgi import get_wsgi_application
 
 os.environ["DJANGO_SETTINGS_MODULE"] = "careerplus.config.settings_live"
 #NewRelic
-#newrelic.agent.initialize('/var/www/site/shine_learning/newrelic.ini')
+newrelic.agent.initialize('/var/www/site/learning/current/careerplus/config/newrelic.ini')
+
 application = get_wsgi_application()
