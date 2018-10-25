06103dbdb829b4ccfc65246f0e633c46888bcd23
Ritesh Bisht <ritesh.bisht@hindustantimes.com>
2018-10-25 17:14:07 +0530

    #university_courses - Add read more/read less for university course page on mobile

--- careerplus/static_core/shinelearn/js/mobile/common/base.js
+++ careerplus/static_core/shinelearn/js/mobile/common/base.js
@@ -1,6 +1,43 @@
 
 // Products Box Items JS Starts here
 $(document).ready(function() { 
+	var showChar = 280 ;  // How many characters are shown by default
+	var ellipsestext = "..." ;
+	var moretext = " Read more" ;
+	var lesstext = " Read less" ;
+
+	function showMoreLess(){
+	  // Configure/customize these variables.
+
+	  $('.more1 p').each(function() {
+	    var content = $(this).html();
+
+	    if(content.length > showChar) {
+	        var c = content.substr(0, showChar);
+	        var h = content.substr(showChar, content.length - showChar);
+	        var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span style="display: none;">' + h + '</span>&nbsp;&nbsp;<a href="#" class="morelink1" style="display:inline-block;">' + moretext + '</a></span>';
+
+
+	        $(this).html(html);
+	    }
+
+	  });
+
+	};
+	showMoreLess();
+    $(".morelink1").click(function(){
+      if($(this).hasClass("less")) {
+          $(this).removeClass("less");
+          $(this).html(moretext);
+      } else {
+          $(this).addClass("less");
+          $(this).html(lesstext);
+      }
+      $(this).parent().prev().toggle();
+      $(this).prev().toggle();
+      return false;
+    });
+
 
 	$("#share-btn").click(function() {
 	    $("#share-icon").slideToggle("slow");
