5bd9cfef0cc7b42ccf56d5c4900836b7415db832
gauravchopra18 <chopra_gaurav18@yahoo.com>
2018-11-05 15:50:28 +0530

    imports_aligned:

--- scripts/welcome_call_report.py
+++ scripts/welcome_call_report.py
@@ -1,8 +1,15 @@
+#python imports
 import sys,os,django
 import csv
 from io import StringIO
-from django.utils import timezone
 from dateutil import relativedelta
+from datetime import datetime,timedelta
+from collections import OrderedDict
+import logging
+#django imports
+from django.utils import timezone
+from django.conf import settings
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "careerplus.config.settings")
 ROOT_FOLDER = os.path.realpath(os.path.dirname(__file__))
@@ -11,13 +18,10 @@ if ROOT_FOLDER not in sys.path:
     sys.path.insert(1, ROOT_FOLDER + '/')
 django.setup()
 
-from django.conf import settings
-
-from datetime import datetime,timedelta
+#internal imports
 from order.models import Order,WelcomeCallOperation
-from collections import OrderedDict
 from emailers.email import SendMail
-import logging
+
 
 
 def date_diff(date1,date2):
@@ -29,22 +33,23 @@ def date_timezone_convert(date=None):
     from pytz import timezone
     if not date:
         return 'N.A'
-    return date.astimezone(timezone('Asia/Calcutta'))
+    return date.astimezone(timezone(settings.TIME_ZONE))
 
 
 def main(argv):
     cur_datetime = timezone.now()
     cur_date_end = datetime(cur_datetime.year, cur_datetime.month \
         , cur_datetime.day, 23, 59, 59)
-    last7days = cur_datetime - timedelta(days=7)
+    #setting last_duration for a week
+    last_duration = cur_datetime - timedelta(days=7)
 
     if argv == 'monthly':
-        last7days = cur_datetime + relativedelta.relativedelta(months=-1)
+        #setting last_duration for a month
+        last_duration = cur_datetime + relativedelta.relativedelta(months=-1)
 
-    order_objects = Order.objects.filter(created__gte= last7days,status__in=[ 1, 2,3])
+    order_objects = Order.objects.filter(created__gte=last_duration,status__in=[1,2,3])
 
     try:
-        order_object={}
         send_dict = {}
         csvfile = StringIO()
         csvwriter = csv.writer(csvfile, delimiter=',', quotechar="'", \
@@ -53,39 +58,39 @@ def main(argv):
             'Welcome call Date','Welcome call Status','Welcome Call Status - Category',\
             'Welcome Call Status - SubCategory', 'Days difference'])
         def order_status(order):
-            row_one = OrderedDict()
-            row_one['Order ID'] = 'CP' + str(order.pk)
-            row_one['Order_payment_date'] = date_timezone_convert(order.payment_date)\
+            row = OrderedDict()
+            row['Order ID'] = 'CP' + str(order.pk)
+            row['Order_payment_date'] = date_timezone_convert(order.payment_date)\
                 .strftime('%m/%d/%Y %H:%M:%S')
-            row_one['Order_created_date'] = date_timezone_convert(order.created)\
+            row['Order_created_date'] = date_timezone_convert(order.created)\
                 .strftime('%m/%d/%Y %H:%M:%S')
-            return row_one
+            return row
         for order in order_objects:
-            row_one = OrderedDict()
+            row = OrderedDict()
             welc_objects = WelcomeCallOperation.objects.filter(order_id=order.id)\
                 .order_by('created')
             base_dict = order_status(order)
 
             for welc_obj in welc_objects:
-                row_one = OrderedDict(base_dict)
-                row_one['Welcome Call Date'] = date_timezone_convert(welc_obj.created)\
+                row = OrderedDict(base_dict)
+                row['Welcome Call Date'] = date_timezone_convert(welc_obj.created)\
                     .strftime('%m/%d/%Y %H:%M:%S')
                 ist_date_welcome = date_timezone_convert(welc_obj.created)
                 ist_date_order = date_timezone_convert(order.created)
-                row_one['Welcome Call Status'] = welc_obj.get_wc_status("N.A")
-                row_one['Welcome Call Status - Category'] = welc_obj.get_wc_cat("N.A")
-                row_one['Welcome Call Status - SubCategory'] = welc_obj.get_wc_sub_cat("N.A")
-                row_one['Days difference'] = date_diff(ist_date_welcome,ist_date_order)
-                csvwriter.writerow(row_one.values())
+                row['Welcome Call Status'] = welc_obj.get_wc_status("N.A")
+                row['Welcome Call Status - Category'] = welc_obj.get_wc_cat("N.A")
+                row['Welcome Call Status - SubCategory'] = welc_obj.get_wc_sub_cat("N.A")
+                row['Days difference'] = date_diff(ist_date_welcome,ist_date_order)
+                csvwriter.writerow(row.values())
 
             if not welc_objects:
-                row_one = OrderedDict(base_dict)
-                row_one['Welcome Call Status'] = "N.A"
-                row_one['Welcome Call Date'] = 'NA'
-                row_one['Welcome Call Status - Category'] = "N.A"
-                row_one['Welcome Call Status - SubCategory'] = "N.A"
-                row_one['Days difference'] = 'NA'
-                csvwriter.writerow(row_one.values())
+                row = OrderedDict(base_dict)
+                row['Welcome Call Status'] = "N.A"
+                row['Welcome Call Date'] = 'NA'
+                row['Welcome Call Status - Category'] = "N.A"
+                row['Welcome Call Status - SubCategory'] = "N.A"
+                row['Days difference'] = 'NA'
+                csvwriter.writerow(row.values())
         send_dict['subject'] = "Welcome call Report"
         send_dict['to'] = ["vishal.gupta@hindustantimes.com","purnima.ganguly@shine.com",\
             "vinod@shine.com","nishant.shukla@hindustantimes.com"]
