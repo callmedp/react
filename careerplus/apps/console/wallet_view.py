36fe0590f1df35262fb77296c5610db20e23c798
Ritesh Bisht <ritesh.bisht@hindustantimes.com>
2018-11-19 16:50:05 +0530

    Enhancement-34207 - Add added_by field in wallet transaction - Add permission for adding wallet points from console

--- careerplus/apps/console/wallet_view.py
+++ careerplus/apps/console/wallet_view.py
@@ -4,6 +4,7 @@ from wallet.models import (
     Wallet, RewardPoint, ECash,
     WalletTransaction, ECashTransaction, PointTransaction)
 from .wallet_form import Walletform
+from order.models import Order
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.http import HttpResponseForbidden
@@ -14,26 +15,20 @@ from django.utils import timezone
 from django.views.generic.edit import FormView
 from django.contrib import messages
 from django.contrib.auth.decorators import user_passes_test
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import permission_required
 
 
 
 
 
-
+@method_decorator(permission_required('wallet.console_add_remove_wallet_point', login_url='/console/login/'),  name='dispatch')
 class WalletView(FormView):
     template_name = 'console/wallet/walletrew.html'
     form_class = Walletform
     http_method_names = [u'get', u'post']
     model = Wallet
     success_url = "/console/wallet/"
-
-    def get(self, request, *args, **kwargs):
-        if request.user.is_superuser == True:
-            form_class = self.get_form_class()
-            form = self.get_form(form_class)
-            return self.render_to_response(self.get_context_data(form=form))
-        else:
-            return HttpResponseForbidden("YOU ARE NOT ALLOWED TO VIEW THIS PAGE")
 
     def get_context_data(self, **kwargs):
         context = super(self.__class__, self).get_context_data(**kwargs)
@@ -51,6 +46,7 @@ class WalletView(FormView):
             note = form.cleaned_data['notes']
             points = form.cleaned_data['point_value']
             action = form.cleaned_data['wallet_action']
+            order = form.cleaned_data.get('order', None)
             wal_obj = ""
 
             if owner:
@@ -68,6 +64,14 @@ class WalletView(FormView):
                     wal_obj = None
                 elif wal_obj == 1:
                     wal_obj = Wallet.objects.filter(owner_email=email)[0]
+            if order:
+                orders = Order.objects.filter(email=email, number=order)
+                if not orders.exists():
+                    messages.add_message(
+                        request, messages.ERROR,
+                        'Unable TO Find Order')
+                    return self.form_invalid(form)
+                order = orders[0]
 
             if wal_obj:
                 expiry = timezone.now() + datetime.timedelta(days=30)
@@ -80,7 +84,15 @@ class WalletView(FormView):
                         status=1
                     )
 
-                    wal_txn = wal_obj.wallettxn.create(txn_type=1, status=1, point_value=points)
+                    wal_txn = wal_obj.wallettxn.create(
+                        txn_type=1,
+                        status=1,
+                        point_value=points,
+                        added_by=request.user.id
+                    )
+                    if order:
+                        wal_txn.order = order
+                        wal_txn.save()
 
                     point_obj.wallettxn.create(
                         transaction=wal_txn,
@@ -99,7 +111,10 @@ class WalletView(FormView):
                     if wal_total < points:
                         points = wal_total
                     wallettxn = WalletTransaction.objects.create(wallet=wal_obj, txn_type=2, point_value=points,
-                                                                     notes=note)
+                                                                     notes=note, added_by=request.user.id)
+                    if order:
+                        wal_txn.order = order
+                        wal_txn.save()
                     for pts in rew_points:
                         if pts.current >= points:
                             pts.current -= points
