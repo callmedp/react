0084b5d313bd28d83ea3614a6a1f8dc3d378f4bd
Ritesh Bisht <ritesh.bisht@hindustantimes.com>
2018-12-18 17:03:01 +0530

    Fix location for GCPPrivateMediaStorage and GcpPrivateStaticStorage Update version of django, django storages and drf in common.txt

--- careerplus/apps/core/library/gcloud/custom_cloud_storage.py
+++ careerplus/apps/core/library/gcloud/custom_cloud_storage.py
@@ -24,7 +24,7 @@ class GCPStaticStorage(GoogleCloudStorageMixin):
     GCP storage backend that saves the files locally, too.
     """
     bucket_name = settings.GCP_STATIC_BUCKET
-    location = '/l/s/'
+    location = 'l/s/'
 
     def __init__(self, **settings):
         super(GCPStaticStorage, self).__init__(**settings)
@@ -46,7 +46,7 @@ class GCPMediaStorage(GoogleCloudStorageMixin):
     GCP storage backend for public media
     """
     bucket_name = settings.GS_BUCKET_NAME
-    location = '/l/m/'
+    location = 'l/m/'
 
     def url(self, name):
         return settings.MEDIA_URL + name
