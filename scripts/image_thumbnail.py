9329441fa2359399d21c46e77a1c8973a9867995
Admin <admin@Amanpreet-Singh.local>
2019-01-11 13:38:59 +0530

    changes: handled the image_thumbnail check of not generating duplicacy

--- scripts/image_thumbnail.py
+++ scripts/image_thumbnail.py
@@ -1,4 +1,4 @@
-import os, sys, django
+import os, sys, django, re
 
 #Settings Import
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "careerplus.config.settings")
@@ -10,6 +10,7 @@ if ROOT_FOLDER not in sys.path:
 
 django.setup()
 
+#third party imports
 from PIL import Image
 
 #Global Constants
@@ -18,15 +19,16 @@ IMAGE_EXTENSIONS = ('png', 'jpg', 'jpeg', 'PNG', 'JPG', 'JPEG',)
 if __name__=="__main__":
 
     for root, dirs, files in os.walk(ROOT_FOLDER):
+        regex_text = '-thumbnail'
         for file in files:
-            if not file.endswith(IMAGE_EXTENSIONS):
+            if not file.endswith(IMAGE_EXTENSIONS) or re.search(regex_text, file) is not None:
                 continue
             file_path = os.path.join(root, file)
             img_dir = file_path[:file_path.rindex('/')]
             file_extension = file.split(".")[-1]
             file_name = ".".join(file.split(".")[:-1])
             file_path_without_extension = os.path.join(root, file_name)
-            thumbnail_name = file_name + '-thumbnail.' + file_extension
+            thumbnail_name = '{}-thumbnail.{}'.format(file_name, file_extension)
             img = Image.open(file_path)
             img.thumbnail((100, 100))
             img.save(os.path.join(img_dir, thumbnail_name))
