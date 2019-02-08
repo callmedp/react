915b341b5aad1b2338060301dadad488f3d77bb6
admin <amanpreet1040@gmail.com>
2019-02-08 13:05:02 +0530

    : updated form in the detail screen

--- careerplus/static_core/react/src/js/components/UserInfo/Detail/detail.jsx
+++ careerplus/static_core/react/src/js/components/UserInfo/Detail/detail.jsx
@@ -3,7 +3,14 @@ import PropTypes from 'prop-types';
 import {connect} from "react-redux";
 import * as actions from '../../../store/userInfo/actions/index';
 import {Field, reduxForm} from 'redux-form';
-import {renderField, required, datePicker, renderSelect, renderTextArea} from '../../../fieldLevelValidationForm';
+import {
+    renderField,
+    required,
+    datePicker,
+    renderSelect,
+    renderTextArea,
+    phoneNumber
+} from '../../../fieldLevelValidationForm';
 
 export class Detail extends React.Component {
     constructor(props) {
@@ -13,66 +20,87 @@ export class Detail extends React.Component {
     render() {
         const {error, handleSubmit, pristine, reset, submitting} = this.props;
         return (
-            <div className={'Authentication-page'}>
-                <form onSubmit={handleSubmit}>
-                    <div className={'Text-spacing'}>
-                        <div>
-                            <Field type="date" name="date_of_birth" component={renderField} validate={required}
-                                   label="Date Of Birth:"/>
-                        </div>
+            <div className="container pr">
+                <header className="login-page-bg">
+                    <div className="login-bg-txt">
+                        <figure className="login-icon1"></figure>
+                        <strong>1 Lacs+</strong>
+                        Satisfied users
                     </div>
-                    <div className={'Text-spacing'}>
-                        <div>
-                            <Field type="text" name="location" component={renderField} validate={required}
-                                   label="Location :"/>
-                        </div>
+                    <div className="login-bg-txt">
+                        <figure className="login-icon2"></figure>
+                        <strong>300+</strong>
+                        Courses
                     </div>
-                    <div className={'Text-spacing'}>
-                        <div>
-                            <Field
-                                name="gender"
-                                component={renderSelect}
-                                validate={required}
-                                label="Gender:"
-                            >
-                                <option></option>
-                                <option name="Male" value="M">Male</option>
-                                <option name="Female" value="F">Female</option>
-                                <option name="Other" value="O">Other</option>
+                    <div className="login-bg-txt">
+                        <figure className="login-icon3"></figure>
+                        <strong>500+</strong>
+                        Professional resumes delivered
+                    </div>
+                </header>
 
-                            </Field>
+                <div className="register login-signup-box">
+                    <h1 className="modal-title">Add Your Information</h1>
+
+                    <form onSubmit={handleSubmit}>
+                        <div className={'Text-spacing'}>
+                            <div>
+                                <Field type="date" name="date_of_birth" component={renderField} validate={required}
+                                       label="Date Of Birth:"/>
+                            </div>
                         </div>
+                        <div className={'Text-spacing'}>
+                            <div>
+                                <Field type="text" name="location" component={renderField} validate={required}
+                                       label="Location :"/>
+                            </div>
+                        </div>
+                        <div className={'Text-spacing'}>
+                            <div>
+                                <Field
+                                    name="gender"
+                                    component={renderSelect}
+                                    validate={required}
+                                    label="Gender:"
+                                >
+                                    <option></option>
+                                    <option name="Male" value="M">Male</option>
+                                    <option name="Female" value="F">Female</option>
+                                    <option name="Other" value="O">Other</option>
+
+                                </Field>
+                            </div>
+                        </div>
+                        <div className={'Text-spacing'}>
+                            <div>
+                                <Field
+                                    name="extra_info"
+                                    component={renderTextArea}
+                                    type="text"
+                                    label="Extra Information:"
+                                />
+                            </div>
+                        </div>
+                        <div className={'Button-group'}>
+                            <div className={'Button-parent'}>
+                                <button className={'Submit-button'} onClick={() => {
+                                    this.props.history.goBack()
+                                }}>
+                                    Back
+                                </button>
+                            </div>
+                            <div className={'Button-parent'}>
+                                <button className={'Submit-button'} type="submit" disabled={pristine || submitting}>
+                                    Next
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                    {error && <div className={'Api-error'}>
+                        <span>{error}</span>
                     </div>
-                    <div className={'Text-spacing'}>
-                        <div>
-                            <Field
-                                name="extra_info"
-                                component={renderTextArea}
-                                type="text"
-                                label="Extra Information:"
-                            />
-                        </div>
-                    </div>
-                    <div className={'Button-group'}>
-                        <div className={'Button-parent'}>
-                            <button className={'Submit-button'} onClick={() => {
-                                this.props.history.goBack()
-                            }}>
-                                Back
-                            </button>
-                        </div>
-                        <div className={'Button-parent'}>
-                            <button className={'Submit-button'} type="submit" disabled={pristine || submitting}>
-                                Next
-                            </button>
-                        </div>
-                    </div>
-                </form>
-                {error && <div className={'Api-error'}>
-                    <span>{error}</span>
+                    }
                 </div>
-
-                }
             </div>
         );
     }
