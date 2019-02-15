7e48b368cc5fb435f2a30392cb6d56a0e042a40d
admin <amanpreet1040@gmail.com>
2019-02-15 16:09:13 +0530

    changes: deleted extra files

--- scripts/html_to_pdf.py
+++ scripts/html_to_pdf.py
@@ -21,17 +21,13 @@ if __name__ == '__main__':
     obj = InvoiceGenerate()
     pdf_file = obj.generate_pdf({"STATIC_URL": settings.STATIC_URL, "SITE_DOMAIN": settings.SITE_DOMAIN,
                                  "SITE_PROTOCOL": settings.SITE_PROTOCOL}, 'emailers/candidate/resume_test.html')
-
     file_path = os.path.join(os.path.dirname(__file__))
     file_name = 'test.pdf'
-
     pdf_file = SimpleUploadedFile(
         file_name, pdf_file,
         content_type='application/pdf')
-
     dest = open(
         file_path + file_name, 'wb')
-
     for chunk in pdf_file.chunks():
         dest.write(chunk)
     dest.close()
