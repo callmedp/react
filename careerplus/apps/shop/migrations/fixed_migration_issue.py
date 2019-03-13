c17cabd94df6823a80d82fab941db46f358f169b
GoRave <gaurav@Souravs-Mac-mini.local>
2019-03-13 15:13:42 +0530

    sub_category_migration_fixed1

--- careerplus/apps/shop/migrations/fixed_migration_issue.py
+++ careerplus/apps/shop/migrations/fixed_migration_issue.py
@@ -11,7 +11,7 @@ import shop.functions
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('shop', '0035_auto_20181026_1317   '),
+        ('shop', '0035_auto_20181026_1317'),
     ]
 
     operations = [
