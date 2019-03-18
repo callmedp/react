264991ce671edb5f86295c97eb4f92559c2998fc
admin <amanpreet1040@gmail.com>
2019-03-19 01:17:07 +0530

    feat: added conditional event clicks

--- resume-builder/src/components/Core/Editor/LeftSideBar/leftSideBar.jsx
+++ resume-builder/src/components/Core/Editor/LeftSideBar/leftSideBar.jsx
@@ -4,23 +4,46 @@ import Edit from '../Edit/edit.jsx'
 import Preview from '../Preview/preview.jsx'
 
 export default class LeftSideBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            edit: true
+        };
+        this.activateEditTab = this.activateEditTab.bind(this);
+        this.activatePreviewTab = this.activatePreviewTab.bind(this);
+    }
+
+    activateEditTab() {
+        this.setState({
+            edit: true
+        })
+
+    }
+
+    activatePreviewTab() {
+        this.setState({
+            edit: false
+        })
+    }
+
+
     render() {
+        const isEdit = this.state.edit;
         return (
             <section className="left-sidebar">
-            <ul className="tab-heading">
-                	<li>
-                		<span class="edit-icon"></span>
-                		<a href="#">Add/ Edit</a>
-                	</li>
-                	<li class="active">
-                		<span class="preview-icon"></span>
-                		<a href="#">Preview</a>
-                	</li>
+                <ul className="tab-heading">
+                    <li onClick={this.activateEditTab} className={isEdit ? "active" : ''}>
+                        <span class="edit-icon"></span>
+                        <a href="#">Add/ Edit</a>
+                    </li>
+                    <li onClick={this.activatePreviewTab} class={!isEdit ? "active" : ''}>
+                        <span class="preview-icon"></span>
+                        <a href="#">Preview</a>
+                    </li>
                 </ul>
-{/*                <Edit/>
-*/}
-                <Preview/>
-
+                {
+                    isEdit ? <Edit/> : <Preview/>
+                }
             </section>
         )
     }
