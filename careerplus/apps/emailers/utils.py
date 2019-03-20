8ebe1163d995607e2e85a9c85dab31b849c0f2f9
Ritesh Bisht <ritesh.bisht@hindustantimes.com>
2019-03-20 12:14:45 +0530

    priority applicant Fix get_featured_profile_data_for_candidate

--- careerplus/apps/emailers/utils.py
+++ careerplus/apps/emailers/utils.py
@@ -4,7 +4,7 @@ from order.models import Order
 
 
 def get_featured_profile_data_for_candidate(candidate_id, curr_order_item, feature=False):
-    others_feature_profile_items_sub_type_flow = ShineProfileData.objects.all()
+    feature_profile_items_sub_type_flow = list(ShineProfileData.objects.all().values_list('sub_type_flow',flat=True))
     current_sub_type_flow = ShineProfileData.objects.filter(sub_type_flow=curr_order_item.product.sub_type_flow)
     if feature:
         data = {"ShineCareerPlus": {'ec': [current_sub_type_flow.first().id]}}
@@ -13,9 +13,9 @@ def get_featured_profile_data_for_candidate(candidate_id, curr_order_item, featu
     other_featured_items = Order.objects.filter(
         candidate_id=candidate_id,
         orderitems__product__type_flow=5,
-        orderitems__product__sub_type_flow__in=others_feature_profile_items_sub_type_flow,
+        orderitems__product__sub_type_flow__in=feature_profile_items_sub_type_flow,
         orderitems__oi_status=28
-    ).exclude(order=curr_order_item.order)
+    ).exclude(id=curr_order_item.order.id)
     if other_featured_items.exists():
         other_active_sub_type_flow = list(set(other_featured_items.values_list('orderitems__product__sub_type_flow', flat=True)))
         other_feature_profile_items = list(ShineProfileData.objects.filter(sub_type_flow__in=other_active_sub_type_flow).values_list('id', flat=True))
