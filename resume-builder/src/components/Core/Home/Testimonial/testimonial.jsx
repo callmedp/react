2545843df1b330bc24f15632dedef6bb9eeb918d
admin <amanpreet1040@gmail.com>
2019-03-29 12:10:03 +0530

    added sub components for home page

---
+++ resume-builder/src/components/Core/Home/Testimonial/testimonial.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import './testimonial.scss'
+
+export default class Testimonial extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+
+    render() {
+        return (
+            <div>
+                Testimonial
+            </div>
+        )
+    }
+
+}
