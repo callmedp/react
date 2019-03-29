af5c006a5d1f5fd40bc0b42b4721aa959291b8e5
admin <amanpreet1040@gmail.com>
2019-03-29 11:07:56 +0530

    feat: top bar text and content changes according the props

--- resume-builder/src/components/Core/Editor/TopBar/topBar.jsx
+++ resume-builder/src/components/Core/Editor/TopBar/topBar.jsx
@@ -1,20 +1,52 @@
 import React, {Component} from 'react'
 import './topBar.scss'
+
 export default class TopBar extends Component {
+
+    constructor(props) {
+        super(props);
+
+    }
+
+
     render() {
+        const {page} = this.props;
         return (
             <section className="flex-container">
                 <div className="top-banner">
-                	<div className="top-banner--banner-txt">
-                		<h1>You are closer to your perfect resume.</h1>
-	                	<p>Fill the details to generate your resume </p>
-                	</div>
-                	<div className="top-banner--banner-right">
-                		<button className="white-button mr-20" href="#">Change template</button>
-                		<span className="top-banner--banner-right--banner-thumb">
-                			<img src="/media/static/react/assets/images/resume-thumb.jpg" alt="" />
-                		</span>
-                	</div>
+                    {
+                        page === 'buy' ?
+                            <div className="top-banner--banner-txt">
+                                <h1>Buy page.</h1>
+                                <p>Fill the details to generate your resume </p>
+                            </div> :
+                            page === 'download' ?
+                                <div className="top-banner--banner-txt">
+                                    <h1>Download Page.</h1>
+                                    <p>Fill the details to generate your resume </p>
+                                </div> :
+                                <div className="top-banner--banner-txt">
+                                    <h1>You are closer to your perfect resume.</h1>
+                                    <p>Fill the details to generate your resume </p>
+                                </div>
+                    }
+                    {
+                        page === 'download' ?
+                            <div className="top-banner--banner-right">
+                                <button className="white-button mr-20" href="#">Change template</button>
+                                <span className="top-banner--banner-right--banner-thumb">
+                			    <img src="/media/static/react/assets/images/resume-thumb.jpg" alt=""/>
+                		        </span>
+                            </div> :
+                            page === 'buy' ?
+                                '' :
+                                <div className="top-banner--banner-right">
+                                    <button className="white-button mr-20" href="#">Change template</button>
+                                    <span className="top-banner--banner-right--banner-thumb">
+                                    <img src="/media/static/react/assets/images/resume-thumb.jpg" alt=""/>
+                		            </span>
+                                </div>
+                    }
                 </div>
             </section>
         )
--- resume-builder/src/components/Core/Payment/Buy/buy.jsx
+++ resume-builder/src/components/Core/Payment/Buy/buy.jsx
@@ -6,8 +6,8 @@ import Footer from '../../../Common/Footer/footer.jsx'
 import {withRouter} from "react-router-dom";
 
 class Buy extends Component {
-    
-        
+
+
 
 
     render() {
@@ -18,7 +18,7 @@ class Buy extends Component {
             <div>
                 <Header/>
                 <div className="page-container">
-                    <TopBar/>
+                    <TopBar page={'buy'}/>
                     <section className={'flex-container mt-30'}>
 
                     	<section className="left-sidebar half-width pos-rel">
@@ -254,7 +254,7 @@ class Buy extends Component {
                     			</ul>
                     			<div className="">
                     				<div className="choose-plan--pay-price">
-                    					You pay 
+                    					You pay
                     					<span>Rs. <strong>999/-</strong></span>
                     				</div>
                     				<button className="choose-plan--orange-button-change orange-button items-right pull-right mt-10">Pay now</button>
