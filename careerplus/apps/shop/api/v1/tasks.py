dd06db2562a452f7a5f446bbe63515995e5333d1
admin <amanpreet1040@gmail.com>
2019-04-01 11:02:09 +0530

    fixed : shop product delete issue fixed

--- careerplus/apps/shop/api/v1/tasks.py
+++ careerplus/apps/shop/api/v1/tasks.py
@@ -4,7 +4,7 @@ from django.conf import settings
 
 
 @task(name='delete_products_from_solr')
-def delete_from_solr(self):
+def delete_from_solr():
     subprocess.call([settings.VENV_PATH,
                      os.paths.join(settings.CODE_PATH, "manage.py"), "index_products",
                      "--noinput", "--settings=careerplus.config.settings_staging"])
