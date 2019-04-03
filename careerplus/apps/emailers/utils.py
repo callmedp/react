c5e2b0630f00609f466b0c39ed12cdbf3c107af6
Ritesh Bisht <ritesh.bisht@hindustantimes.com>
2019-04-03 13:37:38 +0530

    make changes in get_featured_profile_data_for_candidate

--- careerplus/apps/emailers/utils.py
+++ careerplus/apps/emailers/utils.py
@@ -4,8 +4,10 @@ from order.models import OrderItem
 
 
 def get_featured_profile_data_for_candidate(candidate_id, curr_order_item, feature):
+
     feature_profile_items_sub_type_flow = list(ShineProfileData.objects.all().values_list('sub_type_flow',flat=True))
     current_sub_type_flow = ShineProfileData.objects.filter(sub_type_flow=curr_order_item.product.sub_type_flow)
+    current_ecs_value = list(current_sub_type_flow.values('id', 'priority_value'))
     if feature:
         data = {"ShineCareerPlus": {'ec': [current_sub_type_flow.first().id], 'xfr':1}}
     else:
@@ -16,10 +18,18 @@ def get_featured_profile_data_for_candidate(candidate_id, curr_order_item, featu
         product__sub_type_flow__in=feature_profile_items_sub_type_flow,
         oi_status=28
     ).exclude(id=curr_order_item.id)
+
     if other_featured_items.exists():
         other_active_sub_type_flow = list(set(other_featured_items.values_list('product__sub_type_flow', flat=True)))
-        other_feature_profile_items = list(ShineProfileData.objects.filter(sub_type_flow__in=other_active_sub_type_flow).values_list('id', flat=True))
-        data['ShineCareerPlus']['ec'].extend(other_feature_profile_items)
-    final_data = list(set(data['ShineCareerPlus']['ec']))
-    data['ShineCareerPlus']['ec'] = final_data
+        other_feature_profile_items = list(ShineProfileData.objects.filter(
+            sub_type_flow__in=other_active_sub_type_flow
+        ).order_by('-priority_value', '-id').values('id', 'priority_value'))
+        current_ecs_value.extend(other_feature_profile_items)
+        current_ecs_value = list(sorted(current_ecs_value, key=lambda x: x['priority_value'], reverse=True))
+        final_values = []
+        for val in current_ecs_value:
+            if val['id'] not in final_values:
+                final_values.append(val['id'])
+
+        data['ShineCareerPlus']['ec'] = final_values
     return data
