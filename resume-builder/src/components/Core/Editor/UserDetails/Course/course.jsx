a474a33624fc5e6e8df9eb1cf24e58384f2827db
admin <amanpreet1040@gmail.com>
2019-04-04 19:41:35 +0530

    changes done

--- resume-builder/src/components/Core/Editor/UserDetails/Course/course.jsx
+++ resume-builder/src/components/Core/Editor/UserDetails/Course/course.jsx
@@ -3,7 +3,7 @@ import './course.scss'
 import {Field, reduxForm} from "redux-form";
 import * as actions from "../../../../../store/course/actions";
 import {connect} from "react-redux";
-import {renderField, datepicker} from "../../../../FormHandler/formFieldRenderer";
+import {renderField, datepicker} from "../../../../FormHandler/formFieldRenderer.jsx";
 import moment from "moment";
 
 
