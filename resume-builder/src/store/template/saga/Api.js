ce6f92c4614bb97f3818e20bd2b7b38ff5bb74f6
admin <amanpreet1040@gmail.com>
2019-04-08 13:03:03 +0530

    changes: formatted code

--- resume-builder/src/store/template/saga/Api.js
+++ resume-builder/src/store/template/saga/Api.js
@@ -1,15 +1,9 @@
 import BaseApiService from '../../../services/BaseApiService'
 
-const fetchTemplate = (candidateId, templateId = 1) => {
+const fetchTemplate = (candidateId, templateId = 3) => {
 
     const url = `candidate/${candidateId}/preview/${templateId}/`;
     return BaseApiService.get(`http://127.0.0.1:8000/api/v1/resume/${url}`);
-    // // return {
-    // //     data: {
-    // //         "name": 'JavaScript',
-    // //         "proficiency": 7
-    // //     }
-    // }
 };
 
 export const Api = {
