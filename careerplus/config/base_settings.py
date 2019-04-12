6c072e1f083ef48e386be3cf7d56f357b5c919b1
GoRave <gaurav@Souravs-Mac-mini.local>
2019-04-12 13:35:58 +0530

    base_settings_added

--- careerplus/config/base_settings.py
+++ careerplus/config/base_settings.py
@@ -522,3 +522,13 @@ LOCAL_NETWORK_IPS = [ "172.16.64.80","125.19.44.195","124.124.86.138","115.112.3
     "182.64.252.176","125.17.83.97","220.225.255.161","118.102.181.219","220.227.36.202",
     "202.164.38.195","122.162.129.43","192.168.1.5","223.179.134.80","223.179.151.72",
     "122.162.42.142","171.79.76.124"]
+
+
+
+EXOITEL = {
+           'token': '9e4df38c0c3bd1009ca142da306d827e71e74737',
+           'sid': 'hindustantimes3',
+           'callerid': '08047105151',
+           'url': 'https://{sid}:{token}@api.exotel.com/v1/Accounts/{sid}/Calls/connect.json',
+           'record_url': 'https://{sid}:{token}@api.exotel.com/v1/Accounts/{sid}/Calls/{callid}.json'
+            }
