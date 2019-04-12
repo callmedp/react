0d97cd671feb5f1fd0682c3abfd986ae9197db01
GoRave <gaurav@Souravs-Mac-mini.local>
2019-04-12 14:34:11 +0530

    updated_exotelbtns

--- careerplus/static_core/console/js/order/exotelCall.js
+++ careerplus/static_core/console/js/order/exotelCall.js
@@ -1,4 +1,5 @@
 function makeCallBtn(id) {
+ $('#' + id + 'callingBtn').attr('disabled',true)
     if (id) {
         $('#' + id + 'callingBtn').css('color','orange');
         $.ajax({
@@ -10,9 +11,12 @@ function makeCallBtn(id) {
             success: function(response) {
                 if (response.status == 1) {
                     $('#' + id + 'callingBtn').css("color","green");
+                    $('#' + id + 'callingBtn').attr('disabled',true)
+
                      $('#' + id + 'callingBtn').attr('title',response.msg);
 
                 } else {
+                     $('#' + id + 'callingBtn').removeAttr('disabled')
                     $('#' + id + 'callingBtn').css('color','red');
                        $('#' + id + 'callingBtn').attr('title',response.msg);
 
