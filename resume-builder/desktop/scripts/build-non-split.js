29f564b88f1b6c95923ee0ec63ee66bae761b07e
admin <amanpreet1040@gmail.com>
2019-04-25 17:46:30 +0530

    build created

--- resume-builder/desktop/scripts/build-non-split.js
+++ resume-builder/desktop/scripts/build-non-split.js
@@ -1,6 +1,9 @@
 const rewire = require('rewire');
 const defaults = rewire('react-scripts/scripts/build.js');
 let config = defaults.__get__('config');
+var sass = require("node-sass");
+var sassUtils = require("node-sass-utils")(sass);
+
 
 config.optimization.splitChunks = {
     cacheGroups: {
@@ -14,3 +17,31 @@ config.optimization.runtimeChunk = false;
 config.output.filename = '../../../careerplus/static_core/react/dist/desktop/main.js';
 // CSS. "5" is MiniCssPlugin
 config.plugins[5].options.filename = '../../../careerplus/static_core/react/dist/desktop/main.css';
+
+
+console.log('-----config======', config.module.rules[2]['oneOf'][5]['use'][3]['options']);
+
+
+config.module.rules[2]['oneOf'][5]['use'][3]['options'] = {
+    ...config.module.rules[2]['oneOf'][5]['use'][3]['options'],
+    ...{
+        functions: {
+            "get($keys)": function (keys) {
+                console.log('---keys----', keys.getValue());
+                keys = keys.getValue().split(".");
+                console.log('--dot keys---', keys);
+                // let i;
+                // for (i = 0; i < keys.length; i++) {
+                //     result = result[keys[i]];
+                // }
+                // result = sassUtils.castToSass(result);
+                // return result;
+                let result = 'url(/media/static/react/assets/images/sprite1.svg)'
+                result = sassUtils.castToSass(result);
+
+                return result;
+
+            }
+        }
+    }
+}
