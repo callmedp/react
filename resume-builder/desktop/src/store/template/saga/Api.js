85365b6e39894d00e453b81cd008d62a541597e2
Amanpreet Singh <amanpreet1040@gmail.com>
2019-05-03 15:44:58 +0530

    added slick

--- resume-builder/desktop/src/store/template/saga/Api.js
+++ resume-builder/desktop/src/store/template/saga/Api.js
@@ -1,7 +1,7 @@
 import BaseApiService from '../../../services/BaseApiService'
 import {siteDomain} from "../../../Utils/domains";
 
-const fetchTemplate = (candidateId, templateId = 2) => {
+const fetchTemplate = (candidateId, templateId = 5) => {
 
     const url = `candidate/${candidateId}/preview/${templateId}/`;
     return BaseApiService.get(`${siteDomain}/api/v1/resume/${url}`);
