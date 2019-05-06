b53297e3eb8d546e2cd23a986836693380965592
Amanpreet Singh <amanpreet1040@gmail.com>
2019-05-06 11:03:39 +0530

    fixed: webpack config settings

--- resume-builder/desktop/src/components/FormHandler/validations/personalInfo/validate.js
+++ resume-builder/desktop/src/components/FormHandler/validations/personalInfo/validate.js
@@ -1,22 +1,21 @@
-export const required = value => value ? undefined : 'Required';
-
-export const email = value =>
-    value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)
-        ? 'Invalid email address'
-        : undefined;
-
-export const phoneNumber = value =>
-    value && !/^(0|[1-9][0-9]{9})$/i.test(value)
-        ? 'Invalid phone number, must be 10 digits'
-        : undefined;
-
-const validate = value => {
-    const errors = {}
-    errors['email'] = value && value.email && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value.email)
-        ? 'Invalid email address'
-        : undefined;
-    console.log('errros', errors);
-    return errors;
+const validate = values => {
+    const errors = {};
+    errors.first_name = !values || !values.first_name ? 'Required' : undefined;
+    errors.last_name = !values || !values.last_name ? 'Required' : undefined;
+    errors.date_of_birth = !values || !values.date_of_birth ? 'Required' : undefined;
+    if (!values || !values.number) {
+        errors.number = 'Required'
+    } else if (values.number && !/^(0|[1-9][0-9]{9})$/i.test(values.number)) {
+        errors.number = 'Invalid phone number, must be 10 digits'
+    }
+    if (!values || !values.email) {
+        errors.email = 'Required'
+    }
+    if (values.email && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        errors.email = 'Invalid email address'
+    }
+    console.log(errors)
+    return errors
 
 };
 
