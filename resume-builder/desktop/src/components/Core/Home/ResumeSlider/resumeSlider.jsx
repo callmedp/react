686734292c1b71869819a1c7443d88aa6dec9362
Hitesh Rexwal <hiteshrexwal03@gmail.com>
2019-05-10 12:37:28 +0530

    Slick Added

--- resume-builder/desktop/src/components/Core/Home/ResumeSlider/resumeSlider.jsx
+++ resume-builder/desktop/src/components/Core/Home/ResumeSlider/resumeSlider.jsx
@@ -9,7 +9,17 @@ export default class ResumeSlider extends Component {
     }
 
     render() {
+        // const settings = {
+        //     className: "center",
+        //     centerMode: true,
+        //     infinite: true,
+        //     centerPadding: "60px",
+        //     slidesToShow: 3,
+        //     speed: 500
+        // };
+
         const settings = {
+            dots: false,
             className: "center",
             centerMode: true,
             infinite: true,
@@ -17,14 +27,15 @@ export default class ResumeSlider extends Component {
             slidesToShow: 3,
             speed: 500
         };
+
         return (
             <section id="templates" className="section-container">
                 <h2>Proven resume templates</h2>
                 <strong className="section-container--sub-head">Choose from a library of classic templates and land a
                     new job</strong>
-                <ul >
+                {/* <ul >
                     <li>
-                        <img src={`${this.staticUrl}react/assets/images/resume-1.jpg`}/>
+                        
                     </li>
                     <li>
                         <img src={`${this.staticUrl}react/assets/images/resume-2.jpg`}/>
@@ -35,8 +46,21 @@ export default class ResumeSlider extends Component {
                     <li>
                         <img src={`${this.staticUrl}react/assets/images/resume-4.jpg`}/>
                     </li>
-                </ul>
+                </ul> */}
 
+                <Slider {...settings}>
+                    {[1,2,3,4].map((item,key)=>{
+                        return(
+                            <div>
+                                <img src={`${this.staticUrl}react/assets/images/resume-${item}.jpg`}/>
+                            </div>
+                        )
+                    })
+
+                    }
+                    
+                
+                </Slider>
                 {/*<Slider {...settings}>*/}
                     {/*{*/}
                         {/*[1, 2, 3, 4, 5, 6, 7, 8].map((el, ind) => {*/}
@@ -47,11 +71,11 @@ export default class ResumeSlider extends Component {
                         {/*})*/}
                     {/*}*/}
                 {/*</Slider>*/}
-                <ul className="slider">
+                {/* <ul className="slider">
                 <li><img onClick={() => this.props.history.push('/resume-builder/edit/')} alt={'Slider'}
                 src={`${this.staticUrl}react/assets/images/slider.jpg`}
-                className="img-responsive"/></li>
-                </ul>
+                className="img-responsive"/></li> */}
+                {/* </ul> */}
                 {/*<button className="orange-button orange-button--custom" onClick={() => this.scrollTo('templates')}>Customise*/}
                 {/*</button>*/}
             </section>
