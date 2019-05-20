d376085438cd558e9329ec3233ac46098f9db8e3
Amanpreet Singh <amanpreet1040@gmail.com>
2019-05-20 15:22:46 +0530

    made loader background fixed

--- resume-builder/desktop/src/components/Loader/loaderPage.jsx
+++ resume-builder/desktop/src/components/Loader/loaderPage.jsx
@@ -1,16 +1,23 @@
 import React, {Component} from 'react';
 import './loader.scss'
 
-export default class LoaderPage  extends Component {
+export default class LoaderPage extends Component {
     constructor(props) {
         super(props);
         this.staticUrl = window && window.config && window.config.staticUrl || '/media/static/'
+
+    }
+
+    componentWillUnmount() {
+        let body = document.body;
+        body.classList.remove('scrollynone');
     }
 
     render() {
+        let body = document.body;
+        body.classList.add('scrollynone')
         return (
             <div className="">
-
                 <div className="loader-page">
                     <span className="loader-img">
                         <img className="" src={`${this.staticUrl}react/assets/images/loader.png`}/><br/>
--- scripts/resume_pdf.py
+++ scripts/resume_pdf.py
@@ -16,7 +16,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "careerplus.config.settings_stag
 django.setup()
 from django.conf import settings
 from django.template.loader import get_template
-from resumebuilder.models import User
+# from resumebuilder.models import Candidate
 
 if __name__ == '__main__':
     # import ipdb;
@@ -42,4 +42,4 @@ if __name__ == '__main__':
     template = get_template('resume1.html')
     rendered_template = template.render({"is_pdf": True}).encode(encoding='UTF-8')
     print(rendered_template)
-    HTML(string=rendered_template).write_pdf('test2.pdf', stylesheets=[CSS(string='@page {size:A3; margin:0px}')])
+    HTML(string=rendered_template).write_png('test3.png', stylesheets=[CSS(string='@page {size:A3; margin:0px}')])
