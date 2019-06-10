e3c0a4f7adb8a320275094d835ab05ff1912534c
Animesh Sharma <sharma.animesh@hotmail.com>
2019-06-10 21:04:18 +0530

    Cart handling for new type flow

--- careerplus/apps/cart/views.py
+++ careerplus/apps/cart/views.py
@@ -54,7 +54,7 @@ class CartView(TemplateView, CartMixin, UserMixin):
             line_item = line_items_list[0];
             type_flow = int(line_item.product.type_flow)
         #  resume builder flow handle
-        if type_flow == 16:
+        if type_flow == 17:
             cart_dict = self.get_local_cart_items(cart_obj=cart_obj)
         else:
             cart_dict = self.get_solr_cart_items(cart_obj=cart_obj)
