480fd5706e701cda8e04a3d1da6dd39bbd578acc
Animesh Sharma <sharma.animesh@hotmail.com>
2019-06-20 15:25:51 +0530

    Image preview API for live update

--- careerplus/apps/resumebuilder/api/v1/views.py
+++ careerplus/apps/resumebuilder/api/v1/views.py
@@ -37,8 +37,7 @@ from rest_framework.parsers import (FormParser, MultiPartParser)
 from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from weasyprint import HTML, CSS
-from weasyprint import HTML, CSS
+import imgkit
 
 
 class CandidateCreateView(CreateAPIView):
@@ -751,11 +750,81 @@ class ResumeImagePreviewView(APIView):
 
     GET params supported - 
 
-    tsize - Get thumbnails (?tsize=200x200)
+    quality - Image quality (0-100)
     """
     authentication_classes = (ShineUserAuthentication,)
     permission_classes = (IsAuthenticated,)
     serializer_class = None
+
+    def get_image_base_64_encoded_data(self,candidate,template_no):
+        current_config = candidate.ordercustomisation_set.filter(template_no=template_no).first()
+        entity_position = current_config.entity_position_eval
+        entity_preference = eval(candidate.entity_preference_data)
+        extracurricular = candidate.extracurricular_list
+        education = candidate.candidateeducation_set.all().order_by('order')
+        experience = candidate.candidateexperience_set.all().order_by('order')
+        skills = candidate.skill_set.all().order_by('order')
+        achievements = candidate.candidateachievement_set.all().order_by('order')
+        references = candidate.candidatereference_set.all().order_by('order')
+        projects = candidate.candidateproject_set.all().order_by('order')
+        certifications = candidate.candidatecertification_set.all().order_by('order')
+        languages = candidate.candidatelanguage_set.all().order_by('order')
+        current_exp = experience.filter(is_working=True).order_by('-start_date').first()
+
+        entity_id_count_mapping = {
+                    2:bool(education.count()),
+                    3:bool(experience.count()),
+                    4:bool(projects.count()),
+                    5:bool(skills.count()),
+                    7:bool(achievements.count()),
+                    8:bool(certifications.count()),
+                    9:bool(languages.count()),
+                    10:bool(references.count()),
+                    11:bool(len(extracurricular)),
+                }
+        updated_entity_position = []
+
+        for item in entity_position:
+            item.update({"count":entity_id_count_mapping.get(item['entity_id'])})
+            updated_entity_position.append(item)
+
+        latest_experience, latest_end_date = '', None
+        for i in experience:
+            if i.is_working:
+                latest_end_date = date.today()
+                latest_experience = i.job_profile
+                break
+            elif latest_end_date == None:
+                latest_end_date = i.end_date
+                latest_experience = i.job_profile
+            else:
+                if latest_end_date < i.end_date:
+                    latest_end_date = i.end_date
+                    latest_experience = i.job_profile
+
+        template = get_template('resume{}_preview.html'.format(template_no))
+
+        rendered_template = template.render(
+            {'candidate': candidate, 'education': education, 'experience': experience, 'skills': skills,
+            'achievements': achievements, 'references': references, 'projects': projects,
+            'certifications': certifications, 'extracurricular': extracurricular, 'languages': languages,
+            'current_exp': current_exp, 'latest_exp': latest_experience,
+            'preference_list': entity_preference,'current_config': current_config,
+            'entity_position': updated_entity_position, 'width': 100, 'activate_water_mark': True
+            }).encode(encoding='UTF-8')
+
+        file_name = 'resumetemplate-' + str(template_no) + '.jpg'
+        rendered_template = rendered_template.decode()
+        rendered_template = rendered_template.replace("\n","")
+        options = {'quiet':'',
+                'quality':self.request.GET.get('quality',40),
+                'format':'JPG',
+                'disable-smart-width': '',
+                'disable-smart-height': '',
+                }
+
+        file_obj = imgkit.from_string(rendered_template,False,options=options)
+        return Response(base64.b64encode(file_obj))
 
     def get(self, request, *args, **kwargs):
         candidate_id = kwargs.get('candidate_id')
@@ -768,6 +837,8 @@ class ResumeImagePreviewView(APIView):
 
         if candidate_obj.candidate_id != request.user.id:
             return Response(status=status.HTTP_404_NOT_FOUND)
+
+        return self.get_image_base_64_encoded_data(candidate_obj,template_no)
 
         name_suffix = template_no
         split_tsize = tsize.split("x")
