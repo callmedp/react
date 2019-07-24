6725ead4d43a2dd51f4ea58622ecb32030269268
Brijesh <brijesh@Brijesh.local>
2019-07-24 16:26:43 +0530

    subscribe UI change

--- resume-builder/mobile/src/components/Core/Editor/RightSection/subscribe.jsx
+++ resume-builder/mobile/src/components/Core/Editor/RightSection/subscribe.jsx
@@ -8,7 +8,7 @@ export default class Subscribe extends Component{
             <div>
                 <div className="buildResume__subscribe">
                     <p className="buildResume__subscribe--text">Subscribe now create later</p>
-                    <a href="#" className="btn btn__round btn--outline">Subscribe</a>
+                    <a href="#" className="btn btn__sm btn__round btn--outline">Subscribe</a>
                 </div>
             </div>
         )
