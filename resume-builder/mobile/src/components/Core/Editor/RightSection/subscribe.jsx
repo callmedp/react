c83ccb540db6207c7f359b2239a489fca1bb897b
Brijesh <brijesh@Brijesh.local>
2019-07-25 10:11:45 +0530

    close button on subcribe

--- resume-builder/mobile/src/components/Core/Editor/RightSection/subscribe.jsx
+++ resume-builder/mobile/src/components/Core/Editor/RightSection/subscribe.jsx
@@ -9,6 +9,7 @@ export default class Subscribe extends Component{
                 <div className="buildResume__subscribe">
                     <p className="buildResume__subscribe--text">Subscribe now create later</p>
                     <a href="#" className="btn btn__sm btn__round btn--outline">Subscribe</a>
+                    <a className="close" href="javascript:void(0)">+</a>
                 </div>
             </div>
         )
