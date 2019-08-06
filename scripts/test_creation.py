9666466d781ad13e365e8e4bd93568c3773fc6ba
GoRave <gaurav@Gaurav-Mac-mini.local>
2019-08-06 14:46:54 +0530

    script_updated_field_updated

--- scripts/test_creation.py
+++ scripts/test_creation.py
@@ -1,10 +1,7 @@
 
 #python imports
 
-import sys,os,django,pandas
-
-#Settings imports
-
+import sys,os,django,pandas,json,logging
 
 #Settings imports
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "careerplus.config.settings")
@@ -23,24 +20,59 @@ from django.conf import settings
 from shop.models import Product
 from assessment.models import Test,Question
 
+ANSWER_MAPPING_DICT = {
+    'a': '1',
+    'b': '2',
+    'c': '3',
+    'd': '4',
+    1:   'a',
+    2:    'b',
+    3:    'c',
+    4:    'd',
+    'A': '1',
+    'B': '2',
+    'C': '3',
+    'D': '4',
+}
 
 if __name__ == "__main__":
-
     for file in os.listdir("/Users/gaurav/Desktop/20 files/"):
         product_id = file.split('_')[0]
-        print(product_id)
         if not product_id.isnumeric():
             continue
         prod = Product.objects.filter(id=product_id).first()
         if not prod:
+            logging.getLogger('info_log').info('No Product found for - {}'.format(product_id))
             continue
 
-)
         category = prod.category_main
         df = pandas.read_excel("/Users/gaurav/Desktop/20 files/"+file)
-        file_header = df.head(n=0)
+        logging.getLogger('info_log').info('Reading File  - {}'.format(file))
+        test_obj = Test.objects.create(category=category,product=prod,vendor=prod.vendor,\
+                                       duration=600,title=prod.name,is_active=True)
 
-        # test_obj = Test.objects.create(category=category,vendor=prod.vendor)
-        for key in file_header[1:len(file_header)-1]:
-            ques_op.update('')
-     Question.objects.create(question_text=df[key][index])
+        for question_array in df.get_values()[:df.values.size]:
+            question_object = Question.objects.create()
+            question_dict = {}
+            option_list = []
+            question_dict.update({'test':test_obj})
+            for index, value in enumerate(question_array[:-1]):
+                option_dict = {}
+                if index == 0:
+                    question_dict.update({'question_text': value})
+                    continue
+                if question_array[-1] == index or question_array[-1] == ANSWER_MAPPING_DICT.get(question_array[-1]):
+                    option_dict.update({'is_correct': 1})
+                else:
+                    option_dict.update({'is_correct': 0})
+                option_dict.update({'option': value, 'option_id': str(question_object.id) +
+                                    ANSWER_MAPPING_DICT.get(index), 'option_image': ''})
+                option_list.append(option_dict)
+            option_json = json.dumps(option_list)
+
+            question_dict.update({'question_options': option_json})
+            for key,value in question_dict.items():
+                setattr(question_object,key,value)
+            question_object.save()
+            print(test_obj.id)
+            logging.getLogger('info_log').info('Question Object Created - {}'.format(question_object.id))
