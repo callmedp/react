0dbb65974bcf542b7e8f087e0269cbd52d152065
Ritesh Bisht <ritesh.bisht@hindustantimes.com>
2019-08-15 14:03:29 +0530

    Fix Badging Logic - Get badging data first from shine and then operate on it

--- careerplus/apps/emailers/utils.py
+++ careerplus/apps/emailers/utils.py
@@ -1,64 +1,88 @@
+import bson
+import logging
 
 from shop.models import ShineProfileData
 from order.models import OrderItem
 from django.db.models import Q
+from pymongo import MongoClient
+from django.conf import settings
+from core.api_mixin import FeatureProfileUpdate
 
-def get_featured_profile_data_for_candidate(candidate_id, curr_order_item, feature):
-    feature_profile_items_sub_type_flow = list(ShineProfileData.objects.all().values_list('sub_type_flow',flat=True))
-    if curr_order_item.product.sub_type_flow in [1602]:
-        filter_kwargs = {
-            'vendor': curr_order_item.product.vendor,
-            'sub_type_flow': curr_order_item.product.sub_type_flow
+class BadgingMixin(object):
+    CANDIDATE_MONGO_PORT = settings.CANDIDATE_MONGO_PORT
+    CANDIDATE_MONGO_USERNAME = settings.CANDIDATE_MONGO_USERNAME
+    CANDIDATE_MONGO_PASSWORD = settings.CANDIDATE_MONGO_PASSWORD
+    CANDIDATE_MONGO_INSTANCE_STR = settings.CANDIDATE_MONGO_INSTANCE_STR
+    CANDIDATE_MONGO_DB = settings.CANDIDATE_MONGO_DB
+
+    def __init__(self):
+        connection_string = 'mongodb://{}:{}@{}/{}'.format(
+            self.CANDIDATE_MONGO_USERNAME, self.CANDIDATE_MONGO_PASSWORD,
+            self.CANDIDATE_MONGO_INSTANCE_STR, self.CANDIDATE_MONGO_DB
+        )
+        conn = MongoClient(connection_string)
+        database = conn['sumoplus']
+        self.coll = database['CandidateStatic']
+
+    def get_existing_badge_value(self, candidate_id):
+        candidate_id = bson.ObjectId(candidate_id)
+        candidate = self.coll.find_one({"_id": candidate_id}, {"scp": 1})
+
+        if candidate:
+            print(candidate)
+            return candidate.get('scp')
+
+
+    def get_badging_value_for_order(self, candidate_id, curr_order_item):
+        if curr_order_item.product.sub_type_flow in [1602]:
+            filter_kwargs = {
+                'vendor': curr_order_item.product.vendor,
+                'sub_type_flow': curr_order_item.product.sub_type_flow
+            }
+        else:
+            filter_kwargs = {
+                'sub_type_flow': curr_order_item.product.sub_type_flow
+            }
+        current_sub_type_flow = ShineProfileData.objects.filter(**filter_kwargs)
+        if current_sub_type_flow.first():
+            return current_sub_type_flow.first().id
+
+    def sort_as_per_priority(self, existing_badge_value):
+        ec_values = existing_badge_value.get('ec', [])
+        if ec_values:
+            sorted_ec_values = list(ShineProfileData.objects.filter(
+                id__in=ec_values
+            ).order_by('-priority_value').values_list('id', flat=True))
+            existing_badge_value['ec'] = sorted_ec_values
+        return existing_badge_value
+
+    def get_badging_data(self, candidate_id, curr_order_item=None, feature=False):
+        existing_badge_value = self.get_existing_badge_value(candidate_id)
+        if existing_badge_value is None:
+            return None
+        if candidate_id and curr_order_item:
+            new_value = self.get_badging_value_for_order(candidate_id, curr_order_item)
+            if feature:
+                existing_values = existing_badge_value.get('ec', [])
+                existing_values.append(new_value)
+                final_values = list(set(existing_values))
+                existing_badge_value['ec'] = final_values
+            else:
+                try:
+                    existing_badge_value.get('ec', []).remove(new_value)
+                except ValueError:
+                    logging.getLogger('error_log').error('unfeature values does not exist in ec')
+                    return None
+
+        existing_badge_value = self.sort_as_per_priority(existing_badge_value)
+        return existing_badge_value
+
+    def update_badging_data(self, candidate_id, data):
+        data = {
+            'ShineCareerPlus': data
         }
-    else:
-        filter_kwargs = {
-            'sub_type_flow': curr_order_item.product.sub_type_flow
-        }
-    current_sub_type_flow = ShineProfileData.objects.filter(**filter_kwargs)
-    if feature:
-        data = {"ShineCareerPlus": {'ec': [current_sub_type_flow.first().id]}}
-        current_ecs_value = list(current_sub_type_flow.values('id', 'priority_value'))
-    else:
-        data = {"ShineCareerPlus": {'ec': []}}
-        current_ecs_value = []
+        flag = FeatureProfileUpdate().update_feature_profile(
+            candidate_id=candidate_id, data=data
+        )
 
-    # Other features items includes badging for paid test which has certificates 
-    # assosciated with them.
-    other_featured_items = OrderItem.objects.filter(
-        Q(order__candidate_id=candidate_id,
-            product__type_flow=5,
-            product__sub_type_flow__in=feature_profile_items_sub_type_flow,
-            oi_status=28) |
-        Q(order__candidate_id=candidate_id,
-            product__type_flow=16,
-            product__sub_type_flow=1602,
-            product__sub_type_flow__in=feature_profile_items_sub_type_flow,
-            oi_status=4)
-    ).exclude(id=curr_order_item.id).exclude(user_certificate_orderitem=None, product__sub_type_flow=1602)
-
-    if other_featured_items.exists():
-        other_active_sub_type_flow = list(set(other_featured_items.values_list('product__sub_type_flow', flat=True)))
-        other_feature_profile_items = list(ShineProfileData.objects.filter(
-            sub_type_flow__in=other_active_sub_type_flow
-        ).order_by('-priority_value', '-id').values('id', 'priority_value'))
-        current_ecs_value.extend(other_feature_profile_items)
-        current_ecs_value = list(sorted(current_ecs_value, key=lambda x: x['id'], reverse=True))
-        current_ecs_value = list(sorted(current_ecs_value, key=lambda x: x['priority_value'], reverse=True))
-        final_values = []
-        for val in current_ecs_value:
-            if val['id'] not in final_values:
-                final_values.append(val['id'])
-
-        data['ShineCareerPlus']['ec'] = final_values
-    # get feature profile flow id
-    feature_profile_item_id = ShineProfileData.objects.filter(sub_type_flow=501).first().id
-    if feature:
-        if feature_profile_item_id in data['ShineCareerPlus']['ec']:
-            data['ShineCareerPlus']['xfr'] = 1
-    else:
-        if curr_order_item.product.sub_type_flow == 501 and feature_profile_item_id not in data['ShineCareerPlus']['ec'] :
-            data['ShineCareerPlus']['xfr'] = 0
-        elif feature_profile_item_id in data['ShineCareerPlus']['ec']:
-            data['ShineCareerPlus']['xfr'] = 1
-
-    return data
+        return flag
