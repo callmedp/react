83ad8cf9143b985d93ac498caa532e1b870028c2
Ritesh Bisht <ritesh.bisht@hindustantimes.com>
2019-09-03 11:34:12 +0530

    Fix mereg changes

--- careerplus/apps/shop/choices.py
+++ careerplus/apps/shop/choices.py
@@ -74,13 +74,8 @@ SUB_FLOWS = {
         (1602, "Paid Test")
     ),
     2: (
-<<<<<<< Updated upstream
-        (201, "Certificate Product")
-    )
-=======
         (201, "Certificate Product"),
     ),
->>>>>>> Stashed changes
 
 }
 
