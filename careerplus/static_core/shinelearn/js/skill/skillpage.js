174ee6d9416354a3e586c776f0e1b68a3e0a6359
Animesh Sharma <sharma.animesh@hotmail.com>
2019-09-12 16:22:45 +0530

    Json parsing added to try/catch block

--- careerplus/static_core/shinelearn/js/skill/skillpage.js
+++ careerplus/static_core/shinelearn/js/skill/skillpage.js
@@ -384,12 +384,15 @@ $(function() {
                                         </a>
                                     </p>
                                     <p class="pb-5 fs-11">`;
-                    if(data.results[d].pAsft){
+                    try{
                     var pAsft = JSON.parse(data.results[d].pAsft[0]);
                     if(pAsft["number_of_questions"]){
                         cert_detail += `No. of questions : <strong>${pAsft["number_of_questions"]}</strong> `;}
                     if(pAsft["test_duration"]){
                         cert_detail += `<span class="pl-5 pr-5">|</span> Duration: <strong>${pAsft["test_duration"]} mins</strong> `;}
+                    }
+                    catch(err) {
+
                     }
                     cert_detail += `</p>
                                     <p class="pb-0 fs-12">
@@ -468,7 +471,8 @@ $(function() {
                                                <span>${data.results[d].pPvn}</span>
                                             </div>
                                              <ul class="que-duration">`;
-                                            if(data.results[d].pAsft){
+                                            try{
+
                                             var pAsft = JSON.parse(data.results[d].pAsft[0]);
                                             if(pAsft["number_of_questions"]){
                                                 cert_detail +=`<li>No. of questions : <span>${pAsft["number_of_questions"]}</span></li>`;
@@ -477,6 +481,10 @@ $(function() {
                                                 cert_detail += `<li>Duration: <span>${pAsft["test_duration"]}mins</span></li>`;
                                             } 
                                             }
+                                            catch(err) {
+                                                
+                                            }
+
                                             cert_detail+=`</ul>
                                           <div class="pricing-box">
                                           <div class="pull-left">
