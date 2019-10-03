b3591f2aef1166ef8c02548322cfb320b25d988d
Amanpreet Singh <amanpreet1040@gmail.com>
2019-10-03 17:44:41 +0530

    changes: coupon and wallet authentication , permission changed to ShineUserAuthentication and IsAuthenticated

--- careerplus/apps/coupon/views.py
+++ careerplus/apps/coupon/views.py
@@ -9,7 +9,9 @@ from .models import Coupon, CouponUser
 from cart.mixins import CartMixin
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 import logging
-
+from shared.rest_addons.authentication import ShineUserAuthentication
+from rest_framework.permissions import IsAuthenticated
+from django.views.decorators.csrf import csrf_exempt
 
 class CsrfExemptSessionAuthentication(SessionAuthentication):
 
@@ -18,15 +20,18 @@ class CsrfExemptSessionAuthentication(SessionAuthentication):
 
 
 class CouponRedeemView(APIView, CartMixin):
-    permission_classes = (AllowAny,)
-    authentication_classes = (CsrfExemptSessionAuthentication, BasicAuthentication)
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (ShineUserAuthentication,)
 
+    @csrf_exempt
     def post(self, request, format=None):
         code = request.data.get('code')
+        candidate_id = request.user.candidate_id
+
         try:
             coupon = Coupon.objects.get(code=code)
         except Coupon.DoesNotExist:
-            logging.getLogger('error_log').error('This code is not valid.')
+            logging.getLogger('error_log').error('This code is not valid .')
             return Response(
                 {'success': 0,
                  'error': 'This code is not valid.'
@@ -35,7 +40,7 @@ class CouponRedeemView(APIView, CartMixin):
         if coupon.is_redeemed:
             return Response(
                 {'success': 0,
-                 'error': 'This code has already been used.'
+                 'error': 'This code has already been used for candidate id {0}'.format(candidate_id)
                  }, status=400, content_type='application/json')
         if not request.session.get('cart_pk'):
             self.getCartObject()
@@ -44,7 +49,7 @@ class CouponRedeemView(APIView, CartMixin):
         try:
             cart_obj = Cart.objects.select_related('coupon').get(pk=cart_pk)
         except Cart.DoesNotExist:
-            logging.getLogger('error_log').error('Something went wrong, Please login to continue.')
+            logging.getLogger('error_log').error('Something went wrong, Please login to continue for candidate id {0}'.format(candidate_id))
             return Response(
                 {'success': 0,
                  'error': 'Something went wrong, Please login to continue.'
@@ -52,7 +57,7 @@ class CouponRedeemView(APIView, CartMixin):
         wal_txn = cart_obj.wallettxn.filter(
             txn_type=2).order_by('-created').select_related('wallet')
         if wal_txn:
-            logging.getLogger('error_log').error('Points already applied!.')
+            logging.getLogger('error_log').error('Points already applied! for candidate id {0}'.format(candidate_id))
             return Response(
                 {'success': 0,
                  'error': 'Points already applied!.'
@@ -62,34 +67,34 @@ class CouponRedeemView(APIView, CartMixin):
         old_coupon = cart_obj.coupon
 
         if old_coupon:
-            logging.getLogger('error_log').error('Another coupon is already applied.')
+            logging.getLogger('error_log').error('Another coupon is already applied for candidate id {0}'.format(candidate_id))
             return Response(
                 {'success': 0,
                  'error': 'Another coupon is already applied.'
                  }, status=400, content_type='application/json')
         if not user_email:
-            logging.getLogger('error_log').error('Session Expired, Please login to continue.')
+            logging.getLogger('error_log').error('Session Expired, Please login to continue for candidate id {0}'.format(candidate_id))
             return Response(
                 {'success': 0,
                  'error': 'Session Expired, Please login to continue.'
                  }, status=400, content_type='application/json')
 
         if coupon.expired():
-            logging.getLogger('error_log').error('This code is expired.')
+            logging.getLogger('error_log').error('This code is expired for candidate id {0}'.format(candidate_id))
             return Response(
                 {'success': 0,
                  'error': 'This code is expired.'
                  }, status=400, content_type='application/json')
 
         if coupon.suspended():
-            logging.getLogger('error_log').error('This code is suspended.')
+            logging.getLogger('error_log').error('This code is suspended for candidate id {0}'.format(candidate_id))
             return Response(
                 {'success': 0,
                  'error': 'This code is suspended.'
                  }, status=400, content_type='application/json')
 
         if coupon.site not in [0, 1]:
-            logging.getLogger('error_log').error('This code is not valid.')
+            logging.getLogger('error_log').error('This code is not valid for candidate id {0}'.format(candidate_id))
             return Response(
                 {'success': 0,
                  'error': 'This code is not valid.'
@@ -110,7 +115,7 @@ class CouponRedeemView(APIView, CartMixin):
         try:
             user_coupon = coupon.users.get(user=user_email)
             if user_coupon.redeemed_at is not None:
-                logging.getLogger('error_log').error('This code has already been used by your account.')
+                logging.getLogger('error_log').error('This code has already been used by your account for candidate id {0}'.format(candidate_id))
                 return Response(
                     {'success': 0,
                      'error': 'This code has already been used by your account.'
@@ -119,13 +124,13 @@ class CouponRedeemView(APIView, CartMixin):
             if coupon.user_limit is not 0:  # zero means no limit of user count
                 # only user bound coupons left and you don't have one
                 if coupon.user_limit is coupon.users.filter(user__isnull=False).count():
-                    logging.getLogger('error_log').error('This code is not valid for your account.')
+                    logging.getLogger('error_log').error('This code is not valid for your account for candidate id {0}'.format(candidate_id))
                     return Response(
                         {'success': 0,
                          'error': 'This code is not valid for your account.'
                          }, status=400)
                 if coupon.user_limit is coupon.users.filter(redeemed_at__isnull=False).count():  # all coupons redeemed
-                    logging.getLogger('error_log').error('This code has already been used.')
+                    logging.getLogger('error_log').error('This code has already been used for candidate id {0}'.format(candidate_id))
                     return Response(
                         {'success': 0,
                          'error': 'This code has already been used.'
@@ -137,7 +142,7 @@ class CouponRedeemView(APIView, CartMixin):
                 total += li.product.get_price()
             if coupon.min_purchase:
                 if total < coupon.min_purchase:
-                    logging.getLogger('error_log').error('This cart total is below minimum purchase.')
+                    logging.getLogger('error_log').error('This cart total is below minimum purchase for candidate id {0}'.format(candidate_id))
                     return Response(
                         {'success': 0,
                          'error': 'This cart total is below minimum purchase.'
@@ -168,9 +173,10 @@ class CouponRedeemView(APIView, CartMixin):
 
 
 class CouponRemoveView(APIView, CartMixin):
-    permission_classes = (AllowAny,)
-    authentication_classes = (CsrfExemptSessionAuthentication, BasicAuthentication)
-
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (ShineUserAuthentication,)
+    
+    @csrf_exempt
     def post(self, request, format=None):
         try:
             if not request.session.get('cart_pk'):
