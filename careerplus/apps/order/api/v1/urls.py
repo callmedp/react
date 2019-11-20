10c6fdd066f4525de01c3a1a417d172449fcd865
GoRave <gaurav@Gaurav-Mac-mini.local>
2019-11-20 18:30:56 +0530

    url_fixed

--- careerplus/apps/order/api/v1/urls.py
+++ careerplus/apps/order/api/v1/urls.py
@@ -1,7 +1,7 @@
 #python imports
 
 # django imports
-from django.conf.urls import url, include
+from django.conf.urls import url
 
 # local imports
 from .views import (OrderItemViewSet,OrderItemsListView,OrderUpdateView,
@@ -15,21 +15,19 @@ from rest_framework import routers
 
 router = routers.SimpleRouter()
 
-router.register(r'orderitem', OrderItemViewSet)
+# router.register(r'orderitem', OrderItemViewSet)
 
 urlpatterns = router.urls
 
 urlpatterns = [
-    url(r'^orderitem/$', OrderItemViewSet),
     url(r'^order/(?P<pk>\d+)/items/$', OrderItemsListView.as_view()),
     url(r'^order/(?P<pk>\d+)/update/$', OrderUpdateView.as_view()),
-	url(r'orderitem/(?P<pk>\d+)/update/$',OrderItemUpdateView.as_view()),
+	url(r'^order/orderitem/(?P<pk>\d+)/update/$',OrderItemUpdateView.as_view()),
 	url(r'^order/order-item-operation/(?P<oi_id>\d+)/$',
 		OrderItemOperationApiView.as_view(),
 		name='orderitemoperations'),
 	url(r'^order/order-item/(?P<oi_id>\d+)/message/$',
 		MessageCommunicationListApiView.as_view(),
 		name='message-communications'),
-
     url(r'^ltv-report/(?P<year>\d+)/(?P<month>\d+)/$', LTVReportView.as_view()),
 ]
