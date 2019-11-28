8ac5ea09c8b98b851a9b2cbb36982c64230e61a3
Brijesh Rajput <brijeshrajput@brijesh.local>
2019-11-28 16:22:57 +0530

    trail offer widgets

--- resume-builder/mobile/src/components/Core/Payment/Buy/buy.jsx
+++ resume-builder/mobile/src/components/Core/Payment/Buy/buy.jsx
@@ -147,21 +147,20 @@ class Buy extends Component {
 
 
                 <div className="buy">
-                    <p className="buy--create">Use resume builder for 12 months
-                        to<strong> create/edit</strong> unlimited resume.</p>
-                        <div class="buy__wrap mt-15">
-                            <div class="buy__item buy__trial">
-                                <div class="buy__trial--tag">
-                                    <div className="buy__trial--tag__text">Trial offer</div>
-                                </div>
-                                <div>
-                                    <div className="buy__trial__download-text">Free one time download for 1st time users</div>
-                                    <div className="buy__trial__download-button">
-                                        <button class="btn btn__round btn--outline">Download</button>
-                                    </div>
-                                </div>
+                    <p className="buy--create">Use resume builder for 12 months to<strong> create/edit</strong> unlimited resume.</p>
+
+                    <div class="buy__wrap mt-15">
+                        <div className="buy__item buy__trial">
+                            <div className="buy__recommended--tag">Trial offer</div>
+                            <div className="buy_item--left">
+                                Free one time download for 1st time users
+                            </div>
+                            <div className="buy_item--right">
+                                    <button class="btn btn__round btn--outline">Download</button>
                             </div>
                         </div>
+                    </div>
+
                     <div className="buy__wrap mt-15">
                         <div className="buy__item">
                             <div className="buy__item--left">
@@ -245,8 +244,6 @@ class Buy extends Component {
                         </div>
                     </div>
                 </div>
-
-
             </div>
 
         )
