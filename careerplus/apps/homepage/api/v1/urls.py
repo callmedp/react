f650882ba0efa7db17c742ae5fe883430c77dfe1
GoRave <gaurav@Gaurav-Mac-mini.local>
2020-02-03 15:21:32 +0530

    updated_homepage

--- careerplus/apps/homepage/api/v1/urls.py
+++ careerplus/apps/homepage/api/v1/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from . import views
 app_name = "homepage"
 urlpatterns = [
-    re_path(r'^static-site-page/(?P<pk>\d+)/$',
+    re_path(r'^static-site-page/(?P<page_type>\d+)/$',
         views.StaticSiteView.as_view(), name='StaticSitePage'),
     
     re_path(r'^testimonial-category-map/$',
