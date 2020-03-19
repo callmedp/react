c001a8338e568bb7944e3fb7db08438472cf76d2
admin <admin@admins-MacBook-Pro-4.local>
2020-03-19 23:37:30 +0530

    Created mobile and desktop react app

---
+++ resume-score-checker/mobile/src/components/Core/Home/home.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+// import Banner from './Banner/banner.jsx'
+// import ResumeCheckerParameter from './ResumeCheckerParameters/resumeCheckerParameters.jsx'
+// import ResumeCheckerWorks from './ResumeCheckerWorks/resumeCheckerWorks.jsx'
+// import NextBenefit from './nextBenefit/nextBenefit.jsx';
+
+
+export default function Home(){
+    return (
+    <div>
+        <h2>Hello, React Mobile</h2>
+        {/* <Banner/>
+        <NextBenefit/>
+        <ResumeCheckerWorks/>
+        <ResumeCheckerParameter/> */}
+    </div>
+    );
+}
