c482825ce2107f05246eefd4c6af5b7c30fe3379
Brijesh Rajput <brijeshrajput@brijesh.local>
2020-03-24 20:00:21 +0530

    Get expert form

--- resume-score-checker/mobile/src/components/Core/GetExpertForm/getExpertForm.jsx
+++ resume-score-checker/mobile/src/components/Core/GetExpertForm/getExpertForm.jsx
@@ -7,41 +7,51 @@ export default function GetExpertForm() {
         <div className="getExpertForm">
             <div className="getExpertForm__header">
                 <span className="sprite back-icon"></span>
-                <strong>Get Help</strong>
+                <strong className="ml-15">Get Help</strong>
             </div>
 
             <div className="container-box">
+                <p className="getExpertForm__head">Fill the form below to get help</p>
                 <form action="" className="getExpertForm__form">
                     <ul>
-                        <li class="form-group input-effect">
-                            <input type="text" id="name" name="name" value="{{request.session.full_name}}" 
-                                class="form--control animation-input {% if request.session.full_name %}has-content{% endif %}" 
-                                placeholder=""></input>
-                            <label for="name">Name</label>
+                        <li class="form-group error">
+                            <div class="floating-label">      
+                                <input class="form--control floating-input" type="text" placeholder=" " />
+                                <label>Name</label>
+                            </div>
                             <span class="error--mgs">Name</span>
-                        </li>
+                        </li>   
 
-                        <li class="form-group d-flex">
-                            <div class="input-effect code">
-                                <select name="country_code" class="form-control" id="country-code">
-                                    
+                        <li class="form-group">
+                            <div class="floating-label">      
+                                <input class="form--control floating-input" type="text" placeholder=" " />
+                                <label>Email ID</label>
+                            </div>
+                        </li>                 
+
+                        <li class="getExpertForm__number form-group d-flex">
+                            <div class="code">
+                                <select class="form--control floating-select">
+                                    <option value="+91">+91</option>
+                                    <option value="+92">+92</option>
+                                    <option value="+2345">+2345</option>
+                                    <option value="+9834">+9834</option>
                                 </select>
                             </div>
-                            <div class="input-effect ml-auto number-w67 pr"> 
-                                <input type="text"  name="number"
-                                    class="form--control animation-input" 
-                                    placeholder=""></input>
-                                <label for="mobile-id">Mobile</label>
-                                <span class="error--mgs"></span>
+
+                            <div class="number"> 
+                                <div class="floating-label">      
+                                    <input class="form--control floating-input" type="text" placeholder=" " />
+                                    <label>Number</label>
+                                </div>
                             </div>
                         </li>
 
-                        <li class="form-group input-effect">
-                            <input type="email" name="email" id="email-id" value="{{request.session.email}}"   
-                                class="form--control animation-input {% if request.session.email %}has-content{% endif %}"
-                                aria-describedby="email-id" placeholder=""></input>
-                            <label for="email-id">Email id</label>
-                            <span class="error--mgs"></span>
+
+                        
+
+                        <li>
+                            <button className="btn btn-round-30 btn-blue w-100">Submit</button>
                         </li>
                     </ul>
                 </form>
