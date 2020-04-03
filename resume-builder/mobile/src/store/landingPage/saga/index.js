ed1bbb7237bab427a117aa89e80cf1318d0d7595
Amanpreet Singh <amanpreet1040@gmail.com>
2020-04-03 14:27:53 +0530

    changed meta title

--- resume-builder/mobile/src/store/landingPage/saga/index.js
+++ resume-builder/mobile/src/store/landingPage/saga/index.js
@@ -200,7 +200,7 @@ function* feedbackSubmit(action) {
 function* getComponentTitle(action) {
     try {
         let { payload: { resolve, reject } } = action;
-        resolve('Resume Builder 2019 | Online Free Resume Maker [Unique Templates] @ Shine Learning')
+        resolve('Resume Builder 2020 | Online Free Resume Maker [Unique Templates] @ Shine Learning')
     } catch (e) {
         console.log(e);
     }
