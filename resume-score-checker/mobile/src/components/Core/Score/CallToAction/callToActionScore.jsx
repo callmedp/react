69b2e04a6ad6494ec05315eeb1390f04bfa46c8b
Brijesh Rajput <brijeshrajput@brijesh.local>
2020-04-06 14:27:17 +0530

    Form UI bug fixed

--- resume-score-checker/mobile/src/components/Core/Score/CallToAction/callToActionScore.jsx
+++ resume-score-checker/mobile/src/components/Core/Score/CallToAction/callToActionScore.jsx
@@ -6,20 +6,22 @@ export default function CallToActionScore() {
     const [isVisible, setIsVisible] = useState(false);
     const toggle = () => setIsVisible(!isVisible)
     return (
-      <div className="call-to-action">
-            <div className="d-flex justify-content-between">
-               <button className="d-flex align-items-center file-upload btn btn-yellow btn-round-30 fs-11 mr-10 px-20" onClick={toggle}>
-                   <i className="sprite mr-5"></i>
-                           Get expert help
-                </button>
-      
-      
-                       <div className="d-flex align-items-center btn btn-outline-white btn-round-30 fs-11 px-20">
-                           <i className="sprite upload mr-5"></i> Upload new resume                            
-                           <input className="file-upload__input_left" type="file" name="file"></input>
-                       </div>
+        <div>
+            <div className="call-to-action">
+                <div className="d-flex justify-content-between">
+                    <button className="d-flex align-items-center file-upload btn btn-yellow btn-round-30 fs-11 mr-10 px-20" onClick={toggle}>
+                        <i className="sprite mr-5"></i>
+                        Get expert help
+                    </button>
+
+
+                    <div className="d-flex align-items-center btn btn-outline-white btn-round-30 fs-11 px-20">
+                        <i className="sprite upload mr-5"></i> Upload new resume                            
+                        <input className="file-upload__input_left" type="file" name="file"></input>
                     </div>
-        <GetExpertForm isVisible={isVisible} hide={toggle}/>
+                </div>
+            </div>
+            <GetExpertForm isVisible={isVisible} hide={toggle}/>
         </div>
     );
   };
