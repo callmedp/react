1cfb86c377891b751afabd4e737e9ef684166751
Admin <admin@Admins-MacBook-Pro-3.local>
2020-04-19 22:58:09 +0530

    Backend data commented

--- resume-score-checker/mobile/src/stores/scorePage/saga/Api.js
+++ resume-score-checker/mobile/src/stores/scorePage/saga/Api.js
@@ -5,12 +5,9 @@ const defaultHeaders = {}
 
 const fileUpload = (data) =>{
     const url = `resume-score-checker/`
-    // return BaseApiService.post(`${siteDomain}/resume/api/v1/${url}`, data, { defaultHeaders } , false, true);
-    return {
-        data :{'total_score': 32, 'section_score': [{'section_name': 'Format/Style', 'section_score': 2.0, 'section_total_score': 10, 'section_description': 'format style', 'section_status': 1}, {'section_name': 'Summary & Objective', 'section_score': 2.5, 'section_total_score': 10, 'section_description': 'summary', 'section_status': 1}, {'section_name': 'Education Detail', 'section_score': 5.0, 'section_total_score': 10, 'section_description': 'education', 'section_status': 2}, {'section_name': 'Work Experience', 'section_score': 6.0, 'section_total_score': 10, 'section_description': 'experience', 'section_status': 2}, {'section_name': 'Contact Detail', 'section_score': 10.0, 'section_total_score': 30, 'section_description': 'New Description', 'section_status': 1}, {'section_name': 'Skills', 'section_score': 7.0, 'section_total_score': 10, 'section_description': 'some description', 'section_status': 2}], 'error_message': ''}
-    }
-    // return{
-    //     data:{'error_message':"Sorry Please Uplaod New Resume"}
+    return BaseApiService.post(`${siteDomain}/resume/api/v1/${url}`, data, { defaultHeaders } , false, true);
+    // return {
+    //     data :{'total_score': 32, 'section_score': [{'section_name': 'Format/Style', 'section_score': 2.0, 'section_total_score': 10, 'section_description': 'format style', 'section_status': 1}, {'section_name': 'Summary & Objective', 'section_score': 2.5, 'section_total_score': 10, 'section_description': 'summary', 'section_status': 1}, {'section_name': 'Education Detail', 'section_score': 5.0, 'section_total_score': 10, 'section_description': 'education', 'section_status': 2}, {'section_name': 'Work Experience', 'section_score': 6.0, 'section_total_score': 10, 'section_description': 'experience', 'section_status': 2}, {'section_name': 'Contact Detail', 'section_score': 10.0, 'section_total_score': 30, 'section_description': 'New Description', 'section_status': 1}, {'section_name': 'Skills', 'section_score': 7.0, 'section_total_score': 10, 'section_description': 'some description', 'section_status': 2}], 'error_message': ''}
     // }
 }
 
