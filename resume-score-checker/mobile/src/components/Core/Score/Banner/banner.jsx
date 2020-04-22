4ce129c73bb1bfe2e4b377544590fbdc4092631a
Admin <admin@Admins-MacBook-Pro-3.local>
2020-04-22 16:50:33 +0530

    total_score changes to 100

--- resume-score-checker/mobile/src/components/Core/Score/Banner/banner.jsx
+++ resume-score-checker/mobile/src/components/Core/Score/Banner/banner.jsx
@@ -4,20 +4,16 @@ import './score-banner.scss';
 export default function Banner() {
     const localValue = JSON.parse(localStorage.getItem('resume_score'))
     const storeValue = localValue?.total_score
-    const store_section_value = localValue?.section_score
 
     const score = localStorage.getItem('resume_score') === null ? storeValue : localValue['total_score']
-    const reduced = (accumulator, currentValue) => accumulator + currentValue.section_total_score
-    const data_progess_value = score * 100 / store_section_value?.reduce(reduced, 0)
 
     const scoreBasedDescription = () => {
-        let scoreBased = data_progess_value
         switch(true){
-            case scoreBased > 80 :
+            case score > 80 :
                 return <p className="mt-15">Great Job! Your resume scores well as per the industry standards. Check out the detailed reviews</p>
-            case scoreBased >65 && score <= 80:
+            case score >65 && score <= 80:
                 return <p className="mt-15">Good Job! You are just few steps away from perfecting your resume. Check out the detailed reviews to improve the score. Score more to improve your chances of short listing</p>
-            case scoreBased >50 && score <=65:
+            case score >50 && score <=65:
                 return <p className="mt-15">Your resume score is average and can be improved a lot with quick fixes we have highlighted in the detailed review. You can also get expert assistance to perfect the score</p>
             default : {
                 return <p className="mt-15">Your resume score is low. It has room for lot of improvements. Check out the detailed reviews to improve the score or reach out to our experts to improve your resume</p>
@@ -29,11 +25,11 @@ export default function Banner() {
         <div>
             <div className="score-banner">
             <div className="container-box">
-                <h1 className="mb-10"><span>Hello {localStorage.getItem('userName') || "User"} <br/>Your resume scored {score} out of {store_section_value?.reduce(reduced, 0)}</span></h1>
+                <h1 className="mb-10"><span>Hello {localStorage.getItem('userName') || "User"} <br/>Your resume scored {score} out of 100</span></h1>
                 
                 <div className="score-banner__image">
                     <div className="score-banner__progressBar">
-                        <div className="ko-progress-circle" data-progress={ data_progess_value ? Math.round(data_progess_value) : 0}>
+                        <div className="ko-progress-circle" data-progress={ Math.round(score) }>
                         <div className="ko-progress-circle__text">
                             <strong>{score}</strong>
                             <p className="fs-12">Resume score</p>
