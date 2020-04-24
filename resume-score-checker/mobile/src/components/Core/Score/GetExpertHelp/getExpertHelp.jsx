e90f3d4b038a7a7530d2dda51810c6502cafcd41
Brijesh Rajput <brijeshrajput@brijesh.local>
2020-04-24 18:25:55 +0530

    UI Issue

--- resume-score-checker/mobile/src/components/Core/Score/GetExpertHelp/getExpertHelp.jsx
+++ resume-score-checker/mobile/src/components/Core/Score/GetExpertHelp/getExpertHelp.jsx
@@ -20,7 +20,7 @@ export default function GetExpertHelp() {
                 <h2><span>Get Expert Help</span></h2>
                 <p className="mt-15">Make your resume more compelling with the help of our professional resume writers, who has helped over 10,000 professional to get interviewed in many companies and get hired quickly. Our experts will not only help in improving your score but also help you in customizing your resume as per your desired job and industry.</p>
 
-                <ul className="get-expert-help__list">
+                <ul className="get-expert-help__list mt-30">
                     <li><a href = {`${siteDomain}/services/resume-writing/63/`} onClick={()=>handleEventClick('M_resumewriting','ExpertHelp')} style={{color:'grey'}}>Resume Writing</a></li>
                     <li><a href={`${siteDomain}/services/resume-services/entry-level-freshers/pd-2052`} onClick= {() =>handleEventClick('M_VisualResume','ExpertHelp')} style={{color:'grey'}}>Visual Resume</a></li>
                     <li><a href={`${siteDomain}/resume-builder/`} onClick= {() =>handleEventClick('M_Resume Builder','ExpertHelp')} style={{color:'grey'}}>Resume Builder</a></li>
