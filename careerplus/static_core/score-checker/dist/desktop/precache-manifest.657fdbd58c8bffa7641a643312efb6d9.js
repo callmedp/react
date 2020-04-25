33157bee48a89894d94f62d706062f910263c4fc
Amanpreet Singh <amanpreet1040@gmail.com>
2020-04-25 10:47:55 +0530

    fixes: mobile and destop fixed

---
+++ careerplus/static_core/score-checker/dist/desktop/precache-manifest.657fdbd58c8bffa7641a643312efb6d9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72e38116dd8cfb52e8bd9c1822ee2990",
+    "url": "https://learning-static-staging-189607.storage.googleapis.com/l1/s/score-checker/dist/desktop/index.html"
+  },
+  {
+    "revision": "55ae851e44f3e5b68d07",
+    "url": "https://learning-static-staging-189607.storage.googleapis.com/l1/s/score-checker/dist/desktop/main-1587791760572.css"
+  },
+  {
+    "revision": "55ae851e44f3e5b68d07",
+    "url": "https://learning-static-staging-189607.storage.googleapis.com/l1/s/score-checker/dist/desktop/main-1587791760572.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "https://learning-static-staging-189607.storage.googleapis.com/l1/s/score-checker/dist/desktop/main-1587791760572.js.LICENSE.txt"
+  }
+]);
