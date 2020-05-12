ce5fea2ea20e9357ece9bc14656849dd1e758ecc
Gaurav <gaurav@Gaurav-Mac-mini.local>
2020-05-12 18:46:16 +0530

    review updated

--- careerplus/apps/shop/search_indexes.py
+++ careerplus/apps/shop/search_indexes.py
@@ -1034,7 +1034,8 @@ class ProductIndex(indexes.SearchIndex, indexes.Indexable):
             return json.dumps(detail)
 
     def prepare_pRT(self, obj):
-        review = Review.objects.only('id', 'status', 'title', 'user_email', 'user_id', 'user_name','content','average_rating','created').filter(object_id=obj.id,status=1)
+        review = Review.objects.only('id', 'status', 'title', 'user_email', 'user_id', 'user_name','content',
+                                     'average_rating','created').filter(object_id=obj.id,status__in=[0,1])
         data = [{'id':rev.id,'status':rev.status,'title':rev.title ,'user_email':rev.user_email,'user_id':rev.user_id,
                  'user_name':rev.user_name,'content':rev.content,'average_rating':rev.average_rating,
                  'created':rev.created.strftime("%d %b, %Y"),'rating':rev.get_ratings()} for
