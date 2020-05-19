cdc0e0e1c1906db86868e51979704ce968f5ed2c
SahilSingla357 <Sahil.Singla@hindustantimes.com>
2020-05-20 01:08:20 +0530

    staging changes

---
+++ careerplus/static_core/score-checker/dist/desktop/precache-manifest.1a507d8f3ac08e3eda4d14a7c62aee90.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "910516178787f514ee6e20b7f169be8f",
+    "url": "https://learning-static-staging-189607.storage.googleapis.com/l1/s/score-checker/dist/desktop/index.html"
+  },
+  {
+    "revision": "c0f6449fb084d2d37494",
+    "url": "https://learning-static-staging-189607.storage.googleapis.com/l1/s/score-checker/dist/desktop/main-1589916472611.css"
+  },
+  {
+    "revision": "c0f6449fb084d2d37494",
+    "url": "https://learning-static-staging-189607.storage.googleapis.com/l1/s/score-checker/dist/desktop/main-1589916472611.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "https://learning-static-staging-189607.storage.googleapis.com/l1/s/score-checker/dist/desktop/main-1589916472611.js.LICENSE.txt"
+  }
+]);
--- careerplus/static_core/score-checker/dist/desktop/service-worker.js
+++ careerplus/static_core/score-checker/dist/desktop/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://learning-static-staging-189607.storage.googleapis.com/l1/s/score-checker/dist/desktop/precache-manifest.128c7eae5ab9f946a377cfac7dc45358.js"
+  "https://learning-static-staging-189607.storage.googleapis.com/l1/s/score-checker/dist/desktop/precache-manifest.1a507d8f3ac08e3eda4d14a7c62aee90.js"
 );
 
 self.addEventListener('message', (event) => {
