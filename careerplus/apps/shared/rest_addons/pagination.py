e81ad408013cfa693bdc0c8b58485fab54fca1d9
Gaurav <gaurav@Gaurav-Mac-mini.local>
2020-05-21 23:38:41 +0530

    pagination file updated to 10

--- careerplus/apps/shared/rest_addons/pagination.py
+++ careerplus/apps/shared/rest_addons/pagination.py
@@ -10,6 +10,6 @@
 from rest_framework.pagination import PageNumberPagination
 
 class LearningCustomPagination(PageNumberPagination):
-	page_size = 2
+	page_size = 10
 	page_size_query_param = 'page_size'
 	max_page_size = 100
