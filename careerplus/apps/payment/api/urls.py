e16197c86aab7034310eb6f33721d0a39c1063b0
Amanpreet Singh <amanpreet1040@gmail.com>
2020-06-02 23:30:43 +0530

    thank you page get api created

--- careerplus/apps/payment/api/urls.py
+++ careerplus/apps/payment/api/urls.py
@@ -2,21 +2,22 @@
 # from django.conf.urls import url
 from django.urls import re_path
 from . import views
+from .thankyou import ThankYouAPIView
 
 app_name = 'payment'
 
 urlpatterns = [
-	
-	re_path(r'^zest-money/emi-plans/$', views.ZestMoneyFetchEMIPlansApi.as_view(), name='zest-money-emi'),
+    re_path(r'^zest-money/emi-plans/$',
+            views.ZestMoneyFetchEMIPlansApi.as_view(), name='zest-money-emi'),
 
-	re_path(r'^payu/request/$', views.ResumeShinePayuRequestAPIView.as_view()),
+    re_path(r'^payu/request/$', views.ResumeShinePayuRequestAPIView.as_view()),
 
-	re_path(r'^payu/resume-response/(?P<type>success|cancel|failure)/$', views.ResumeShinePayuRequestAPIView.as_view()),
+    re_path(r'^payu/resume-response/(?P<type>success|cancel|failure)/$',
+            views.ResumeShinePayuRequestAPIView.as_view()),
 
-	re_path(r'^ccavenue/request/(?P<cart_id>[-\w]+)/(?P<paytype>[-\w]+)/$',
-			(views.Ccavenue.as_view())),
-	re_path(r'^epaylater/request/(?P<cart_id>[-\w]+)/$',
-			views.EPayLaterRequestView.as_view(), name="epaylater-request"),
-
-
+    re_path(r'^ccavenue/request/(?P<cart_id>[-\w]+)/(?P<paytype>[-\w]+)/$',
+            (views.Ccavenue.as_view())),
+    re_path(r'^epaylater/request/(?P<cart_id>[-\w]+)/$',
+            views.EPayLaterRequestView.as_view(), name="epaylater-request"),
+    re_path(r'^thankyou/$', ThankYouAPIView.as_view(), name="thank-you"),
 ]
