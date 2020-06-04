1337b279098f0dbd922a9a8c81d846f88ce156ef
Amanpreet Singh <amanpreet1040@gmail.com>
2020-06-05 01:47:48 +0530

    remove cart issue

--- careerplus/apps/cart/api/v1/removeFromCart.py
+++ careerplus/apps/cart/api/v1/removeFromCart.py
@@ -19,6 +19,7 @@ from cart.models import Cart
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from wallet.models import Wallet
 
 
 class RemoveFromCartAPIView(CartMixin, APIView):
