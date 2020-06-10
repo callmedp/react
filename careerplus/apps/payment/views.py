2bdc0fd5d8a67387b8ac989cb11bd6b018e934f1
SahilSingla357 <Sahil.Singla@hindustantimes.com>
2020-06-11 03:06:38 +0530

    bug shl-1066

--- careerplus/apps/payment/views.py
+++ careerplus/apps/payment/views.py
@@ -221,8 +221,10 @@ class PaymentOptionView(TemplateView, OrderMixin, PaymentMixin):
     def get_context_data(self, **kwargs):
         context = super(PaymentOptionView, self).get_context_data(**kwargs)
         payment_dict = self.getPayableAmount(cart_obj=self.cart_obj)
-        line_item = self.cart_obj.lineitems.filter(parent=None)[0]
-        type_flow = int(line_item.product.type_flow)
+        line_items = list(self.cart_obj.lineitems.filter(parent=None).values_list('product__type_flow'))
+        type_flow = 17
+        if 17 not in line_items:     
+            type_flow = int(line_items[0])
         # Fallback for cart object not being properly updated. TODO FIND SOURCE OF ISSUE
         email_id = self.cart_obj.owner_email or self.cart_obj.email or self.request.session.get('email','')
         first_name = self.cart_obj.first_name or self.request.session.get('first_name')
