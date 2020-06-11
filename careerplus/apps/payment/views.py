a6de6fd99f76e86c8333a6a6cb7e1bdb2d646f38
SahilSingla357 <Sahil.Singla@hindustantimes.com>
2020-06-11 19:58:45 +0530

    changes - tuple to list

--- careerplus/apps/payment/views.py
+++ careerplus/apps/payment/views.py
@@ -222,9 +222,10 @@ class PaymentOptionView(TemplateView, OrderMixin, PaymentMixin):
         context = super(PaymentOptionView, self).get_context_data(**kwargs)
         payment_dict = self.getPayableAmount(cart_obj=self.cart_obj)
         line_items = list(self.cart_obj.lineitems.filter(parent=None).values_list('product__type_flow'))
+        products_in_cart = [ item[0] for item in line_items]
         type_flow = 17
-        if 17 not in line_items:     
-            type_flow = int(line_items[0])
+        if 17 not in products_in_cart:     
+            type_flow = int(products_in_cart[0])
         # Fallback for cart object not being properly updated. TODO FIND SOURCE OF ISSUE
         email_id = self.cart_obj.owner_email or self.cart_obj.email or self.request.session.get('email','')
         first_name = self.cart_obj.first_name or self.request.session.get('first_name')
