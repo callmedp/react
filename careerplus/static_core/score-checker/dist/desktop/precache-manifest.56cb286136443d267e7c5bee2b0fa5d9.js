dcc9309880d52dfda84da7206c77d900a2121270
Gaurav_S <admin@Admins-MacBook-Pro-3.local>
2020-06-18 20:48:18 +0530

    build for desktop

---
+++ careerplus/static_core/score-checker/dist/desktop/precache-manifest.56cb286136443d267e7c5bee2b0fa5d9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5082794a9d8d4bc56538c948f3192fb0",
+    "url": "https://learning-static-staging-189607.storage.googleapis.com/l1/s/score-checker/dist/desktop/index.html"
+  },
+  {
+    "revision": "7b1f272f8e1bf217d866",
+    "url": "https://learning-static-staging-189607.storage.googleapis.com/l1/s/score-checker/dist/desktop/main-1592493433712.css"
+  },
+  {
+    "revision": "7b1f272f8e1bf217d866",
+    "url": "https://learning-static-staging-189607.storage.googleapis.com/l1/s/score-checker/dist/desktop/main-1592493433712.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "https://learning-static-staging-189607.storage.googleapis.com/l1/s/score-checker/dist/desktop/main-1592493433712.js.LICENSE.txt"
+  }
+]);
