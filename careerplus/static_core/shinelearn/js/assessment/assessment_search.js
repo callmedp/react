2dec9205743c694439b9b053d28ee612c6999d10
SahilSingla357 <Sahil.Singla@hindustantimes.com>
2020-07-13 15:04:48 +0530

    ss_SHLEARN_1022

--- careerplus/static_core/shinelearn/js/assessment/assessment_search.js
+++ careerplus/static_core/shinelearn/js/assessment/assessment_search.js
@@ -17,29 +17,78 @@ var tests = new Bloodhound({
   }
 });
 
+var test_fa = new Bloodhound({
+  datumTokenizer: function(test_fa) {
+      return Bloodhound.tokenizers.whitespace(test_fa.value);
+  },
+  queryTokenizer: Bloodhound.tokenizers.whitespace,
+  remote: {
+    url: "/api/get-category-level-products/?nopage=true&fl=id,title,slug&title=%QUERY&active=true&format=json",
+    wildcard: '%QUERY',
+    filter: function(responses) {
+      return $.map(responses, function(response){
+        return { 
+          title: response.title,
+          slug: response.slug
+        };
+      });
+    } 
+  }
+});
+
 // initialize the bloodhound suggestion engine
 tests.initialize();
+test_fa.initialize();
 
 // instantiate the typeahead UI
 $('#bloodhound .typeahead').typeahead(
-  { hint: true,
+  { 
+    hint: true,
     highlight: true,
     minLength: 1
   },
   {
-  displayKey: 'value',
-  source: tests.ttAdapter()
-});
+    name: 'practice_test',
+    source: tests.ttAdapter(),
+    displayKey: 'value',
+    limit: 4,
+    templates: {
+        header: '<h3 class="search_heading">Practice Tests</h3>'
+    }
+  },
+  {
+    name: 'functional_area',
+    source: test_fa.ttAdapter(),
+    displayKey: 'title',
+    limit: 4,
+    templates: {
+        header: '<h3 class="search_heading">Functional Area</h3>'
+    }
+  }
+
+//   {
+//   displayKey: 'value',
+//   source: tests.ttAdapter()
+// }
+);
 
 $('.typeahead').on('typeahead:selected', function(evt, item) {
- window.location = 'http://' + window.location.host +'/practice-tests/'+ item['slug'] +'-test/';
+  if(item['title']){
+    window.location = 'http://' + window.location.host +'/practice-tests/'+ item['slug'] +'/';}
+  else{
+    window.location = 'http://' + window.location.host +'/practice-tests/'+ item['slug'] +'-test/';
+  }
 })
 
 $('#bloodhound_submit').click(function() {
   api_url = "/api/get-test/?nopage=true&fl=id,category,title,slug&title="+$('#bloodhound_input').val()+"&active=true&format=json"
  $.ajax({url: api_url, success: function(result){
   if(result.length == 1){
-    window.location = 'http://' + window.location.host +'/practice-tests/'+ result[0]['slug'] +'-test/';
+    if(item['title']){
+      window.location = 'http://' + window.location.host +'/practice-tests/'+ result[0]['slug'] +'/';}
+    else{
+      window.location = 'http://' + window.location.host +'/practice-tests/'+ result[0]['slug'] +'-test/';
+    }
   }
   else{
     $("#bloodhound_message").show();
