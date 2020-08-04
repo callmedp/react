2a1a37976a671f44d3dec88d4b3be4c51b18e101
SahilSingla357 <Sahil.Singla@hindustantimes.com>
2020-08-04 18:25:55 +0530

    read more function fix

--- careerplus/apps/shop/views.py
+++ careerplus/apps/shop/views.py
@@ -1404,7 +1404,8 @@ class AnalyticsVidhyaProductView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(AnalyticsVidhyaProductView, self).get_context_data(**kwargs)
         context.update({
-            "campaign_slug" : "analytics_vidhya"
+            "campaign_slug" : "analytics_vidhya",
+            "initial_country" : "91"
         })
         return context
 
