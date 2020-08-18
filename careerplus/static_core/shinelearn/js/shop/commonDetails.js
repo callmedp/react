44bcdb6e0b28786d3d4e08347987e5650de564fb
Amanpreet Singh <amanpreet1040@gmail.com>
2020-08-18 15:08:09 +0530

    fixed: debugger removed

--- careerplus/static_core/shinelearn/js/shop/commonDetails.js
+++ careerplus/static_core/shinelearn/js/shop/commonDetails.js
@@ -12,14 +12,12 @@ const makeTrackingRequest = (loggingData) => {
 
 function handleTracking(action) {
     let loggingData = { t_id: trackingId, products: [productTrackingMappingId], action: action, 'position': 1, domain: 2, sub_product: trackingProductId };
-    debugger;
     if (trackingId && trackingProductId === productId){
         makeTrackingRequest(loggingData);
     }
 }
 
 function trackClickEvent() {
-    debugger;
     // exit product page handling 
     let loggingData = { t_id: trackingId, products: [productTrackingMappingId], action: 'exit_product_page', 'position': 1, domain: 2, sub_product: trackingProductId };
     if (trackingId && trackingProductId === productId ){
