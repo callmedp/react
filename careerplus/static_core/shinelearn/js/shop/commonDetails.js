4d2be03da9c55a4b378d7273d0ba25fb39a2e98e
Amanpreet Singh <amanpreet1040@gmail.com>
2020-09-08 15:01:49 +0530

    fix: tracking fixes

--- careerplus/static_core/shinelearn/js/shop/commonDetails.js
+++ careerplus/static_core/shinelearn/js/shop/commonDetails.js
@@ -11,7 +11,7 @@ const makeTrackingRequest = (loggingData) => {
 
 
 function handleTracking(action) {
-    let loggingData = { t_id: trackingId, products: [productTrackingMappingId], action: action, 'position': 1, domain: 2, sub_product: trackingProductId };
+    let loggingData = { t_id: trackingId, products: [productTrackingMappingId], action: action, 'position': parseInt(position), domain: 2, sub_product: trackingProductId, trigger_point: triggerPoint, u_id: uId, utm_campaign: utmCampaign};
     if (trackingId && trackingProductId === productId){
         makeTrackingRequest(loggingData);
     }
@@ -19,7 +19,7 @@ function handleTracking(action) {
 
 function trackClickEvent() {
     // exit product page handling 
-    let loggingData = { t_id: trackingId, products: [productTrackingMappingId], action: 'exit_product_page', 'position': 1, domain: 2, sub_product: trackingProductId };
+    let loggingData = { t_id: trackingId, products: [productTrackingMappingId], action: 'exit_product_page', 'position': 1, domain: 2, sub_product: trackingProductId, trigger_point: triggerPoint, u_id: uId, utm_campaign: utmCampaign};
     if (trackingId && trackingProductId === productId ){
         makeTrackingRequest(loggingData);
     }
