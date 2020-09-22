af99600d4d97514ae2d9e90b45482d19c8e2ab1e
Divyanshu Pandey <divyanshu.pandey@hindustantimes.com>
2020-09-22 15:03:31 +0530

    resume-score-checker redirection

--- careerplus/apps/resumescorechecker/views.py
+++ careerplus/apps/resumescorechecker/views.py
@@ -1,8 +1,11 @@
 from django.views.generic.base import TemplateView, View
-
+from django.http import HttpResponsePermanentRedirect
+from django.conf import settings
 
 class ScoreCheckerView(TemplateView):
-    template_name = 'resumescorechecker/index.html'
+    # template_name = 'resumescorechecker/index.html'
+    def get(self,request,**kwargs):
+        return HttpResponsePermanentRedirect("{}{}".format(settings.RESUME_SHINE_MAIN_DOMAIN, request.get_full_path()))
 
 
 class ScoreCheckerView2(TemplateView):
