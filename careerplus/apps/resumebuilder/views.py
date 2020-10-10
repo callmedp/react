6ad786d02c5b52dbbb4eb8dead394d46ba0ca882
aakanshamastwal <aakansha.mastwal92@gmail.com>
2020-10-10 23:00:21 +0530

    resume shine bug

--- careerplus/apps/resumebuilder/views.py
+++ careerplus/apps/resumebuilder/views.py
@@ -16,7 +16,8 @@ class WriteResumeView(TemplateView):
     # template_name = 'https://resumestage.shine.com/resume-builder'
 
     def get(self, request, **kwargs):
-        return HttpResponsePermanentRedirect("{}{}".format(settings.RESUME_SHINE_BUILDER_DOMAIN, request.get_full_path()))
+        print(request.get_full_path())
+        return HttpResponsePermanentRedirect("{}{}".format(settings.RESUME_SHINE_MAIN_DOMAIN, request.get_full_path()))
         # tracking_id = request.GET.get('t_id', '')
         # utm_campaign = request.GET.get('utm_campaign', '')
         # trigger_point = request.GET.get('trigger_point', '')
