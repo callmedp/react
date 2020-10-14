653c8d0ea505718881f5ce80be01da82d4afb998
aakanshamastwal <aakansha.mastwal92@gmail.com>
2020-10-14 14:38:48 +0530

    bugs fixes

--- resume-builder/desktop/src/App.js
+++ resume-builder/desktop/src/App.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 import AppRouter from './routes/index';
+import  { Redirect } from 'react-router-dom'
 
 class App extends Component {
     render() {
         return (
 
             // < AppRouter />
-            null
+            // null
+            <Redirect to='https://learning2.shine.com/resume-builder'  />
     )
         ;
     }
