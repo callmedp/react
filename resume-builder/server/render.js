d5274dae35348c592aabbcfbad5daf51bece32bb
aakanshamastwal <aakansha.mastwal92@gmail.com>
2020-10-15 22:12:07 +0530

    bugs solved

--- resume-builder/server/render.js
+++ resume-builder/server/render.js
@@ -11,23 +11,23 @@ export default (pathname, store, routes, context, timeStamp, staticUrl, isMobile
   
   const content = renderToString(< Provider
     store={store} >
-    < StaticRouter
+    {/* < StaticRouter
     location={pathname}
     context={context} >
     < div >
     {renderRoutes(routes)}
     </div>
-    </StaticRouter>
+    </StaticRouter> */}
     </Provider>);
     
     
-    const cssUrl = isMobile ? `${staticUrl}react/dist/mobile/main-${timeStamp}.css` : `${staticUrl}react/dist/desktop/main-${timeStamp}.css`;
-    const jsBuildUrl = isMobile ? `${staticUrl}react/dist/mobile/main-${timeStamp}.js` : `${staticUrl}react/dist/desktop/main-${timeStamp}.js`;
-    const viewPort = isMobile ? `width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no`
-    : "width=device-width, initial-scale=1, shrink-to-fit=no";
+    // const cssUrl = isMobile ? `${staticUrl}react/dist/mobile/main-${timeStamp}.css` : `${staticUrl}react/dist/desktop/main-${timeStamp}.css`;
+    // const jsBuildUrl = isMobile ? `${staticUrl}react/dist/mobile/main-${timeStamp}.js` : `${staticUrl}react/dist/desktop/main-${timeStamp}.js`;
+    // const viewPort = isMobile ? `width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no`
+    // : "width=device-width, initial-scale=1, shrink-to-fit=no";
     
-    //  const cssUrl = isMobile ? `dist/main-mobile.css` : `dist/main-desktop.css`,
-    //  jsBuildUrl = isMobile ? `dist/main-mobile.js` : `dist/main-desktop.js`;
+     const cssUrl = isMobile ? `dist/main-mobile.css` : `dist/main-desktop.css`,
+     jsBuildUrl = isMobile ? `dist/main-mobile.js` : `dist/main-desktop.js`;
     
     // Meta descriptions 
     
@@ -41,6 +41,7 @@ export default (pathname, store, routes, context, timeStamp, staticUrl, isMobile
     <title>${context.title}</title>
     <link type="text/css" href="${cssUrl}" rel="stylesheet" />
     <link rel="shortcut icon" href="${staticUrl}shinelearn/images/fav_icon.png"/>
+    <meta http-equiv="refresh" content="0;url=https://resumestage.shine.com/resume-builder">
     <meta name="viewport" content="${viewPort}"/>
     <meta name="description" content="Online Resume Builder, Make your resume with our easy-to-use templates & pro resume tips from experts. Select, Personalize/Customize any built-in resume templates for free download.">
     <meta property="og:title" content="${context.title}">
@@ -55,7 +56,6 @@ export default (pathname, store, routes, context, timeStamp, staticUrl, isMobile
     <meta itemprop="description" content="Online Resume Builder, Make your resume with our easy-to-use templates & pro resume tips from experts. Select, Personalize/Customize any built-in resume templates for free download.">
     <link rel="canonical" href="${siteDomain}${pathname}">
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
     integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf"
     crossorigin="anonymous">
@@ -72,12 +72,14 @@ export default (pathname, store, routes, context, timeStamp, staticUrl, isMobile
       "logo": "https://static1.shine.com/l/s/shinelearn/images/logo.png"
     }
     </script>
+
+    <script>
+    console.log('<><><>windows', window.location)
+    window.location.href = "https://resumestage.shine.com/resume-builder";
+    </script>
     </head>
     <body>
-    <div id="react-app">${content}</div>
     <script>
-    location.href = "https://resumestage.shine.com/resume-builder";
-
     window.__PRELOADED_STATE__ = ${JSON.stringify(store.getState()).replace(/</g, '\\u003c')}
     window.config = {}
     config.staticUrl = "${staticUrl}" 
