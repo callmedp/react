d2d05525b270df1b2c8f47fd8b303fd611bb744b
Rahul Singh <rahul.singh1@hindustantimes.com>
2020-10-22 18:36:14 +0530

    added: popup css & js

---
+++ careerplus/static_core/shinelearn/js/common/popup.js
@@ -0,0 +1,13 @@
+var modal = document.getElementById("popup_subscribe");
+var span = document.getElementsByClassName("close_popup_modal")[0];
+span.onclick = function() {
+    modal.style.display = "none";
+}
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+window.onload = function() {
+        modal.style.display = "block";
+}
