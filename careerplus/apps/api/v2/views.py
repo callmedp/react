673541c3ca7104a6b7464d39446e6ebf331142ea
Gaurav <gaurav@Gaurav-Mac-mini.local>
2020-10-29 14:15:04 +0530

    testprep updated

---
+++ careerplus/apps/api/v2/views.py
@@ -0,0 +1,71 @@
+
+from rest_framework.views import  APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from partner.models import  Vendor,Certificate
+from shop.models import Skill
+from partner.models import ProductSkill
+from django.utils.text import slugify
+from oauth2_provider.contrib.rest_framework import OAuth2Authentication
+from rest_framework.permissions import IsAuthenticated
+
+
+class VendorCertificateMappingApiView(APIView):
+    authentication_classes = [OAuth2Authentication]
+    permission_classes = [IsAuthenticated]
+    serializer_class = None
+    pagination_class = None
+
+    def get(self, request, *args, **kwargs):
+        new_skill_product = {}
+        vendors = self.request.GET.get('name','')
+        if not vendors:
+            return Response({'error':'Please provide the vendor'},status=status.HTTP_400_BAD_REQUEST)
+
+        vendors = vendors.split(',')
+        vendors = Vendor.objects.only('id','slug').filter(slug__in=vendors)
+        if not vendors.exists():
+            return Response({'error':'No vendor Found'},status=status.HTTP_400_BAD_REQUEST)
+        certificates = Certificate.objects.filter(vendor_provider__in=vendors)
+        if not certificates.exists():
+            return Response({'error':'No certificates found'},status=status.HTTP_400_BAD_REQUEST)
+        new_list = []
+        new_dict = {}
+        new_skill_dict = {}
+
+        for vendor in vendors:
+            new_dict.update({vendor.id:vendor.slug})
+
+        skills_names = certificates.values_list('skill',flat=True)
+        skills_names = ",".join(skills_names)
+        skills_names = list(map(lambda x: slugify(x), skills_names.split(',')))
+        skills = Skill.objects.filter(slug__in=skills_names)
+
+        for skill in skills:
+            new_skill_dict.update({skill.id:skill.slug})
+
+        product_skill = ProductSkill.objects.filter(skill_id__in=list(new_skill_dict.keys()),)
+        for ps in product_skill:
+            val = new_skill_product.get(new_skill_dict[ps.skill_id],[])
+            val.append(ps.product_id)
+            new_skill_product.update({new_skill_dict[ps.skill_id]: val})
+
+        for certificate in certificates:
+            skills = certificate.skill.split(',')
+            # skills = list(map(lambda x: slugify(x), certificate.skill.split(',')))
+            new_skill = []
+            for skill in skills:
+                new_skill.append({'name':skill,'product_id':new_skill_product.get(slugify(skill),[])})
+            new_list.append({'name':new_dict[certificate.vendor_provider_id],'certificate_set':[{
+                'name':certificate.name,'skill':new_skill}]})
+        return Response({'data':new_list},status=status.HTTP_200_OK)
+
+
+
+
+
+
+
+
+
