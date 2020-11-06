73415fc0af6f9ef7e2031fb8507dd8e2fbec164b
Rahul Singh <rahul.singh1@hindustantimes.com>
2020-11-06 12:23:27 +0530

    js mobile ga fixed

--- careerplus/static_core/shinelearn/js/mobile/shop/detail.js
+++ careerplus/static_core/shinelearn/js/mobile/shop/detail.js
@@ -186,7 +186,6 @@ function submit_feedback_form(is_logged_in) {
   if (is_logged_in == 'True') {
     var formData = $('#feedback-form').serialize();
     feedback_submit(formData);
-    MyGA.SendEvent('ln_review', 'ln_review', 'ln_submit_review', JSON.stringify(formData), '', false, true);
   }
   else {
     var flag = $('#feedback-form').valid();
