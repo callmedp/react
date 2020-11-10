2609ee1dd12aab8c6bebc94a1cd710f0e6373153
sachin <sachinchochi@gmail.com>
2020-11-10 16:04:20 +0530

    url bug

--- frontend/desktop/src/components/Core/SkillPage/skillPage.jsx
+++ frontend/desktop/src/components/Core/SkillPage/skillPage.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import SkillBanner from './Banner/banner';
 import AboutSection from './AboutSection/aboutSection';
+import WhoLearn from './WhoLearn/whoLearn';
+import '../SkillPage/skillPage.scss'
 
 const SkillPage = (props) => {
     return (
-        <AboutSection/>
+        <div>
+            <SkillBanner/>
+            <div className="col-sm-9">
+                <div className="ml-5">
+                    <AboutSection/>
+                    <WhoLearn/>
+                </div>
+            </div>
+            <div className="col-sm-3">
+            
+            </div>
+        </div>
     )
 }
 
