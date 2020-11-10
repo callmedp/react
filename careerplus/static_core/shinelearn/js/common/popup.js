27a6f437a63a28cccc228d4cea3e9d34ce7fff11
Rahul Singh <rahul.singh1@hindustantimes.com>
2020-11-10 15:29:06 +0530

    css issue & social sharing (1935)

--- careerplus/static_core/shinelearn/js/common/popup.js
+++ careerplus/static_core/shinelearn/js/common/popup.js
@@ -1,32 +1,10 @@
-function disableScroll() {
-    // Get the current page scroll position
-    scrollTop =
-      window.pageYOffset || document.documentElement.scrollTop;
-    scrollLeft =
-      window.pageXOffset || document.documentElement.scrollLeft,
-        window.onscroll = function() {
-            window.scrollTo(scrollLeft, scrollTop);
-        };
-}
-function enableScroll() {
-    window.onscroll = function() {};
-}
-
-var modal = document.getElementById("popup_subscribe");
 var span = document.getElementsByClassName("close_popup_modal")[0];
 span.onclick = function() {
-    modal.style.display = "none";
-    enableScroll();
+    $('#popup_subscribe').modal('toggle');
 }
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-        enableScroll();
-    }
-}
+
 $(document).ready(function() {
   setTimeout(function() {
-      modal.style.display = "block";
-      disableScroll();
+      $("#popup_subscribe").modal('show');
   }, 10000);
 });
