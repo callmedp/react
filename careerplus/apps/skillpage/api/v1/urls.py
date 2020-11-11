9d3b0076c0a9fd3a95eacd3bd3998d8ff7a2a876
shaziakaleem <shaziakaleem1616@gmail.com>
2020-11-11 10:58:01 +0530

    url skillpage: about api

--- careerplus/apps/skillpage/api/v1/urls.py
+++ careerplus/apps/skillpage/api/v1/urls.py
@@ -2,11 +2,11 @@
 from django.urls import re_path
 from django.conf import settings
 
-from .views import LoadMoreApiView
+from .views import LoadMoreApiView, SkillPageAbout
 app_name='skillpage'
 urlpatterns = [
     re_path(r'^v1/load-more/$',
         LoadMoreApiView.as_view(),
         name='load-more'),
-    # re_path('about',skill_page_about,name='about-skill')
+    re_path(r'^v1/about/$',SkillPageAbout.as_view(),name='skill-about')
 ]
