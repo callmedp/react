2269349d0788f66cfc016722f812f5541e1123c7
Rahul Singh <rahul.singh1@hindustantimes.com>
2020-11-12 19:04:44 +0530

    routes fixed

---
+++ careerplus/apps/crmapi/api/v1/urls.py
@@ -0,0 +1,14 @@
+# from django.conf.urls import url, include
+from django.urls import re_path, include
+
+#internal imports
+from .views import LeadManagementAPI
+
+#third party imports
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+app_name = 'console'
+urlpatterns = [
+    re_path(r'^lead-management/$', LeadManagementAPI.as_view())
+]
--- careerplus/apps/crmapi/urls.py
+++ careerplus/apps/crmapi/urls.py
@@ -1,6 +1,6 @@
 # from django.conf.urls import url
 
-from django.urls import re_path
+from django.urls import re_path, include
 from . import views
 
 app_name = 'crmapi'
@@ -9,4 +9,5 @@ urlpatterns = [
         views.LeadManagement.as_view(), name='lead-management'),
     re_path(r'^lead-save/$',
         views.LeadManagementWithCaptcha.as_view(), name='lead-management-captcha'),
+    re_path(r'^api/v1/', include('crmapi.api.urls', namespace='api')),
 ]
