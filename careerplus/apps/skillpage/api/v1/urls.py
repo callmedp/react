5aadc9e3a910362166d0c44ad447145560289915
SahilSingla357 <Sahil.Singla@hindustantimes.com>
2020-11-17 18:08:35 +0530

    changes made to shazia'a api

--- careerplus/apps/skillpage/api/v1/urls.py
+++ careerplus/apps/skillpage/api/v1/urls.py
@@ -3,12 +3,10 @@ from django.urls import re_path
 from django.conf import settings
 
 from .views import LoadMoreApiView, SkillPageAbout, CourseComponentView, DomainJobsView
-app_name='skillpage'
+app_name = 'skillpage'
 urlpatterns = [
-    re_path(r'^v1/load-more/$',
-        LoadMoreApiView.as_view(),
-        name='load-more'),
-    re_path(r'^v1/about/$',SkillPageAbout.as_view(),name='skill-about'),
-    re_path(r'^v1/courses/$',CourseComponentView.as_view(),name='courses-tray'),
-    re_path(r'^v1/domain-jobs/$',DomainJobsView.as_view(),name='domain-jobs')
+    re_path(r'^v1/load-more/$', LoadMoreApiView.as_view(), name='load-more'),
+    re_path(r'^v1/about/(?P<pk>\d+)/$', SkillPageAbout.as_view(), name='skill-about'),
+    re_path(r'^v1/courses/$', CourseComponentView.as_view(), name='courses-tray'),
+    re_path(r'^v1/domain-jobs/$', DomainJobsView.as_view(), name='domain-jobs')
 ]
