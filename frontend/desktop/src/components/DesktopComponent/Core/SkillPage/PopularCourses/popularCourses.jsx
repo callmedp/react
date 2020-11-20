eda0dd9641fdc277cc7cd7ac51202fba280ba29f
Gaurav_S <admin@Admins-MacBook-Pro-3.local>
2020-11-20 07:15:28 +0530

    populer courses frontend integration

--- frontend/desktop/src/components/DesktopComponent/Core/SkillPage/PopularCourses/popularCourses.jsx
+++ frontend/desktop/src/components/DesktopComponent/Core/SkillPage/PopularCourses/popularCourses.jsx
@@ -1,13 +1,46 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import './popularCourses.scss'
+import { fetchPopulerCourses } from 'store/SkillPage/PopularCourses/actions'
 
 const PopularCourses = (props) => {
+    const dispatch = useDispatch()
+    useEffect(() => {
+        dispatch(fetchPopulerCourses())
+    }, [])
+    const { pCourseList } = useSelector(store => store.popularCourses)
+
     return (
         <div className="popular-courses mt-40">
             <h2 className="heading2">Popular Courses</h2>
             <ul className="popular-courses__list">
-                <li>
+            {
+                pCourseList?.map((course) => {
+                    return (
+                        <>
+                            <li key={course.id}>
+                                <figure>
+                                    <img src={course.img} alt={course.alt} />
+                                </figure>
+                                <div className="links">
+                                    <Link to={course.url}>{course.name}</Link>
+                                    <span className="mr-10">By ERB</span>
+                                        <span className="rating">
+                                            { 
+                                                course.stars?.map(() => {
+                                                    return (<em className="icon-fullstar"></em>)
+                                                }) 
+                                            }
+                                            <span>{course.ratings}/5</span>
+                                        </span>
+                                </div>
+                            </li>
+                        </>
+                    )
+                })
+            }
+                {/* <li>
                     <figure>
                         <img src="https://static1.shine.com/l/m/product_image/3425/1542800087_8980.png" alt="Digital Marketing Training Course" />
                     </figure>
@@ -23,41 +56,7 @@ const PopularCourses = (props) => {
                                 <span>4/5</span>
                             </span>
                     </div>
-                </li>
-                <li>
-                    <figure>
-                        <img src="https://static1.shine.com/l/m/product_image/3425/1542800087_8980.png" alt="Digital Marketing Training Course" />
-                    </figure>
-                    <div className="links">
-                        <Link to={"#"}>Email Marketing Master Training Course</Link>
-                        <span className="mr-10">By ERB</span>
-                            <span className="rating">
-                                <em className="icon-fullstar"></em>
-                                <em className="icon-fullstar"></em>
-                                <em className="icon-fullstar"></em>
-                                <em className="icon-fullstar"></em>
-                                <em className="icon-blankstar"></em>
-                                <span>4/5</span>
-                            </span>
-                    </div>
-                </li>
-                <li>
-                    <figure>
-                        <img src="https://static1.shine.com/l/m/product_image/3425/1542800087_8980.png" alt="Digital Marketing Training Course" />
-                    </figure>
-                    <div className="links">
-                        <Link to={"#"}>Email Marketing Master Training Course</Link>
-                        <span className="mr-10">By ERB</span>
-                            <span className="rating">
-                                <em className="icon-fullstar"></em>
-                                <em className="icon-fullstar"></em>
-                                <em className="icon-fullstar"></em>
-                                <em className="icon-fullstar"></em>
-                                <em className="icon-blankstar"></em>
-                                <span>4/5</span>
-                            </span>
-                    </div>
-                </li>
+                </li> */}
             </ul>
             <Link className="view-all" to={"#"}>View all courses</Link>
         </div>
