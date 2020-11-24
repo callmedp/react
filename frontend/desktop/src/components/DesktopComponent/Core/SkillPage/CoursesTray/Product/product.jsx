2a87b7f1a440f798945d5059787a7240faf153f4
Divyanshu Pandey <divyanshu.pandey@hindustantimes.com>
2020-11-24 10:31:44 +0530

    CourseTray completed

--- frontend/desktop/src/components/DesktopComponent/Core/SkillPage/CoursesTray/Product/product.jsx
+++ frontend/desktop/src/components/DesktopComponent/Core/SkillPage/CoursesTray/Product/product.jsx
@@ -1,18 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import { siteDomain } from 'utils/domains';
-import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
-import PopOverDetail from '../PopOverDetail/popOverDetail';
+import CustomOverlay from 'services/CustomOverlay';
+import PopoverDetail from '../PopOverDetail/popoverDetail';
+
 
 const Product = (props) => {
 
     const { product: {
-                        newlyAdded, bestSeller,
-                        name, providerName,
-                        rating, mode,
-                        price, imgUrl,
-                        url
-                        },
-            index } = props
+        name, jobsAvailable,
+        providerName, rating, mode,
+        price, imgUrl, url,
+        tags, about, skillList,
+        highlights, type, level,
+        brochure, duration
+    },
+        index,
+        listSize,
+        listIdx } = props
 
     const [halfStar, setHalfStar] = useState(false)
 
@@ -26,43 +30,48 @@ const Product = (props) => {
     }, [rating])
 
     return (
-        <li className="col" key={index}>
-        <OverlayTrigger trigger={["hover", "focus"]} placement="left" overlay={<PopOverDetail/>} >
-            <div className="card">
-                <div className="card__heading">
-                    {newlyAdded && <span className="flag-blue">NEW</span>}
-                    {bestSeller && <span className="flag-red">BESTSELLER</span>}
-                    <figure>
-                        <img src={ imgUrl } alt={name} />
-                    </figure>
-                    <h3 className="heading3">
-                        <a href={`${siteDomain}${url}`} >{name}</a>
-                    </h3>
-                </div>
-                <div className="card__box">
-                    <div className="card__rating mt-5">
-                        <span className="mr-10">By {providerName}</span>
-                        <span className="rating">
 
-                            { Array(parseInt(rating)).fill().map((_,index) =>  <em key={index} className="icon-fullstar"></em>) }
-                            { halfStar && <em className="icon-halfstar"></em> }
-                            { Array(5-parseInt(rating)).fill().map((_,index) =>  <em key={index} className="icon-blankstar"></em>) }
-  
-                            <span>{rating}/5</span>
-                        </span>
+        <CustomOverlay
+            component={<PopoverDetail popoverData={{ about, skillList, highlights, jobsAvailable, url, type, level }} />}
+            placement={listSize === listIdx ? 'left' : 'right'}
+            onMouseEnter={() => { }}
+            delay={200}
+        >
+            <li className="col" key={index}>
+                <div className="card">
+                    <div className="card__heading">
+                        {tags === 2 && <span className="flag-blue">NEW</span>}
+                        {tags === 1 && <span className="flag-red">BESTSELLER</span>}
+                        <figure>
+                            <img src={imgUrl} alt={name} />
+                        </figure>
+                        <h3 className="heading3">
+                            <a href={`${siteDomain}${url}`} >{name}</a>
+                        </h3>
                     </div>
-                    <div className="card__duration-mode mt-10">
-                        Duration: <strong>90 days</strong>  |   Mode: <strong>{ mode }</strong>
-                    </div>
-                    <div className="card__price mt-30">
-    <strong>{price}/-</strong>
-                        <a href={`${siteDomain}${url}`} className="icon-pdf"></a>
+                    <div className="card__box">
+                        <div className="card__rating mt-5">
+                            <span className="mr-10">By {providerName}</span>
+                            <span className="rating">
+
+                                {Array(parseInt(rating)).fill().map((_, index) => <em key={index} className="icon-fullstar"></em>)}
+                                {halfStar && <em className="icon-halfstar"></em>}
+                                {Array(5 - parseInt(rating)).fill().map((_, index) => <em key={index} className="icon-blankstar"></em>)}
+
+                                <span>{rating}/5</span>
+                            </span>
+                        </div>
+                        <div className="card__duration-mode mt-10">
+                            Duration: <strong>{duration} days</strong>  |   Mode: <strong>{mode}</strong>
+                        </div>
+                        <div className="card__price mt-30">
+                            <strong>{price}/-</strong>
+                            <a href={brochure} className="icon-pdf"></a>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </OverlayTrigger>
-        </li>
+            </li>
+        </CustomOverlay>
     )
 }
-
 export default Product;
