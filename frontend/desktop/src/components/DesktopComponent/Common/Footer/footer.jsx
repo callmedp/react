f4330d7d604ba7a9031ed520a5e13e69a5ae52e9
Divyanshu Pandey <divyanshu.pandey@hindustantimes.com>
2020-12-08 09:04:07 +0530

    footer integrated

--- frontend/desktop/src/components/DesktopComponent/Common/Footer/footer.jsx
+++ frontend/desktop/src/components/DesktopComponent/Common/Footer/footer.jsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useEffect} from 'react';
 import { Link } from 'react-router-dom';
+import { siteDomain } from 'utils/domains';
 import './footer.scss';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchTrendingCnA } from 'store/Footer/actions/index';
 
 const Footer = (props) => {
+
+    const dispatch = useDispatch()
+    const { trendingSkills, trendingCourses } = useSelector( store => store.footer )
+
+    useEffect(() => {
+        dispatch(fetchTrendingCnA())
+    },[])
+
     return(
         <section className="container-fluid">
             <footer className="row">
@@ -10,33 +21,53 @@ const Footer = (props) => {
                     <div className="footer">
                         <ul className="footer-bdr">
                             <li>
-                                <Link to={"#"}>About Us</Link>
+                                <a href={`${siteDomain}/about-us`}>About Us</a>
                             </li>
                             <li>
-                                <Link to={"#"}>Partners</Link>
+                            <a href={`${siteDomain}/privacy-policy`}>Privacy Policy</a>
                             </li>
                             <li>
-                                <Link to={"#"}>Privacy Policy</Link>
+                            <a href={`${siteDomain}/tnc`}>Terms & Conditions</a>
                             </li>
                             <li>
-                                <Link to={"#"}>Terms & Condition</Link>
+                            <a href={`${siteDomain}/contact-us`}>Contact Us</a>
                             </li>
                             <li>
-                                <Link to={"#"}>Contact us</Link>
+                            <a href={`${siteDomain}/disclaimer`}>Disclaimer</a>
                             </li>
                             <li className="social-icon">
-                                <Link to={"#"} className="icon-facebook"></Link>
-                                <Link to={"#"} className="icon-linkedin"></Link>
-                                <Link to={"#"} className="icon-twitter"></Link>
+                                <a href="https://www.facebook.com/shinelearningdotcom/" className="icon-facebook"></a>
+                                <a  href="https://www.linkedin.com/showcase/13203963/" className="icon-linkedin"></a>
+                                <a href="https://twitter.com/shinelearning" className="icon-twitter"></a>
                             </li>
                         </ul>
                         <article className="">
                             <strong>TRENDING  COURSES</strong>
-                            <Link to={"#"}>DevOps Certification Training</Link> | <Link to={"#"}>AWS Architect Training</Link> | <Link to={"#"}>Big Data Hadoop</Link> | <Link to={"#"}>Training Tableau Training</Link> | <Link to={"#"}>Python Certification</Link> | <Link to={"#"}>Training for Data Science Selenium Training PMP® Certification</Link> | <Link to={"#"}>Training Robotic Process Automation Training</Link> | <Link to={"#"}>Spark and Scala Certification Training</Link> | <Link to={"#"}>Microsoft Power BI Training</Link> | <Link to={"#"}>Online Java Course Training Python Certification Course</Link> | <Link to={"#"}>Big Data Hadoop Certification Training Course</Link> | <Link to={"#"}>Data Science with Python Training Course</Link> | <Link to={"#"}>Machine Learning Certification Course</Link>
+                            {
+                                trendingCourses?.map((course, index) => {
+                                    return (
+                                        <React.Fragment key={index}>
+                                            <a href={`${siteDomain}/${course.url}`}>{course.name}</a>&nbsp;
+                                            { trendingCourses.length - 1 === index ? '' : '|'}
+                                        </React.Fragment>
+                                    )
+                                })
+                            }
+                            {/* <Link to={"#"}>DevOps Certification Training</Link> | <Link to={"#"}>AWS Architect Training</Link> | <Link to={"#"}>Big Data Hadoop</Link> | <Link to={"#"}>Training Tableau Training</Link> | <Link to={"#"}>Python Certification</Link> | <Link to={"#"}>Training for Data Science Selenium Training PMP® Certification</Link> | <Link to={"#"}>Training Robotic Process Automation Training</Link> | <Link to={"#"}>Spark and Scala Certification Training</Link> | <Link to={"#"}>Microsoft Power BI Training</Link> | <Link to={"#"}>Online Java Course Training Python Certification Course</Link> | <Link to={"#"}>Big Data Hadoop Certification Training Course</Link> | <Link to={"#"}>Data Science with Python Training Course</Link> | <Link to={"#"}>Machine Learning Certification Course</Link> */}
                         </article>
                         <article className="">
                             <strong>TRENDING  Skills</strong>
-                            <Link to={"#"}>Big Data Hadoop Certification Training Course</Link> | <Link to={"#"}>Data Science with Python Training Course</Link> | <Link to={"#"}>Machine Learning Certification Course</Link>
+                            {
+                                trendingSkills?.map((skill, index) => {
+                                    return (
+                                        <React.Fragment key={index}>
+                                            <a href={`${siteDomain}/${skill.url}`}>{skill.name}</a>&nbsp;
+                                            { trendingSkills.length - 1 === index ? '' : '|'}
+                                        </React.Fragment>
+                                    )
+                                })
+                            }
+                            {/* <Link to={"#"}>Big Data Hadoop Certification Training Course</Link> | <Link to={"#"}>Data Science with Python Training Course</Link> | <Link to={"#"}>Machine Learning Certification Course</Link> */}
                         </article>
                         <div className="footer-btm">
                             <div className="footer-btm__secure-payment">
