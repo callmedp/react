cf81252954fffa15447a669b9cc93c1fcc373f9a
Gaurav_S <admin@Admins-MacBook-Pro-3.local>
2020-12-21 15:14:35 +0530

    learning2 added in domain.js

--- frontend/src/utils/domains.js
+++ frontend/src/utils/domains.js
@@ -1,7 +1,7 @@
 // Please don't change any variable directy in here. If you wan't to change it, do with in the .env file.
 // For info on how to use .env file visit https://create-react-app.dev/docs/adding-custom-environment-variables/
 
-export const siteDomain = process.env.REACT_APP_SITE_DOMAIN || 'http://127.0.0.1:8000';
+export const siteDomain = process.env.REACT_APP_SITE_DOMAIN || 'https://learning2.shine.com';
 
 export const resumeShineSiteDomain = process.env.REACT_APP_RESUME_SHINE_SITE_DOMAIN || 'https://resume.shine.com';
 
