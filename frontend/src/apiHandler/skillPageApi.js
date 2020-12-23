e357c4504f192538968bc07ecc089ee2f6b56fcf
Divyanshu Pandey <divyanshu.pandey@hindustantimes.com>
2020-12-23 13:59:43 +0530

    courses for you resolved

--- frontend/src/apiHandler/skillPageApi.js
+++ frontend/src/apiHandler/skillPageApi.js
@@ -1,6 +1,7 @@
 import { fetchSkillPageBanner } from 'store/SkillPage/Banner/actions';
 import { fetchCoursesAndAssessments } from 'store/SkillPage/CoursesTray/actions/index';
 import { fetchDomainJobs } from 'store/SkillPage/DomainJobs/actions';
+import { fetchRecommendedProducts } from 'store/RecommendedCourses/actions/index';
 
 export const getSkillPageActions = (params) => {
   return [
@@ -15,6 +16,7 @@ export const getSkillPageActionsMobile = (params) => {
     { action: fetchSkillPageBanner, payload: { id: params?.id, 'medium': 1 } },
     { action: fetchCoursesAndAssessments, payload: { id: params?.id, 'medium': 1 } },
     { action: fetchDomainJobs, payload: { id: params?.id } },
+    { action: fetchRecommendedProducts, payload: {} },
   ]
 }
 
