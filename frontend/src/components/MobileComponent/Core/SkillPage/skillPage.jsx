95385560f0797b579bbeeb0319d245bb70336c60
Divyanshu Pandey <divyanshu.pandey@hindustantimes.com>
2020-12-24 18:01:57 +0530

    added comment

--- frontend/src/components/MobileComponent/Core/SkillPage/skillPage.jsx
+++ frontend/src/components/MobileComponent/Core/SkillPage/skillPage.jsx
@@ -45,6 +45,9 @@ const SkillPage = (props) => {
 
     useEffect(() => {
 
+        //You may notice that apis corresponding to these actions are not getting called on initial render.
+        //This is because initial render is done on node server, which is calling these apis, map the data and send it to the browser.
+        //So there is no need to fetch them again on the browser.
         if (!(window && window.config && window.config.isServerRendered)) {
             new Promise((resolve, reject) => dispatch(fetchSkillPageBanner({id : pageId, 'medium': 1, resolve, reject})))
             new Promise((resolve, reject) => dispatch(fetchCoursesAndAssessments({ id: pageId, 'medium': 1, resolve, reject})));
@@ -52,10 +55,10 @@ const SkillPage = (props) => {
             new Promise((resolve, reject) => dispatch(fetchRecommendedProducts({resolve, reject})))
         }
         else {
+            //isServerRendered is needed to be deleted because when routing is done through react and not on the node,
+            //above actions need to be dispatched.
             delete window.config?.isServerRendered
         }
-
-
 
     },[pageId])
 
