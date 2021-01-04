73cd087d45941283b1a9331991690294f5352261
Gaurav_S <admin@Admins-MacBook-Pro-3.local>
2021-01-04 12:52:51 +0530

    trending courses changes

--- frontend/src/components/MobileComponent/Core/CataloguePage/RecentCourses/recentCourses.jsx
+++ frontend/src/components/MobileComponent/Core/CataloguePage/RecentCourses/recentCourses.jsx
@@ -1,67 +1,17 @@
-import React, { Component }  from 'react';
-import { Link } from 'react-router-dom';
-import Slider from "react-slick";
+import React from 'react';
 // import 'slick-carousel/slick/slick.css';
 import './recentCourses.scss'
 import { useSelector } from 'react-redux';
-import { siteDomain } from 'utils/domains';
+import ProductCards from '../../../Common/ProductCardsSlider/productCardsSlider';
 
 const RecentCourses = (props) => {
-    const settings = {
-        dots: false,
-        arrows: false,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        swipeToSlide: true,
-        variableWidth: true,
-    };
     const { recentCoursesList } = useSelector(store => store.recentCourses);
-    
-    const starRatings = (star, index) => {
-        return (
-            star === '*' ? 
-                <em className="micon-fullstar" key={index}></em> : star === '+' ? 
-                <em className="micon-halfstar" key={index}></em> : 
-                <em className="micon-blankstar" key={index}></em>
-        )
-    }
 
     return(
         <section className="m-container m-lightblue-bg mt-0 mb-0" data-aos="fade-up">
             <div className="m-courses m-recent-courses">
                 <h2 className="m-heading2 text-center">Recently added courses</h2>
-                <Slider {...settings}>
-                {
-                    recentCoursesList?.slice(0,4)?.map((course, index) => {
-                        return (
-                            <div className="m-card" key={index}>
-                                <div className="m-card__heading">
-                                    <figure>
-                                        <img src={course.img} alt={course.img_alt} />
-                                    </figure>
-                                    <h3 className="m-heading3">
-                                        <a to={`${siteDomain}${course.url}`}>{course.name}</a>
-                                    </h3>
-                                </div>
-                                <div className="m-card__box">
-                                    <div className="m-card__rating">
-                                    <span className="mr-10">{course.provider}</span>
-                                    <span className="m-rating">
-                                        {course?.stars?.map((star, index) => starRatings(star, index))}
-                                        <span>{course?.rating?.toFixed(1)}/5</span>
-                                    </span>
-                                    </div>
-                                    <div className="m-card__price">
-                                        <strong>{course?.price}/-</strong> 
-                                    </div>
-                                </div>
-                            </div>
-                        )
-                    })
-                }
-                </Slider>
+                <ProductCards productList={recentCoursesList?.slice(0,4)} />
             </div>
         </section>
     )
