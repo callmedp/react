bef25387b5b4584b440e1d6e7977fddd15851384
shaziakaleem <shaziakaleem1616@gmail.com>
2021-01-04 10:57:50 +0530

    added cache

--- careerplus/apps/dashboard/api/v1/views.py
+++ careerplus/apps/dashboard/api/v1/views.py
@@ -6,6 +6,7 @@ from payment.models import PaymentTxn
 from order.models import Order, OrderItem
 from dashboard.api.v1.serializers import OrderSerializer,OrderItemSerializer
 from haystack.query import SearchQuerySet
+from django.core.cache import cache
 
 from django.conf import settings
 
@@ -18,39 +19,43 @@ class DashboardMyorderApi(DashboardInfo, APIView):
         order_list=[]
         if candidate_id:
         # candidate_id='5fed060d9cbeea482331ec4b'
-            orders = Order.objects.filter(
-            status__in=[0, 1, 3],
-            candidate_id=candidate_id)
+            if cache.get('dashboard_my_orders'):
+                order_list = cache.get('dashboard_my_orders')
+            else:
+                orders = Order.objects.filter(
+                status__in=[0, 1, 3],
+                candidate_id=candidate_id)
 
-            excl_txns = PaymentTxn.objects.filter(
-                status__in=[0, 2, 3, 4, 5],
-                payment_mode__in=[6, 7],
-                order__candidate_id=candidate_id)
-            # excl_txns = PaymentTxn.objects.filter(status=0, ).exclude(payment_mode__in=[1, 4])
-            excl_order_list = excl_txns.all().values_list('order_id', flat=True)
+                excl_txns = PaymentTxn.objects.filter(
+                    status__in=[0, 2, 3, 4, 5],
+                    payment_mode__in=[6, 7],
+                    order__candidate_id=candidate_id)
+                # excl_txns = PaymentTxn.objects.filter(status=0, ).exclude(payment_mode__in=[1, 4])
+                excl_order_list = excl_txns.all().values_list('order_id', flat=True)
 
-            orders = orders.exclude(
-                id__in=excl_order_list).order_by('-date_placed')
+                orders = orders.exclude(
+                    id__in=excl_order_list).order_by('-date_placed')
 
-            order_list = []
-            for obj in orders:
-                orderitems = OrderItem.objects.select_related(
-                    'product').filter(no_process=False, order=obj)
-                product_type_flow = None
-                product_id = None
-                item_count = len(orderitems)
-                if item_count > 0:
-                    item_order = orderitems[0]
-                    product_type_flow = item_order and item_order.product_id and item_order.product.type_flow or 0
-                    product_id = item_order and item_order.product_id
-                data = {
-                    "order": OrderSerializer(obj).data,
-                    "item_count": item_count,
-                    'product_type_flow': product_type_flow,
-                    "product_id": product_id,
-                    "orderitems": OrderItemSerializer(orderitems).data,
-                }
-                order_list.append(data)
+                order_list = []
+                for obj in orders:
+                    orderitems = OrderItem.objects.select_related(
+                        'product').filter(no_process=False, order=obj)
+                    product_type_flow = None
+                    product_id = None
+                    item_count = len(orderitems)
+                    if item_count > 0:
+                        item_order = orderitems[0]
+                        product_type_flow = item_order and item_order.product_id and item_order.product.type_flow or 0
+                        product_id = item_order and item_order.product_id
+                    data = {
+                        "order": OrderSerializer(obj).data,
+                        "item_count": item_count,
+                        'product_type_flow': product_type_flow,
+                        "product_id": product_id,
+                        "orderitems": OrderItemSerializer(orderitems).data,
+                    }
+                    order_list.append(data)
+                    cache.set('dashboard_my_orders',order_list,86400)
         return Response(order_list,status=status.HTTP_200_OK)
 
 class MyCoursesApi(DashboardInfo, APIView):
@@ -61,28 +66,32 @@ class MyCoursesApi(DashboardInfo, APIView):
         candidate_id = self.request.session.get('candidate_id', None)
         data = []
         if candidate_id:
-            orders = Order.objects.filter(
-                status__in=[0, 1, 3],
-                candidate_id=candidate_id)
+            if cache.get('dashboard_my_courses'):
+                data = cache.get('dashboard_my_courses')
+            else:
+                orders = Order.objects.filter(
+                    status__in=[0, 1, 3],
+                    candidate_id=candidate_id)
+                    
+                excl_txns = PaymentTxn.objects.filter(
+                    status__in=[0, 2, 3, 4, 5],
+                    payment_mode__in=[6, 7],
+                    order__candidate_id=candidate_id)
+                # excl_txns = PaymentTxn.objects.filter(status=0, ).exclude(payment_mode__in=[1, 4])
+                excl_order_list = excl_txns.all().values_list('order_id', flat=True)
 
-            excl_txns = PaymentTxn.objects.filter(
-                status__in=[0, 2, 3, 4, 5],
-                payment_mode__in=[6, 7],
-                order__candidate_id=candidate_id)
-            # excl_txns = PaymentTxn.objects.filter(status=0, ).exclude(payment_mode__in=[1, 4])
-            excl_order_list = excl_txns.all().values_list('order_id', flat=True)
+                orders = orders.exclude(
+                    id__in=excl_order_list).order_by('-date_placed')
 
-            orders = orders.exclude(
-                id__in=excl_order_list).order_by('-date_placed')
-
-            courses = OrderItem.objects.filter(order__in=orders,product__type_flow=2).values_list('product',flat=True)
-            tsrvcs = SearchQuerySet().filter(id__in=courses, pTP__in=[0, 1, 3]).exclude(
-                id__in=settings.EXCLUDE_SEARCH_PRODUCTS
-            )
-            data = [
-                    {'id': tsrvc.id, 'heading': tsrvc.pHd, 'name': tsrvc.pNm, 'url': tsrvc.pURL, 'img': tsrvc.pImg, \
-                    'img_alt': tsrvc.pImA, 'rating': tsrvc.pARx, 'price': tsrvc.pPinb, 'vendor': tsrvc.pPvn, 'stars': tsrvc.pStar,
-                    'provider': tsrvc.pPvn} for tsrvc in tsrvcs]
+                courses = OrderItem.objects.filter(order__in=orders,product__type_flow=2).values_list('product',flat=True)
+                tsrvcs = SearchQuerySet().filter(id__in=courses, pTP__in=[0, 1, 3]).exclude(
+                    id__in=settings.EXCLUDE_SEARCH_PRODUCTS
+                )
+                data = [
+                        {'id': tsrvc.id, 'heading': tsrvc.pHd, 'name': tsrvc.pNm, 'url': tsrvc.pURL, 'img': tsrvc.pImg, \
+                        'img_alt': tsrvc.pImA, 'rating': tsrvc.pARx, 'price': tsrvc.pPinb, 'vendor': tsrvc.pPvn, 'stars': tsrvc.pStar,
+                        'provider': tsrvc.pPvn} for tsrvc in tsrvcs]
+                cache.set('dashboard_my_courses',data,86400)
         return Response(data=data, status=status.HTTP_200_OK)
 
 class MyServicesApi(DashboardInfo, APIView):
@@ -93,25 +102,30 @@ class MyServicesApi(DashboardInfo, APIView):
         candidate_id = self.request.session.get('candidate_id', None)
         data = []
         if candidate_id:
-            orders = Order.objects.filter(
-                status__in=[0, 1, 3],
-                candidate_id=candidate_id)
+            if cache.get('dashboard_my_services'):
+                data = cache.get('dashboard_my_services')
+            else:
+                orders = Order.objects.filter(
+                    status__in=[0, 1, 3],
+                    candidate_id=candidate_id)
 
-            excl_txns = PaymentTxn.objects.filter(
-                status__in=[0, 2, 3, 4, 5],
-                payment_mode__in=[6, 7],
-                order__candidate_id=candidate_id)
-            excl_order_list = excl_txns.all().values_list('order_id', flat=True)
+                excl_txns = PaymentTxn.objects.filter(
+                    status__in=[0, 2, 3, 4, 5],
+                    payment_mode__in=[6, 7],
+                    order__candidate_id=candidate_id)
+                excl_order_list = excl_txns.all().values_list('order_id', flat=True)
 
-            orders = orders.exclude(
-                id__in=excl_order_list).order_by('-date_placed')
+                orders = orders.exclude(
+                    id__in=excl_order_list).order_by('-date_placed')
 
-            services = OrderItem.objects.filter(order__in=orders,product__product_class__slug__in=settings.SERVICE_SLUG).values_list('product',flat=True)
-            tsrvcs = SearchQuerySet().filter(id__in=services, pTP__in=[0, 1, 3]).exclude(
-                id__in=settings.EXCLUDE_SEARCH_PRODUCTS
-            )
-            data = [
-                    {'id': tsrvc.id, 'heading': tsrvc.pHd, 'name': tsrvc.pNm, 'url': tsrvc.pURL, 'img': tsrvc.pImg, \
-                    'img_alt': tsrvc.pImA, 'rating': tsrvc.pARx, 'price': tsrvc.pPinb, 'vendor': tsrvc.pPvn, 'stars': tsrvc.pStar,
-                    'provider': tsrvc.pPvn} for tsrvc in tsrvcs]
+                services = OrderItem.objects.filter(order__in=orders,product__product_class__slug__in=settings.SERVICE_SLUG).values_list('product',flat=True)
+                tsrvcs = SearchQuerySet().filter(id__in=services, pTP__in=[0, 1, 3]).exclude(
+                    id__in=settings.EXCLUDE_SEARCH_PRODUCTS
+                )
+                data = [
+                        {'id': tsrvc.id, 'heading': tsrvc.pHd, 'name': tsrvc.pNm, 'url': tsrvc.pURL, 'img': tsrvc.pImg, \
+                        'img_alt': tsrvc.pImA, 'rating': tsrvc.pARx, 'price': tsrvc.pPinb, 'vendor': tsrvc.pPvn, 'stars': tsrvc.pStar,
+                        'provider': tsrvc.pPvn} for tsrvc in tsrvcs]
+                cache.set('dashboard_my_services',data,86400)
+
         return Response(data=data, status=status.HTTP_200_OK)
