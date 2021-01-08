3538b83982c5a5f9a59f204ac1cb69a09cb926d1
Divyanshu Pandey <divyanshu.pandey@hindustantimes.com>
2021-01-08 11:31:48 +0530

    frontend apis saved

--- frontend/src/components/DesktopComponent/Core/CataloguePage/cataloguePage.jsx
+++ frontend/src/components/DesktopComponent/Core/CataloguePage/cataloguePage.jsx
@@ -11,19 +11,31 @@ import './cataloguePage.scss';
 import Aos from "aos";
 // import "aos/dist/aos.css";
 import { useDispatch } from 'react-redux';
-import { fetchRecentlyAddedCourses, fetchPopularServices, 
-    fetchAllCategoriesAndVendors, fetchTrendingCategories } from 'store/CataloguePage/actions/index';
+import {
+    fetchRecentlyAddedCourses, fetchPopularServices,
+    fetchAllCategoriesAndVendors, fetchTrendingCategories
+} from 'store/CataloguePage/actions/index';
 
 const CatalogPage = (props) => {
 
     const dispatch = useDispatch();
 
-    useEffect( () => {
+    useEffect(() => {
         Aos.init({ duration: 2000, once: true, offset: 10, anchorPlacement: 'bottom-bottom' });
-        new Promise((resolve, reject) => dispatch(fetchRecentlyAddedCourses({ resolve, reject })));
-        new Promise((resolve, reject) => dispatch(fetchPopularServices({ resolve, reject })));
-        new Promise((resolve, reject) => dispatch(fetchTrendingCategories({ 'medium' : 0, resolve, reject })));
-        new Promise((resolve, reject) => dispatch(fetchAllCategoriesAndVendors({ num:8, resolve, reject })));
+        //You may notice that apis corresponding to these actions are not getting called on initial render.
+        //This is because initial render is done on node server, which is calling these apis, map the data and send it to the browser.
+        //So there is no need to fetch them again on the browser.
+        if (!(window && window.config && window.config.isServerRendered)) {
+            new Promise((resolve, reject) => dispatch(fetchRecentlyAddedCourses({ resolve, reject })));
+            new Promise((resolve, reject) => dispatch(fetchPopularServices({ resolve, reject })));
+            new Promise((resolve, reject) => dispatch(fetchTrendingCategories({ 'medium': 0, resolve, reject })));
+            new Promise((resolve, reject) => dispatch(fetchAllCategoriesAndVendors({ num: 8, resolve, reject })));
+        }
+        else {
+            //isServerRendered is needed to be deleted because when routing is done through react and not on the node,
+            //above actions need to be dispatched.
+            delete window.config?.isServerRendered
+        }
     }, [])
 
     return (
--- frontend/src/components/MobileComponent/Core/CataloguePage/cataloguePage.jsx
+++ frontend/src/components/MobileComponent/Core/CataloguePage/cataloguePage.jsx
@@ -28,10 +28,20 @@ const CatalogPage = (props) => {
 
     useEffect( () => {
         Aos.init({ duration: 2000, once: true, offset: 10, anchorPlacement: 'bottom-bottom' });
+        //You may notice that apis corresponding to these actions are not getting called on initial render.
+        //This is because initial render is done on node server, which is calling these apis, map the data and send it to the browser.
+        //So there is no need to fetch them again on the browser.
+        if (!(window && window.config && window.config.isServerRendered)) {
         new Promise((resolve, reject) => dispatch(fetchRecentlyAddedCourses({ resolve, reject })));
         new Promise((resolve, reject) => dispatch(fetchPopularServices({ resolve, reject })));
         new Promise((resolve, reject) => dispatch(fetchTrendingCategories({ 'medium': 1, resolve, reject })));
         new Promise((resolve, reject) => dispatch(fetchAllCategoriesAndVendors({ num:8, resolve, reject })));
+    }
+    else {
+        //isServerRendered is needed to be deleted because when routing is done through react and not on the node,
+        //above actions need to be dispatched.
+        delete window.config?.isServerRendered
+    }
     }, [])
 
     return (
