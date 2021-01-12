1d5b1def38e42842702d3229d6b6752d472c5508
Divyanshu Pandey <divyanshu.pandey@hindustantimes.com>
2021-01-12 18:18:53 +0530

    resolved listitem bugs

--- frontend/src/components/DesktopComponent/Core/SkillPage/CoursesTray/Product/product.jsx
+++ frontend/src/components/DesktopComponent/Core/SkillPage/CoursesTray/Product/product.jsx
@@ -53,8 +53,8 @@ const Product = (props) => {
             delay={200}
         >
             <li className="col-sm-4" key={index} itemProp="itemListElement" itemScope itemType="http://schema.org/ListItem">
-                <meta itemprop="position" content={index+1}/>
-                <div className="card" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-offset="0" data-aos-delay={index*50+50} data-aos-duration="1000">
+                <meta itemprop="position" content={listIdx+1}/>
+                <div className="card" data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-offset="0" data-aos-delay={listIdx*50+50} data-aos-duration="1000">
                     <div className="card__heading">
                         {tags === 2 && <span className="flag-blue">NEW</span>}
                         {tags === 1 && <span className="flag-red">BESTSELLER</span>}
