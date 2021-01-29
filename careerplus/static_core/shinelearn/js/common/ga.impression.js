84cf5464eb081569db8c064da025c9f5a96e99c6
Rahul Singh <rahul.singh1@hindustantimes.com>
2021-01-29 22:31:48 +0530

    addToCart & sessionTracking impression fixed

--- careerplus/static_core/shinelearn/js/common/ga.impression.js
+++ careerplus/static_core/shinelearn/js/common/ga.impression.js
@@ -1,65 +1,78 @@
 function GALayer() {
     var that = this;
     var trackingId = 'UA-3537905-41';
-    that.fireGaEvent = function(options) {
+    that.fireGaEvent = function (options) {
         var event = options.event || '';
         var product = options.product || [];
         var currencyCode = options.currencyCode || 'INR';
         var actionField = options.actionField || null;
 
         window.dataLayer = window.dataLayer || [];
-        
-        try{
+
+        try {
+            console.log(event);
             console.log(product);
             if (event === 'productdetail') {
-            window.dataLayer.push(
-                {
-                "event": event,
-                'ecommerce': {
-                    'currencyCode': currencyCode,
-                    'details': {   
-                        'actionField': { 'list': actionField},
-                        'products': product
-                    }
-                }
-            });
-        }
+                window.dataLayer.push(
+                    {
+                        "event": event,
+                        'ecommerce': {
+                            'currencyCode': currencyCode,
+                            'details': {
+                                'actionField': { 'list': actionField },
+                                'products': product
+                            }
+                        }
+                    });
+            }
+            else if (event === 'addToCart') {
+                window.dataLayer.push(
+                    {
+                        "event": event,
+                        'ecommerce': {
+                            'currencyCode': currencyCode,
+                            'add': {
+                                'products': product
+                            }
+                        }
+                    });
+            }
             else {
                 window.dataLayer.push(
                     {
-                    "event": event,
-                    'ecommerce': {
-                        'currencyCode': currencyCode,
-                        'impressions': product
-                    }
-                });
-        }
+                        "event": event,
+                        'ecommerce': {
+                            'currencyCode': currencyCode,
+                            'impressions': product
+                        }
+                    });
+            }
 
 
         }
-        catch(e) {
+        catch (e) {
             console.log(e)
         }
     }
-    that.customDimension = function(name, value) {
+    that.customDimension = function (name, value) {
         gtag('config', trackingId, {
-          'custom_map': {name: value}
+            'custom_map': { name: value }
         });
     };
 };
 
-GALayer.prototype.SendImpression = function() {
+GALayer.prototype.SendImpression = function () {
     var that = this;
     eventName = arguments[0];
     ecommerce = arguments[1];
     currencyCode = arguments[2];
     actionField = arguments[3] || null;
-    
+
     that.fireGaEvent({
         'event': eventName,
         'product': ecommerce,
         'currencyCode': currencyCode,
-        'actionField' : actionField
+        'actionField': actionField
     })
 };
 
