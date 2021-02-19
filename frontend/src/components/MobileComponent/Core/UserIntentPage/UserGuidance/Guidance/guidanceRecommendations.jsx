1811e93023f1ae0f99daee0316685632bf93ed37
Rahul Singh <rahul.singh1@hindustantimes.com>
2021-02-19 19:45:07 +0530

    Guidance route and image fixed

--- frontend/src/components/MobileComponent/Core/UserIntentPage/UserGuidance/Guidance/guidanceRecommendations.jsx
+++ frontend/src/components/MobileComponent/Core/UserIntentPage/UserGuidance/Guidance/guidanceRecommendations.jsx
@@ -19,33 +19,33 @@ const GuidanceRecommendations = (props) => {
 
                 <ul className="m-ui-list mt-20 mb-30">
                     <li className={ activeId === 1 && 'active'} onTouchStart={() => setActiveId(1)} onTouchEnd={()=> setActiveId(0)} >
-                        <Link to={"#"}>
+                        <Link to={"/user-intent/find-right-job/"}>
                             <figure>
-                                <img src="./media/images/mobile/ui-list-icon1.png" alt="Find the right job" />
+                                <img src="/media/images/mobile/ui-list-icon1.png" alt="Find the right job" />
                             </figure>
                             <h3>Find the right job</h3>
                         </Link>
                     </li>
                     <li className={ activeId === 2 && 'active'} onTouchStart={() => setActiveId(2)} onTouchEnd={()=> setActiveId(0)}>
-                        <Link to={"#"}>
+                        <Link to={"/user-intent/make-career-change/"}>
                             <figure>
-                                <img src="./media/images/mobile/ui-list-icon2.png" alt="Make a career change" />
+                                <img src="/media/images/mobile/ui-list-icon2.png" alt="Make a career change" />
                             </figure>
                             <h3>Make a career change</h3>
                         </Link>
                     </li>
                     <li className={ activeId === 3 && 'active'} onTouchStart={() => setActiveId(3)} onTouchEnd={()=> setActiveId(0)}>
-                        <Link to={"#"}>
+                        <Link to={"/user-intent/improve-profile/"}>
                             <figure>
-                                <img src="./media/images/mobile/ui-list-icon3.png" alt="Improve your profile" />
+                                <img src="/media/images/mobile/ui-list-icon3.png" alt="Improve your profile" />
                             </figure>
                             <h3>Improve your profile</h3>
                         </Link>
                     </li>
                     <li className={ activeId === 4 && 'active'} onTouchStart={() => setActiveId(4)} onTouchEnd={()=> setActiveId(0)}>
-                        <Link to={"#"}>
+                        <Link to={"/user-intent/progress-career"}>
                             <figure>
-                                <img src="./media/images/mobile/ui-list-icon4.png" alt="Progress your career" />
+                                <img src="/media/images/mobile/ui-list-icon4.png" alt="Progress your career" />
                             </figure>
                             <h3>Progress your career</h3>
                         </Link>
