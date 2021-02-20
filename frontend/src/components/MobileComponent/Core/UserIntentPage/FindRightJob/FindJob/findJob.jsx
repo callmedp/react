65e8dcd4c46b99e5c398482b0a48841312b74000
Rahul Singh <rahul.singh1@hindustantimes.com>
2021-02-20 12:58:13 +0530

    Mobile form field fixed

--- frontend/src/components/MobileComponent/Core/UserIntentPage/FindRightJob/FindJob/findJob.jsx
+++ frontend/src/components/MobileComponent/Core/UserIntentPage/FindRightJob/FindJob/findJob.jsx
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './findJob.scss';
-
+import { useForm } from 'react-hook-form';
+import { useDispatch } from 'react-redux';
+import { InputField, SelectExperienceBox, MultiSelectBox } from 'formHandler/mobileFormHandler/formFields';
+import UserIntentForm from 'formHandler/mobileFormHandler/formData/userIntent';
+import { fetchedUserIntentData } from 'store/UserIntentPage/actions';
 
 const FindJob = (props) => {
     const { history, type } = props;
+    const dispatch = useDispatch();
+    const [chips, setChips] = useState([]);
+    const [skillSet, setSkillSet] = useState([])
+    const { register, handleSubmit, errors } = useForm();
+
+    const addValues = (values) => {
+        return {
+            ...values,
+            'type': type,
+        }
+    }
+
+    function handleAppend(data, id) {
+        setChips([...chips, data])
+    }
+
+
+    const onSubmit = async (values, event) => {
+        const data = addValues(values)
+        await new Promise((resolve) => dispatch(fetchedUserIntentData({ data, resolve })));
+        history.push({
+            search: `?job=${values.job}&experience=${values.experience}&location=${values.location}&skills=${values.skills}`
+        })
+    }
+
     return (
         <section className="m-container mt-0 mb-0 pl-0 pr-0">
             <div className="m-ui-main col">
@@ -16,16 +45,16 @@ const FindJob = (props) => {
                     </div>
                     <Link className="btn-blue-outline m-back-goal-btn" to={"/user-intent/"}>Back to goal</Link>
                 </div>
-                <h2 className="m-heading3 mt-20">{ type === 'job' ? 'Let’s get you to the right job' : type === 'pcareer' ? 'Get to next level with shine' : 'What do you have in mind' }</h2>
+                <h2 className="m-heading3 mt-20">{type === 'job' ? 'Let’s get you to the right job' : type === 'pcareer' ? 'Get to next level with shine' : 'What do you have in mind'}</h2>
 
-                        <div className="m-find-job">
-                            <form className="mt-20">
-                                <div className="form-group">
-                                    <input type="text" className="form-control" id="job" name="job" placeholder=" "
-                                        aria-required="true" aria-invalid="true" />
-                                    <label for="">Current job title</label>
-                                </div>
-                                <div className="form-group">
+                <div className="m-find-job">
+                    <form className="mt-20" onSubmit={handleSubmit(onSubmit)}>
+                        <div className="form-group">
+                            <input type="text" className="form-control" id="job" name="job" placeholder=" "
+                                aria-required="true" aria-invalid="true" />
+                            <label for="">Current job title</label>
+                        </div>
+                        {/* <div className="form-group">
                                     <div className="custom-select-box">
                                         <select className="select" className="custom-select">
                                             <option selected>Total experience </option>
@@ -34,22 +63,39 @@ const FindJob = (props) => {
                                             <option value="+93">6+</option>
                                         </select>
                                     </div>
-                                </div>
-                                <div className="form-group">
-                                    <input type="text" className="form-control" id="location" name="location" placeholder=" "
-                                        aria-required="true" aria-invalid="true" />
-                                    <label for="">Preferred location</label>
-                                </div>
-                                <div className="form-group">
+                                </div> */}
+                        <SelectExperienceBox attributes={UserIntentForm.experience} register={register}
+                            errors={!!errors ? errors[UserIntentForm.experience.name] : ''} />
+                        <div className="form-group">
+                            <input type="text" className="form-control" id="location" name="location" placeholder=" "
+                                aria-required="true" aria-invalid="true" />
+                            <label for="">Preferred location</label>
+                        </div>
+
+                        {/* <div className="form-group">
                                     <input type="text" className="form-control" id="skills" name="skills" placeholder=" "
                                         aria-required="true" aria-invalid="true" />
                                     <label for="">Your skills</label>
-                                </div>
-                                <button type="submit" className="btn btn-inline btn-primary submit-btn mt-30" role="button" data-toggle="modal"
-                                data-target="#thankyouModal">{ type === 'job' ? 'View jobs' : 'View courses' }</button>
-                            </form>
+                                </div> */}
+
+                        <MultiSelectBox attributes={UserIntentForm.skills} data={chips} register={register}
+                            errors={!!errors ? errors[UserIntentForm.skills.name] : ''} />
+
+                        <div className="form-group-custom">
+                            {skillSet?.map((skill, indx) => {
+                                return (
+                                    <label className="label-add" onClick={() => handleAppend(skill.name, indx)} for="">{skill.name}</label>
+                                )
+                            })
+                            }
                         </div>
-                    </div>
+
+
+                        <button type="submit" className="btn btn-inline btn-primary submit-btn mt-30" role="button" data-toggle="modal"
+                            data-target="#thankyouModal">{type === 'job' ? 'View jobs' : 'View courses'}</button>
+                    </form>
+                </div>
+            </div>
         </section>
     )
 }
---
+++ frontend/src/formHandler/mobileFormHandler/formData/userIntent.js
@@ -0,0 +1,131 @@
+export default {
+    'job': {
+        className: "form-control",
+        type: "text",
+        name: "job",
+        id: "job",
+        placeholder: " ",
+        label: "Current job title",
+        inputType: 'input',
+        validation: {
+            required: true,
+        },
+        errorMessage: {
+            required: "This field is required"
+        }
+    },
+    'experience': {
+        name: 'experience',
+        inputType: 'select',
+        children: [
+            {
+                text: "Total Experience",
+                value: ""
+            },
+            {
+                text: "1 Yr",
+                value: "1 Yr",
+            },
+            {
+                text: "2 Yrs",
+                value: "2 Yrs",
+            },
+            {
+                text: "3 Yrs",
+                value: "3 Yrs",
+            },
+            {
+                text: "4 Yrs",
+                value: "4 Yrs",
+            },
+            {
+                text: "5 Yrs",
+                value: "5 Yrs",
+            },
+            {
+                text: "6 Yrs",
+                value: "6 Yrs",
+            },
+            {
+                text: "7 Yrs",
+                value: "7 Yrs",
+            },
+            {
+                text: "8 Yrs",
+                value: "8 Yrs",
+            },
+            {
+                text: "9 Yrs",
+                value: "9 Yrs",
+            },
+            {
+                text: "10 Yrs",
+                value: "10 Yrs",
+            },
+            {
+                text: "11 Yrs",
+                value: "11 Yrs",
+            },
+            {
+                text: "12 Yrs",
+                value: "12 Yrs",
+            },
+            {
+                text: "13 Yrs",
+                value: "13 Yrs",
+            },
+            {
+                text: "14 Yrs",
+                value: "14 Yrs",
+            },
+            {
+                text: "15 Yrs",
+                value: "15 Yrs",
+            },
+            {
+                text: "16 Yrs",
+                value: "16 Yrs",
+            },
+            {
+                text: "17+ Yrs",
+                value: "17+ Yrs",
+            },
+        ],
+        validation: {
+            required: true
+        },
+        errorMessage: {
+            required: "This field is required"
+        }
+    },
+    'location': {
+        className: "form-control",
+        name: "location",
+        type: "text",
+        placeholder: " ",
+        label: "Preferred location",
+        inputType: "input",
+        validation: {
+            required: true
+        },
+        errorMessage: {
+            required: "This field is required"
+        }
+    },
+    'skills': {
+        className: "form-control",
+        name: "skills",
+        type: "text",
+        placeholder: "Keyword Research",
+        label: "Your skills",
+        inputType: "input",
+        validation: {
+            required: true
+        },
+        errorMessage: {
+            required: "This field is required"
+        },
+        children: []
+    }
+    
+}
