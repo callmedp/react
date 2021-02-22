c704fd787c246186f9220ce1269122e831b92ceb
Divyanshu Pandey <divyanshu.pandey@hindustantimes.com>
2021-02-22 18:38:52 +0530

    added cookie in header of mobile

--- frontend/src/components/MobileComponent/Common/MenuNav/DefaultMenuNav/defaultMenuNav.jsx
+++ frontend/src/components/MobileComponent/Common/MenuNav/DefaultMenuNav/defaultMenuNav.jsx
@@ -1,17 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import { slide as Menu } from 'react-burger-menu';
 import { Link } from 'react-router-dom';
-import { siteDomain, resumeShineSiteDomain } from 'utils/domains'; 
+import { siteDomain, resumeShineSiteDomain } from 'utils/domains';
 import MenuNavHeader from '../MenuNavHeader/menuNavHeader';
 import './defaultMenuNav.scss'
 import { useSelector, useDispatch } from 'react-redux';
 import { cartCount, fetchNavOffersAndTags } from 'store/Header/actions/index';
 import { initLoggedInZendesk } from 'utils/zendeskIniti';
 // import { trackUser } from 'store/Tracking/actions/index.js';
-import { removeTrackingInfo, getCandidateInformation } from 'utils/storage.js';
+import { removeTrackingInfo, getCandidateInformation,getCandidateId } from 'utils/storage.js';
 import { MyGA } from 'utils/ga.tracking.js';
+import { fetchAlreadyLoggedInUser } from "store/Authentication/actions/index";
 
-const DefaultMenuNav = (props) =>{
+const DefaultMenuNav = (props) => {
+
+    let cookies = "";
+    try {
+        cookies = document.cookie.split(";");
+        for (let i = 0; i < cookies.length; i++) {
+            let cookie = cookies[i].trim();
+            if (cookie.substring(0, "_em_".length + 1) === "_em_=") {
+                cookies = cookie.substring("_em_".length + 1);
+                break;
+            }
+        }
+    } catch (err) {
+        cookies = "";
+    }
+
     const {
         setType, setOpen, open
     } = props
@@ -26,7 +42,7 @@ const DefaultMenuNav = (props) =>{
             dispatch(cartCount());
             // const isSessionAvailable = await new Promise((resolve, reject) => dispatch(sessionAvailability({ resolve, reject })));
 
-            if (localStorage.getItem('isAuthenticated') === 'true') {
+            if (getCandidateId()) {
                 try {
                     setIsLoggedIn(true)
                     // const candidateId = isSessionAvailable['candidate_id']
@@ -40,6 +56,18 @@ const DefaultMenuNav = (props) =>{
                     console.log("ERROR OCCURED", e)
                 }
             }
+            else if (cookies != '' && Array.isArray(cookies) == false) {
+
+                let res = await new Promise((resolve, reject) => {
+                    dispatch(
+                        fetchAlreadyLoggedInUser({
+                            resolve,
+                            reject,
+                            payload: { em: cookies },
+                        })
+                    );
+                });
+            }
             else {
                 setIsLoggedIn(false)
             }
@@ -52,9 +80,9 @@ const DefaultMenuNav = (props) =>{
     }
 
     const eventTracking = () => {
-        MyGA.SendEvent('logo_click', 'ln_logo_click', 'ln_logo_click', 'homepage','',false, true)
+        MyGA.SendEvent('logo_click', 'ln_logo_click', 'ln_logo_click', 'homepage', '', false, true)
         let product_tracking_mapping_id = localStorage.getItem("productTrackingMappingId");
-        if(product_tracking_mapping_id == '10'){
+        if (product_tracking_mapping_id == '10') {
             removeTrackingInfo()
         }
     }
@@ -62,38 +90,38 @@ const DefaultMenuNav = (props) =>{
     useEffect(() => {
         fetchUserInfo();
         dispatch(fetchNavOffersAndTags());
-    },[])
+    }, [])
 
     return (
-        <Menu className='navigation' width={ '300px' } isOpen={open} onStateChange={state => setOpen(state.isOpen)}>
-            <MenuNavHeader isLoggedIn={isLoggedIn} candidateInfo={candidateInfo}/>
+        <Menu className='navigation' width={'300px'} isOpen={open} onStateChange={state => setOpen(state.isOpen)}>
+            <MenuNavHeader isLoggedIn={isLoggedIn} candidateInfo={candidateInfo} />
             <div className="m-menu-links">
-                <Link className="menu-item" to="/" onClick={() => {setOpen(state => !state);eventTracking()}}>
-                    <figure className="micon-home" /> Home 
+                <Link className="menu-item" to="/" onClick={() => { setOpen(state => !state); eventTracking() }}>
+                    <figure className="micon-home" /> Home
                 </Link>
-                <a href="/" className="menu-item" onClick={(e) => {e.preventDefault();setType('allCourses')}}>
+                <a href="/" className="menu-item" onClick={(e) => { e.preventDefault(); setType('allCourses') }}>
                     <figure className="micon-courses-services" /> All Courses <figure className="micon-arrow-menusm ml-auto" />
                 </a>
-                <a href="/" className="menu-item" onClick={(e) => {e.preventDefault();setType('jobAssistanceServices'); MyGA.SendEvent('homepage_navigation','ln_homepage_navigation', 'ln_job_assisstance', 'ln_job_assisstance', '', false, true)} }>
+                <a href="/" className="menu-item" onClick={(e) => { e.preventDefault(); setType('jobAssistanceServices'); MyGA.SendEvent('homepage_navigation', 'ln_homepage_navigation', 'ln_job_assisstance', 'ln_job_assisstance', '', false, true) }}>
                     <figure className="micon-resume-service" /> Job Assistance Services <figure className="micon-arrow-menusm ml-auto" />
                 </a>
-                <a href={`${resumeShineSiteDomain}/product/linkedin-profile-writing/entry-level-2/1926/`} className="menu-item" onClick={() => MyGA.SendEvent('homepage_navigation','ln_homepage_navigation', 'ln_linked_profile_writing', 'ln_linked_profile_writing', '', false, true)} ><figure className="micon-linkedin-service" /> Linkedin Profile Writing</a>
-                <a href="/" className="menu-item" onClick={(e) => {e.preventDefault();setType('freeResources'); MyGA.SendEvent('homepage_navigation','ln_homepage_navigation', 'ln_free_resources', 'ln_free_resources', '', false, true)}}>
-                    <figure className="micon-free-resources" /> Free Resources <figure className="micon-arrow-menusm ml-auto"/>
+                <a href={`${resumeShineSiteDomain}/product/linkedin-profile-writing/entry-level-2/1926/`} className="menu-item" onClick={() => MyGA.SendEvent('homepage_navigation', 'ln_homepage_navigation', 'ln_linked_profile_writing', 'ln_linked_profile_writing', '', false, true)} ><figure className="micon-linkedin-service" /> Linkedin Profile Writing</a>
+                <a href="/" className="menu-item" onClick={(e) => { e.preventDefault(); setType('freeResources'); MyGA.SendEvent('homepage_navigation', 'ln_homepage_navigation', 'ln_free_resources', 'ln_free_resources', '', false, true) }}>
+                    <figure className="micon-free-resources" /> Free Resources <figure className="micon-arrow-menusm ml-auto" />
                 </a>
-                <a className="menu-item" href={`${siteDomain}/talenteconomy/`} onClick={() => MyGA.SendEvent('homepage_navigation','ln_homepage_navigation', 'ln_blog', 'ln_blog', '', false, true)}>
+                <a className="menu-item" href={`${siteDomain}/talenteconomy/`} onClick={() => MyGA.SendEvent('homepage_navigation', 'ln_homepage_navigation', 'ln_blog', 'ln_blog', '', false, true)}>
                     <figure className="micon-blog-services" /> Blog
                 </a>
                 {
-                    navTags?.length ? 
-                    (
-                        navTags.map((tag, index)=>{
-                            return <a key={index} className="menu-item" href={tag.skill_page_url}> {tag?.display_name}&emsp;<small className="m-config-tag">{tag?.tag}</small></a>
-                        })
-                    ) : null
+                    navTags?.length ?
+                        (
+                            navTags.map((tag, index) => {
+                                return <a key={index} className="menu-item" href={tag.skill_page_url}> {tag?.display_name}&emsp;<small className="m-config-tag">{tag?.tag}</small></a>
+                            })
+                        ) : null
                 }
                 {
-                    isLoggedIn && 
+                    isLoggedIn &&
                     (
                         <ul className="dashboard-menu">
                             <li>
@@ -108,8 +136,8 @@ const DefaultMenuNav = (props) =>{
                         </ul>
                     )
                 }
-                <a className="menu-item" href={`${siteDomain}/about-us`} onClick={() => MyGA.SendEvent('homepage_footer','ln_homepage_footer', 'ln_homepage_footer_clicked', 'About Us','', false, true)}>About us</a>
-                <a className="menu-item" href={`${siteDomain}/contact-us`} onClick={() =>  MyGA.SendEvent('homepage_footer','ln_homepage_footer', 'ln_homepage_footer_clicked', 'Contact Us','', false, true)}>Contact us</a>
+                <a className="menu-item" href={`${siteDomain}/about-us`} onClick={() => MyGA.SendEvent('homepage_footer', 'ln_homepage_footer', 'ln_homepage_footer_clicked', 'About Us', '', false, true)}>About us</a>
+                <a className="menu-item" href={`${siteDomain}/contact-us`} onClick={() => MyGA.SendEvent('homepage_footer', 'ln_homepage_footer', 'ln_homepage_footer_clicked', 'Contact Us', '', false, true)}>Contact us</a>
             </div>
         </Menu>
     );
