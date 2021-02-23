1aa1fea28345ce29174b2c8d60e9b799d6a1cc27
aakanshamastwal <aakansha.mastwal92@gmail.com>
2021-02-23 11:58:19 +0530

    courses mobile UI done

--- frontend/src/components/MobileComponent/Core/UserIntentPage/FindRightJob/FindJob/findJob.jsx
+++ frontend/src/components/MobileComponent/Core/UserIntentPage/FindRightJob/FindJob/findJob.jsx
@@ -82,7 +82,7 @@ const FindJob = (props) => {
         const data = addValues(values);
         // await new Promise((resolve) => dispatch(fetchFindRightJobsData({ data, resolve })));
         history.push({
-            search: `?job_title=${data.job}&minexp=${data.experience}&loc=${data.location}&skill=${data.skills.join()}`
+            search: `?job_title=${data.job}&minexp=${data.experience}&loc=${data.location}&skill=${data.skills}`
         });
     }
 
