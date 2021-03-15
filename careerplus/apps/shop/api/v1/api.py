9afd76256dce18e7d7eb41a0ad55482377e9919c
Rahul Singh <rahul.singh1@hindustantimes.com>
2021-03-15 12:32:03 +0530

    API initial detail added

--- careerplus/apps/shop/api/v1/api.py
+++ careerplus/apps/shop/api/v1/api.py
@@ -19,3 +19,23 @@ from rest_framework import status
 from rest_framework.permissions import IsAuthenticated, AllowAny
 
 
+class ProductDetailAPI(ProductInformationMixin, APIView):
+    permission_classes = (AllowAny,)
+    serializer_class = ProductDetailSerializer
+
+    def get_object(self, pid):
+        try:
+            return Product.objects.get(pk=pid)
+        except Product.DoesNotExist:
+            raise Http404
+
+    def get(self, request, *args, **kwargs):
+        pid = self.request.GET.get('pid')
+        slug = self.request.GET.get('slug')
+        user = self.request.user
+
+        product = self.get_object(pid)
+
+        serializer_obj = ProductDetailSerializer(product)
+
+        return APIResponse(data=serializer_obj.data)
