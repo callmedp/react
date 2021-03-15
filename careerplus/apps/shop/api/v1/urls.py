55a79439517678916292e40b5db377500bdd3634
Rahul Singh <rahul.singh1@hindustantimes.com>
2021-03-15 12:32:46 +0530

    API url routes added

--- careerplus/apps/shop/api/v1/urls.py
+++ careerplus/apps/shop/api/v1/urls.py
@@ -7,6 +7,11 @@ from .views import (
     UpdateProductSkillView, UpdateScreenProductSkillView, RecommendedProductsAPIView,
     ProductReview,SkillProductView, CourseCatalogueAPI
 )
+
+from .api import (
+    ProductDetailAPI
+)
+
 from django.conf import settings
 from rest_framework import routers
 
@@ -24,7 +29,9 @@ urlpatterns = [
     re_path(r'^recommended-products/$', RecommendedProductsAPIView.as_view()),
     re_path(r'^product-review',ProductReview.as_view()),
     re_path(r'^skill-product/$',SkillProductView.as_view()),
-    re_path(r'^course-catalogue/$', CourseCatalogueAPI.as_view(), name='course-catalogue-api')
+    re_path(r'^course-catalogue/$', CourseCatalogueAPI.as_view(), name='course-catalogue-api'),
+
+    re_path(r'^get-product/$', ProductDetailAPI.as_view(), name='get-product-api')
 ]
 
 if settings.DEBUG:
