cf8e93337da8e33ab04331cbd2712f9a03ed436f
Rahul Singh <rahul.singh1@hindustantimes.com>
2021-03-17 15:33:04 +0530

    API: Action enquire now

--- frontend/src/store/DetailPage/actions.js
+++ frontend/src/store/DetailPage/actions.js
@@ -10,6 +10,9 @@ const fetchReviews = createAction('FETCH_REVIEWS');
 const ReviewsFetched = createAction('REVIEWS_FETCHED');
 const submitReview = createAction('SUBMIT_REVIEW');
 
+const sendEnquireNow = createAction('SEND_ENQUIRE_NOW');
+const sendedEnquireNow = createAction('SENEDED_EQUIRE_NOW');
+
 export {
     fetchOtherProviderCourses,
     OtherProviderCoursesFetched,
@@ -17,5 +20,7 @@ export {
     ReviewsFetched,
     submitReview,
     fetchRecommendedCourses,
-    recommendedCoursesFetched
+    recommendedCoursesFetched,
+    sendEnquireNow,
+    sendedEnquireNow
 }
