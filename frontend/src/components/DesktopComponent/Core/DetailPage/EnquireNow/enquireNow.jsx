9067e330d57a12a233b70df62654f42c55c146ee
Rahul Singh <rahul.singh1@hindustantimes.com>
2021-03-17 14:42:46 +0530

    enqureNow form fixed

--- frontend/src/components/DesktopComponent/Core/DetailPage/EnquireNow/enquireNow.jsx
+++ frontend/src/components/DesktopComponent/Core/DetailPage/EnquireNow/enquireNow.jsx
@@ -1,9 +1,13 @@
 // React Core Import
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { InputField, SelectBox, InputFieldDynamic, TextAreaDynamic } from 'formHandler/desktopFormHandler/formFields';
 
 // Form Import
 import { useForm } from 'react-hook-form';
+
+// Inter-App Import
+import DetailPageForm from 'formHandler/desktopFormHandler/formData/DetailPage';
 
 // Styling Import
 import './enquireNow.scss';
@@ -12,10 +16,18 @@ const EnquireNow = (props) => {
     const { history } = props;
     const { register, handleSubmit, errors } = useForm();
 
+
+    console.log(errors)
+
     const addValues = (values) => {
         // Adding and contributing their values according to contributions
     }
-    
+
+    const onSubmit = async (data) => {
+        // On submit send data to back-end
+
+    }
+
     return (
         <section id="enquire-now" className="container-fluid lightblue-bg mt-40" data-aos="fade-up">
             <div className="row">
@@ -26,33 +38,23 @@ const EnquireNow = (props) => {
                         </div>
                         <div className="enquire-now__form col">
                             <strong className="heading2">Enquire now!</strong>
-                            <form className="mt-30">
-                                <div className="form-group error">
-                                    <input type="text" className="form-control" id="name" name="name" placeholder=" "
-                                        value="" aria-required="true" aria-invalid="true" />
-                                    <label for="">Name</label>
-                                    <span class="error-msg">Required</span>
-                                </div>
+                            <form className="mt-30" onSubmit={handleSubmit(onSubmit)}>
+
+                                <InputField attributes={DetailPageForm.name} register={register}
+                                    errors={!!errors ? errors[DetailPageForm.name.name] : false} />
+
                                 <div className="d-flex">
-                                    <div className="custom-select-box">
-                                        <select className="select" className="custom-select">
-                                            <option selected>+91</option>
-                                            <option value="+91">+91</option>
-                                            <option value="+92">+92</option>
-                                            <option value="+93">+93</option>
-                                        </select>
-                                    </div>
-                                    <div className="form-group flex-1">
-                                        <input type="text" className="form-control" id="mobile" name="mobile"
-                                            placeholder=" " value="" aria-required="true" aria-invalid="true" />
-                                        <label for="">Mobile</label>
-                                    </div>
+
+                                    <SelectBox attributes={DetailPageForm.country_code} register={register} />
+
+                                    <InputFieldDynamic attributes={DetailPageForm.mobile} register={register}
+                                        errors={!!errors ? errors[DetailPageForm.mobile.name] : false} />
+
                                 </div>
-                                <div className="form-group">
-                                    <textarea className="form-control" rows="3" id="mesage" name="mesage" placeholder=" "
-                                        value="" aria-required="true" aria-invalid="true" />
-                                    <label for="">Message</label>
-                                </div>
+
+                                <TextAreaDynamic attributes={DetailPageForm.message} register={register}
+                                    errors={!!errors ? errors[DetailPageForm.message.name] : false} />
+
                                 <button type="submit" className="btn btn-inline btn-primary submit-btn" role="button">Submit</button>
                             </form>
                         </div>
