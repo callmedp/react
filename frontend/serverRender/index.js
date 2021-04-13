e822834c4fceacd89606a265201483ddd0e67c4e
aakanshamastwal <aakansha.mastwal92@gmail.com>
2021-04-13 16:02:47 +0530

    reviews issue

--- frontend/serverRender/index.js
+++ frontend/serverRender/index.js
@@ -128,7 +128,7 @@ app.get(expressRoutes, (req, res) => {
         routes = require('routes/index.desktop').routes;
 
     }
-    const branch = matchRoutes(routes, req.path) || [] ;
+    const branch = matchRoutes(routes, req.path) || [];
 
        const data = async () => await new Promise((resolve, reject) => {
         fetch(`${window.config?.siteDomain || 'https://learning.shine.com'}/api/v1/fetch-info/`,
@@ -136,45 +136,43 @@ app.get(expressRoutes, (req, res) => {
                 method: 'POST',
                 body: JSON.stringify({ "em": cookies }),
                 headers: { 'Content-Type': 'application/json' }
-            }).then(res => res.json())
+            })
+            .then(res => res.json())
             .then(json => {
                 console.log('promise resolved');
             })
             .catch(err => console.log(err))
             .finally(() => {
                 console.log('here in finally');
-    branch.forEach(async ({ route, match }) => {
-        if (route && route.actionGroup) {
-            try {
-                result = await new Promise((resolve, reject) => fetchApiData(store, match.params,cookies, route.actionGroup, resolve, reject));
-            }
-            catch (error) {
-                if (error?.status === 404) {
-                    return res.redirect('/404/');
-                }
-            }
+                branch.forEach(async ({ route, match }) => {
+                    if (route && route.actionGroup) {
+                        try {
+                            result = await new Promise((resolve, reject) => fetchApiData(store, match.params,cookies, route.actionGroup, resolve, reject));
+                        }
+                        catch (error) {
+                            if (error?.status === 404) {
+                                return res.redirect('/404/');
+                            }
+                            if(error?.redirect_url) {
+                                res.redirect(error.redirect_url);
+                            }
+                        }
 
+                        appContent = render(req, routes);
+                        const preloadedState = store.getState()
 
-            appContent = render(req, routes);
-            const preloadedState = store.getState()
+                        return res.render(indexFile, {
+                            appContent,
+                            preloadedState: JSON.stringify(preloadedState).replace(/</g, '\\u003c'),
+                            config: JSON.stringify(window.config)
+                        });
+                    }
 
-            return res.render(indexFile, {
-                appContent,
-                preloadedState: JSON.stringify(preloadedState).replace(/</g, '\\u003c'),
-                config: JSON.stringify(window.config)
+                });
             });
-
-
-
-        }
-
-    });
-});
-});
-
+        });
     data();
 });
-
 
 
 app.get('*', (req, res) => {
--- frontend/src/utils/domains.js
+++ frontend/src/utils/domains.js
@@ -1,7 +1,7 @@
 // Please don't change any variable directy in here. If you wan't to change it, do with in the .env file.
 // For info on how to use .env file visit https://create-react-app.dev/docs/adding-custom-environment-variables/
 
-export const siteDomain =  window?.config?.siteDomain  || process.env.REACT_APP_SITE_DOMAIN || 'https://learning.shine.com';
+export const siteDomain =  window?.config?.siteDomain  || 'https://learning1.shine.com' || 'https://learning.shine.com';
 
 console.log(process.env.REACT_APP_SITE_DOMAIN)
 
