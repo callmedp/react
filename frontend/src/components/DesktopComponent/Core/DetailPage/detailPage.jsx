da25cdba6bf8624d8ce4237d89804fe40e5742b0
aakanshamastwal <aakansha.mastwal92@gmail.com>
2021-04-15 16:29:02 +0530

    bug fixes

--- frontend/src/components/DesktopComponent/Core/DetailPage/detailPage.jsx
+++ frontend/src/components/DesktopComponent/Core/DetailPage/detailPage.jsx
@@ -24,12 +24,15 @@ import { useSelector, useDispatch } from 'react-redux';
 import { startMainCourseLoader, stopMainCourseLoader } from 'store/Loader/actions/index';
 import Loader from '../../Common/Loader/loader';
 import MetaContent from "../../Common/MetaContent/metaContent";
+import queryString from 'query-string';
+import { getTrackingInfo, storageTrackingInfo, removeTrackingInfo } from 'utils/storage.js';
+import { trackUser } from 'store/Tracking/actions/index.js';
 
 const DetailPage = (props) => {
     const dispatch = useDispatch();
-    const {product_detail, skill, product_id} = useSelector(store => store?.mainCourses);
+    const {product_detail, skill, product_id, product_tracking_mapping_id} = useSelector(store => store?.mainCourses);
     const meta_tags = product_detail?.meta;
-    const {match: {params: {id}}, history} = props;
+    const {location: { search }, match: {params: {id}}, history} = props;
     const { mainCourseLoader } = useSelector(store => store.loader);
     const [showStickyNav, setShowStickyNav] = useState(false);
     const [varChecked, changeChecked] = useState({});
@@ -52,21 +55,38 @@ const DetailPage = (props) => {
     const handleEffects = async () => {
         dispatch(startMainCourseLoader());
         try {
-                if (!(window && window.config && window.config.isServerRendered)) {
-                    // dispatch(startMainCourseLoader());
-                    await new Promise((resolve, reject) => dispatch(fetchMainCourses({ payload: {id: id?.split('-')[1], device:'desktop' },resolve, reject })));
-                    dispatch(stopMainCourseLoader());
-                }
-                else {
-                    delete window.config?.isServerRendered;
-                    dispatch(stopMainCourseLoader());
-                }
+            if (!(window && window.config && window.config.isServerRendered)) {
+                // dispatch(startMainCourseLoader());
+                await new Promise((resolve, reject) => dispatch(fetchMainCourses({ payload: {id: id?.split('-')[1], device:'desktop' },resolve, reject })));
+                dispatch(stopMainCourseLoader());
+            }
+            else {
+                delete window.config?.isServerRendered;
+                dispatch(stopMainCourseLoader());
+            }
         }
         catch (error) {
             dispatch(stopMainCourseLoader());
             if (error?.status == 404) history.push('/404');
         }
         dispatch(stopMainCourseLoader());
+
+        const query = queryString.parse(search);
+        if (query["t_id"]) {
+          query["prod_id"] = id;
+          query["product_tracking_mapping_id"] = product_tracking_mapping_id;
+          storageTrackingInfo(query);
+          dispatch(
+            trackUser({
+              query: query,
+              action: "ln_course_page",
+            })
+          );
+        }
+        else {
+          let tracking_data = getTrackingInfo();
+          if (tracking_data["prod_id"] != id && tracking_data["product_tracking_mapping_id"] === product_tracking_mapping_id) removeTrackingInfo();
+        }
     };
 
     return (
@@ -119,7 +139,7 @@ const DetailPage = (props) => {
             { (product_detail?.chapter && product_detail?.prd_service === 'assessment') && <TopicsCovered  chapter_list={product_detail?.chapter_list}/> }
 
             {
-                (product_detail?.prd_should_lrn || product_detail?.free_test) &&
+                (product_detail?.prd_should_lrn || !product_detail?.free_test) &&
                 <div className="container-fluid">
                     <div className="row">
                         { 
@@ -129,7 +149,7 @@ const DetailPage = (props) => {
                             </div>
                         }
                         {
-                            product_detail?.free_test &&
+                            !product_detail?.free_test &&
                             <div className="col-sm-3">
                                 <TakeFreeTest should_take_test_url={product_detail?.shld_take_test_slg} />
                             </div>
