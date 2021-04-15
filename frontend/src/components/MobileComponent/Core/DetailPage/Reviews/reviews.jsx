6c8f26f5f237055cac839820e1ee6c20b534add4
Gaurav_S <admin@Admins-MacBook-Pro-3.local>
2021-04-15 19:04:13 +0530

    warehouse changes

--- frontend/src/components/MobileComponent/Core/DetailPage/Reviews/reviews.jsx
+++ frontend/src/components/MobileComponent/Core/DetailPage/Reviews/reviews.jsx
@@ -43,7 +43,7 @@ const Reviews = (props) => {
     }
 
     return (
-        <section className="m-container mt-0 mb-0 pb-0" id="review" data-aos="fade-up">
+        <section className="m-container mt-0 mb-0 pb-0" id="reviews" data-aos="fade-up">
             <div className="d-flex" itemProp="review" itemScope itemType="https://schema.org/Review">
                 <h2 className="m-heading2 mb-10">Review</h2>
                 {
