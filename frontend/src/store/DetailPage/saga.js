a83cb07409f1c87d65afae98e36f0a3d770cd0ff
aakanshamastwal <aakansha.mastwal92@gmail.com>
2021-04-16 18:53:31 +0530

    bug fixes

--- frontend/src/store/DetailPage/saga.js
+++ frontend/src/store/DetailPage/saga.js
@@ -137,7 +137,7 @@ function* AddToCart(action) {
         if (response?.error) return reject(response);
 
         const item = response?.data?.data;
-        if(cartItems.cart_type === 'cart') return window.location.href = `${siteDomain}${item.cart_url}`;
+        if(cartItems.cart_type === 'cart') return window.location.href = `${siteDomain}${item.cart_url}?t_id=${localStorage.getItem("trackingId")}&prod_id=${localStorage.getItem("productId")}&prod_t_m_id=${localStorage.getItem("productTrackingMappingId")}`;
         else if(cartItems.cart_type === 'express') return window.location.href = `${siteDomain}${item.redirect_url}`;
     }
     catch(e) {
