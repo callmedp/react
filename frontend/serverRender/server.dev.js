32737c48e38261aac48f9f6382dac84dc31f8610
Gaurav_S <admin@Admins-MacBook-Pro-3.local>
2021-04-20 12:20:21 +0530

    server.dev.js changes

--- frontend/serverRender/server.dev.js
+++ frontend/serverRender/server.dev.js
@@ -2,7 +2,7 @@ if (typeof global.window == "undefined") {
   global.window = {
     config: {
         isServerRendered: true,
-        siteDomain: "http://127.0.0.1:8002",
+        siteDomain: "http://127.0.0.1:8000",
         imageUrl: "/media/images/",
         resumeShineSiteDomain: "https://pp-resume.shine.com",
         shineSiteDomain: "https://mapi.shine.com",
