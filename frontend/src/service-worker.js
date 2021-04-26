01399ab64dd3d748ff9009e7b71c13567fbdc4a3
Divyanshu Pandey <divyanshu.pandey@hindustantimes.com>
2021-04-26 11:57:32 +0530

    manifest file configured

--- frontend/src/service-worker.js
+++ frontend/src/service-worker.js
@@ -1,21 +1,26 @@
 // We won't use precaching
 const ignored = self.__WB_MANIFEST;
 
-// self.addEventListener('install',(event) => {
-//   caches.keys().then(cacheNames => {
-//     cacheNames.forEach( cacheName => {
-//       caches.delete(cacheName);
-//     })
-//   })
-//   self.skipWaiting()
-// }
-// )
+self.addEventListener('install',(event) => {
+  caches.keys().then(cacheNames => {
+    cacheNames.forEach( cacheName => {
+      caches.delete(cacheName);
+    })
+  })
+  self.skipWaiting()
+}
+)
 
 
 workbox.routing.registerRoute(
   /\.(?:js|css)$/,
   new workbox.strategies.CacheFirst({
-    cacheName: 'assets'
+    cacheName: 'assets',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 7*24*3600
+      })
+    ]
   })
 )
 
@@ -26,7 +31,17 @@ workbox.routing.registerRoute(
   return request.mode === 'navigate';
 },
 new workbox.strategies.StaleWhileRevalidate({
-  cacheName: 'navigation'
+  cacheName: 'navigation', 
+  plugins: [
+    new workbox.cacheableResponse.Plugin({
+      statuses: [0, 200, 206]  
+   }),
+   new workbox.expiration.Plugin({
+    maxAgeSeconds: 24*3600, 
+    maxEntries: 20
+  })
+
+  ]
 })
 )
 
@@ -35,7 +50,13 @@ workbox.routing.registerRoute(
   return (url.pathname.endsWith('.jpg') || url.pathname.endsWith('.png') || url.pathname.endsWith('.svg'));
 }, 
 new workbox.strategies.CacheFirst({
-  cacheName: 'images'
+  cacheName: 'images', 
+  plugins: [
+    new workbox.expiration.Plugin({
+      maxAgeSeconds: 30*24*3600, 
+      maxEntries: 50
+    })
+  ]
 })
 )
 
