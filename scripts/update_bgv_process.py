f31cf4dac9e3bd1c8837f80e0e951c7617547b41
Rahul Singh <rahul.singh1@hindustantimes.com>
2021-04-27 19:58:28 +0530

    BGV report update daily

---
+++ scripts/update_bgv_process.py
@@ -0,0 +1,63 @@
+# Python Core Imports
+import time
+from datetime import date
+import logging
+import requests
+
+# Django Core Imports
+from django.conf import settings
+from django.utils import timezone
+
+# Inter App Import
+from order.models import OrderItem
+
+
+def update_bgv_process():
+    command = "update_bgv_process"
+    hire_sure_report_url = 'https://demo.hiresure.ai/bgv_verify/v1/view_report/{report_id}'
+    try:
+        orderitems = OrderItem.objects.select_related('order', 'product') \
+            .filter(oi_status__in=[165])
+
+        for oi in orderitems:
+            if oi.product.type_flow == 20:
+
+                report_value = oi.message_set.filter(is_internal=True).first().message
+                verification_url = hire_sure_report_url.format(report_id=report_value)
+                response = requests.get(verification_url)
+                rresponse = response.json()
+                if response.status_code == 200:
+                    report_status = rresponse[0]['bgv_report']['report']
+
+                    if report_status == 'IN_PROGRESS':
+                        pass
+
+                    elif report_status == 'COMPLETED':
+                        last_oi_status = oi.oi_status
+                        oi.oi_status = 166
+                        oi.last_oi_status = last_oi_status
+                        oi.closed_on = timezone.now()
+                        oi.save()
+                        oi.orderitemoperation_set.create(
+                            oi_status=oi.oi_status,
+                            last_oi_status=last_oi_status
+                        )
+                        oi.message_set.create(message=response.json(), candidate_id=oi.order.candidate_id,
+                                              is_internal=True)
+                else:
+                    logging.getLogger('error_log').error(
+                        'Failed BGV report status_code => {} of URL =>'.format(str(response.status_code),
+                                                                               verification_url))
+
+        logging.getLogger('info_log').info(
+            'Completed BGV report of date -> {} and Found Total Item -> {}'.format(str(date.today()),
+                                                                                   str(orderitems.count())))
+        print('Completed BGV report of date -> {} and Found Total Item -> {}'.format(str(date.today()),
+                                                                                     str(orderitems.count())))
+
+    except Exception as e:
+        logging.getLogger('error_log').error('Failed to run BGV process, error => {}'.format(str(e)))
+
+
+if __name__ == '__main__':
+    update_bgv_process()
