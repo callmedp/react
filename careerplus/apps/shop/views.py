65af4dec546bb703ebed11ed547ee4a2a1d40db0
Rahul Singh <rahul.singh1@hindustantimes.com>
2021-05-13 23:51:21 +0530

    recommended thrugh product detail fix

--- careerplus/apps/shop/views.py
+++ careerplus/apps/shop/views.py
@@ -939,6 +939,7 @@ class ProductDetailView(TemplateView, ProductInformationMixin, CartMixin):
         u_id = request.GET.get('u_id', request.session.get('u_id',''))
         position = self.request.GET.get('position', -1)
         popup_based_product = self.request.GET.get('popup_based_product', '')
+        recommendation_by = request.GET.get('recommendation_by', '')
 
         show_popup = self.request.GET.get('spop',False)
         if not show_popup:
@@ -998,7 +999,8 @@ class ProductDetailView(TemplateView, ProductInformationMixin, CartMixin):
                 'u_id': u_id,
                 'position':position,
                 'utm_campaign':utm_campaign,
-                'popup_based_product':popup_based_product
+                'popup_based_product':popup_based_product,
+                'recommendation_by': recommendation_by
                 })
             product_tracking_mapping_id = self.maintain_tracking_info(
                 prod)
@@ -1008,8 +1010,8 @@ class ProductDetailView(TemplateView, ProductInformationMixin, CartMixin):
 
 
             if tracking_id and prod.id and product_tracking_mapping_id:
-                make_logging_request.delay(
-                    prod.id, product_tracking_mapping_id, tracking_id, 'product_page',position, trigger_point, u_id, utm_campaign, 2, popup_based_product)
+                make_logging_request(
+                    prod.id, product_tracking_mapping_id, tracking_id, 'product_page',position, trigger_point, u_id, utm_campaign, 2, popup_based_product, recommendation_by)
 
         elif self.request.session.get('candidate_id') and \
                 request.session.get('tracking_product_id') and \
@@ -1022,8 +1024,9 @@ class ProductDetailView(TemplateView, ProductInformationMixin, CartMixin):
             r_p = request.session.get('referal_product', '')
             r_sp = request.session.get('referal_subproduct', '')
             popup_based_product = request.session.get('popup_based_product', '')
+            recommendation_by = request.GET.get('recommendation_by', '')
             make_logging_sk_request.delay(
-                request.session.get('tracking_product_id'), request.session.get('product_tracking_mapping_id'), request.session.get('tracking_id'), 'product_page',position, trigger_point, u_id, utm_campaign, 2, r_p, r_sp, popup_based_product)
+                request.session.get('tracking_product_id'), request.session.get('product_tracking_mapping_id'), request.session.get('tracking_id'), 'product_page',position, trigger_point, u_id, utm_campaign, 2, r_p, r_sp, popup_based_product, recommendation_by)
         elif self.request.session.get('candidate_id') and \
                 request.session.get('tracking_id') and \
                 not request.session.get('tracking_product_id'):
@@ -1047,9 +1050,10 @@ class ProductDetailView(TemplateView, ProductInformationMixin, CartMixin):
             r_p = request.session.get('referal_product', '')
             r_sp = request.session.get('referal_subproduct', '')
             popup_based_product = request.session.get('popup_based_product', '')
+            recommendation_by = request.GET.get('recommendation_by', '')
             if tracking_id and prod.id and product_tracking_mapping_id:
                 make_logging_sk_request.delay(
-                    prod.id, product_tracking_mapping_id, tracking_id, 'product_page',position, trigger_point, u_id, utm_campaign, 2, r_p, r_sp, popup_based_product)
+                    prod.id, product_tracking_mapping_id, tracking_id, 'product_page',position, trigger_point, u_id, utm_campaign, 2, r_p, r_sp, popup_based_product, recommendation_by)
 
         elif self.request.session.get('candidate_id') and \
                 request.session.get('tracking_id') and \
@@ -1078,9 +1082,10 @@ class ProductDetailView(TemplateView, ProductInformationMixin, CartMixin):
             r_p = request.session.get('referal_product', '')
             r_sp = request.session.get('referal_subproduct', '')
             popup_based_product = request.session.get('popup_based_product', '')
+            recommendation_by = request.GET.get('recommendation_by', '')
             if tracking_id and prod.id and product_tracking_mapping_id:
                 make_logging_sk_request.delay(
-                    prod.id, product_tracking_mapping_id, tracking_id, 'product_page',position, trigger_point, u_id, utm_campaign, 2, r_p, r_sp, popup_based_product)
+                    prod.id, product_tracking_mapping_id, tracking_id, 'product_page',position, trigger_point, u_id, utm_campaign, 2, r_p, r_sp, popup_based_product, recommendation_by)
 
 
         root = request.GET.get('root')
