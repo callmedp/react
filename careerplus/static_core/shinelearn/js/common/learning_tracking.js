ea0bb7972c3e770418cba5719bbccea5db5097d7
Divyanshu Pandey <divyanshu.pandey@hindustantimes.com>
2021-06-28 14:24:28 +0530

    cart summary desktop done

--- careerplus/static_core/shinelearn/js/common/learning_tracking.js
+++ careerplus/static_core/shinelearn/js/common/learning_tracking.js
@@ -1,22 +1,29 @@
-const _getDefaultEvents = () => {
+
+const isMobileDevice = (userAgents) => {
+    return /Android|Phone|Mobile|Opera\sM(in|ob)i|iP[ao]d|BlackBerry|SymbianOS|Safari\.SearchHelper|SAMSUNG-(GT|C)|WAP|CFNetwork|Puffin|PlayBook|Nokia|LAVA|SonyEricsson|Karbonn|UCBrowser|ucweb|Micromax|Silk|LG(MW|-MMS)|PalmOS/i.test(userAgents)
+  }
+
+const getDefaultEvents = () => {
     return {
         source : localStorage.getItem('source') || '',
         device : isMobileDevice(navigator.userAgent) ? 'mobile' : 'desktop',
-        userId : localStorage.getItem('userId') || '',
-        userType : !!localStorage.getItem('userId') ? 'logged_in' : '',
+        userId : uId || localStorage.getItem('userId') || '',
+        userType : !!(uId || localStorage.getItem('userId')) ? 'logged_in' : 'non_logged_in',
         timeStamp : new Date(),
         learning_session_id : '', 
-        shine_t_id : ''
+        shine_t_id : trackingId || ''
     }
 }
 
-const _addDefaultPayload = (payload) => {
-    return { ..._getDefaultEvents(), ...payload };
+const addDefaultPayload = (payload) => {
+    return { ...getDefaultEvents(), ...payload };
 }
 
 const sendLearningTracking = function (payload) {
-    superChargedPayload = _addDefaultPayload(payload);
-    const url = 'https://learning.shine.com/dummy-api/';
+    console.log("tracking function fired")
+    var superChargedPayload = addDefaultPayload(payload);
+    console.log("payload is", superChargedPayload)
+    const url = `${site_domain}/dummy-api/`;
     fetch( url, {
         method: 'POST',
         cache: 'no-cache',
@@ -24,5 +31,8 @@ const sendLearningTracking = function (payload) {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(superChargedPayload)
-    }).then(response => console.log("Tracking fired", response))
+    }).then(response => console.log("Tracking fired", response.json()))
 }
+
+
+console.log("learning tracking is initialized")
