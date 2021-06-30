5b110604c959a8cc0bed0bcc202a69831c00d9fa
aakanshamastwal <aakansha.mastwal92@gmail.com>
2021-06-30 19:28:07 +0530

    crm condition changes

--- careerplus/apps/api/views.py
+++ careerplus/apps/api/views.py
@@ -961,14 +961,14 @@ class ShineCandidateLoginAPIView(APIView):
         return data
 
     def get_existing_order_data(self, candidate_id):
-        from order.models import Order
+            
 
         product_found = False
         order_data = {}
         order_obj_list = Order.objects.filter(
             candidate_id=candidate_id, status__in=[1, 3])
 
-        logging.getLogger('error_log').error("order_object_list1 - {} in order".format(order_obj_list.__dict__))
+        logging.getLogger('error_log').error("CRM_RESUME001 - {} in order".format(order_obj_list.__dict__))
 
         if not order_obj_list:
             return order_data
@@ -977,20 +977,18 @@ class ShineCandidateLoginAPIView(APIView):
             if product_found:
                 break
 
-            logging.getLogger('error_log').error("order_object_list2 - {} in order".format(order_obj.__dict__))
-
             for item in order_obj.orderitems.all():
-                logging.getLogger('error_log').error("order_object_list3 - {} in order".format(item.__dict__))
                 if item.product and item.product.type_flow == 17 and item.product.type_product == 0:
-                    logging.getLogger('error_log').error("order_object_list4 - {} in order".format(item.product))
-                    logging.getLogger('error_log').error("order_object_list14 - {} in order".format(item.product.type_flow))
-                    logging.getLogger('error_log').error("order_object_list24 - {} in order".format(item.product.type_product))
+                    logging.getLogger('error_log').error("CRM_RESUME005 - {} in order".format(item.product.type_flow))
+                    logging.getLogger('error_log').error("CRM_RESUME006 - {} in order".format(item.product.type_product))
+                    logging.getLogger('error_log').error("CRM_RESUME007 - {} in order".format(item.end_date))
+
 
                     order_data = {"id": order_obj.id,
                                   "combo": True if item.product.attr.get_value_by_attribute(item.product.attr.get_attribute_by_name('template_type')).value == 'multiple' else False,
                                   "expiry": item.end_date,
                                   }
-                    logging.getLogger('error_log').error("order_object_list5 - {} in order".format(order_data))
+                    logging.getLogger('error_log').error("CRM_RESUME008 - {} in order".format(order_data))
                     product_found = True
                     break
 
@@ -1010,8 +1008,12 @@ class ShineCandidateLoginAPIView(APIView):
         # Check whether subscription active or not if resumebuilder candidate exists
         resumebuilder_candidate = Candidate.objects.filter(
             candidate_id=candidate_id).first()
+
+        if resumebuilder_candidate and resumebuilder_candidate.__dict__: logging.getLogger('error_log').error("CRM_RESUME009 - {} in order".format(resumebuilder_candidate.__dict__))
+
         if resumebuilder_candidate:
             subscription_active = resumebuilder_candidate.active_subscription or False
+            logging.getLogger('error_log').error("CRM_RESUME0010 - {} in order".format(subscription_active))
 
         self.request.session.update(login_response)
 
@@ -1038,6 +1040,8 @@ class ShineCandidateLoginAPIView(APIView):
                 'cart_pk': self.request.session.get('cart_pk') or self.request._request.session.get('cart_pk'),
                 'profile': personal_info
             }
+
+        logging.getLogger('error_log').error("CRM_RESUME0011 - {} in order".format(data_to_send))
 
         return Response(data_to_send, status=status.HTTP_201_CREATED)
 
